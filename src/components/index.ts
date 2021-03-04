1ad9e639f2f133b93188cf4d28534adf876b4d11
eratos <eratos1122@gmail.com>
2021-03-04 14:20:55 -0500

    UI Update & Receive Note List

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from './Button';
 export * from './Modal';
 export * from './Loader';
 export * from './AccountInfo';
+export * from './ReceiveInfo';
---
+++ src/contexts/userInfoContext/index.tsx
@@ -0,0 +1,75 @@
+import { useConnectedWeb3Context } from 'contexts/connectedWeb3';
+import React, { useEffect, useState } from 'react';
+import { getApiService } from 'services/api';
+import { IUser, Maybe } from 'types';
+
+export interface IUserInfoData {
+  me: Maybe<IUser>;
+  users: IUser[];
+}
+
+const UserInfoContext = React.createContext<
+  IUserInfoData & {
+    refreshUserInfo: () => Promise<void>;
+  }
+>({
+  me: null,
+  users: [],
+  refreshUserInfo: () =>
+    new Promise((resolve) => {
+      resolve();
+    }),
+});
+
+export const useUserInfo = () => {
+  const context = React.useContext(UserInfoContext);
+
+  if (!context) {
+    throw new Error('Component rendered outside the provider tree');
+  }
+
+  return context;
+};
+
+interface IProps {
+  children: React.ReactNode | React.ReactNode[];
+}
+
+export const UserInfoProvider = (props: IProps) => {
+  const [state, setState] = useState<IUserInfoData>({
+    me: null,
+    users: [],
+  });
+  const { account } = useConnectedWeb3Context();
+
+  const getUsers = async () => {
+    if (account) {
+      try {
+        const users = await getApiService().getUsers();
+        setState({
+          me:
+            users.find(
+              (user) => user.address.toLowerCase() === account?.toLowerCase()
+            ) || null,
+          users: users.filter(
+            (user) => user.address.toLowerCase() !== account?.toLowerCase()
+          ),
+        });
+      } catch (error) {
+        setState({ me: null, users: [] });
+      }
+    } else {
+      setState({ me: null, users: [] });
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, [account]);
+
+  return (
+    <UserInfoContext.Provider value={{ ...state, refreshUserInfo: getUsers }}>
+      {props.children}
+    </UserInfoContext.Provider>
+  );
+};
