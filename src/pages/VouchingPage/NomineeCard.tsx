4b5fe0a33c93058552e3bee1110556a3e839da2b
eratos <eratos1122@gmail.com>
2021-08-03 11:57:48 -0400

    build: demo vouched users list popover

--- src/pages/VouchingPage/NomineeCard.tsx
+++ src/pages/VouchingPage/NomineeCard.tsx
@@ -6,6 +6,7 @@ import { NavLink } from 'react-router-dom';
 
 import {
   makeStyles,
+  Popover,
   Button,
   Tooltip,
   Zoom,
@@ -35,7 +36,7 @@ const useStyles = makeStyles((theme) => ({
     textAlign: 'center',
   },
   infoContainer: {
-    margin: theme.spacing(0, 2),
+    margin: theme.spacing(0, 4),
     height: 40,
     display: 'flex',
     flexWrap: 'wrap',
@@ -46,6 +47,30 @@ const useStyles = makeStyles((theme) => ({
     fontSize: 14,
     fontWeight: 400,
     color: transparentize(0.3, theme.colors.text),
+    textAlign: 'center',
+  },
+  vouchedByButton: {
+    minWidth: 0,
+    margin: 0,
+    padding: theme.spacing(0, 0.2),
+    fontSize: 14,
+    fontWeight: 400,
+    color: transparentize(0.3, theme.colors.text),
+  },
+  vouchedByPaper: {
+    maxWidth: 180,
+    maxHeight: 100,
+    padding: theme.spacing(1, 2),
+    fontSize: 14,
+    fontWeight: 400,
+    color: transparentize(0.3, theme.colors.text),
+    overflowY: 'auto',
+    display: 'flex',
+    flexWrap: 'wrap',
+    borderRadius: 8,
+    background:
+      'linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(223, 237, 234, 0.4) 40.1%), linear-gradient(180deg, rgba(237, 253, 254, 0.4) 0%, rgba(207, 231, 233, 0) 100%), #FFFFFF',
+    boxShadow: '0px 4px 6px rgba(181, 193, 199, 0.16)',
   },
   description: {
     margin: theme.spacing(2, 1),
@@ -84,8 +109,9 @@ const TextOnlyTooltip = withStyles({
     maxWidth: 240,
     fontSize: 12,
     fontWeight: 500,
-    color: 'rgba(81, 99, 105, 0.5)',
-    background: '#C3CDCF',
+    color: 'rgba(81, 99, 105, 0.7)',
+    background:
+      'linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(223, 237, 234, 0.4) 40.1%), linear-gradient(180deg, rgba(237, 253, 254, 0.4) 0%, rgba(207, 231, 233, 0) 100%), #FFFFFF',
   },
 })(Tooltip);
 
@@ -98,6 +124,22 @@ const NomineeCard = ({ nominee }: { nominee: INominee }) => {
     ? nominee.nominated_by_user_id === selectedMyUser.id ||
       nominee.nominations.some((user) => user.id === selectedMyUser.id)
     : true;
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
+    null
+  );
+
+  // open VouchedBy
+  const openVouchedBy = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  // close VouchedBy
+  const closeVouchedBy = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'vouched-by-popover' : undefined;
 
   return (
     <div className={classes.root}>
@@ -111,8 +153,51 @@ const NomineeCard = ({ nominee }: { nominee: INominee }) => {
           >
             {nominee.nominator.name}
           </NavLink>
+          {nominee.nominations.length > 0 && (
+            <>
+              {' '}
+              and vouched for by{' '}
+              <Button
+                className={classes.vouchedByButton}
+                aria-describedby={id}
+                onClick={openVouchedBy}
+              >
+                {nominee.nominations.length}
+              </Button>
+            </>
+          )}
         </span>
       </div>
+      <Popover
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        classes={{
+          paper: classes.vouchedByPaper,
+        }}
+        id={id}
+        onClose={closeVouchedBy}
+        open={open}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        {nominee.nominations.map((user, index) => (
+          <>
+            <NavLink
+              key={user.id}
+              className={classes.info}
+              to={`profile/${user.address}`}
+            >
+              {user.name}
+            </NavLink>
+            {index < nominee.nominations.length - 1 && <>,&nbsp;</>}
+          </>
+        ))}
+      </Popover>
       <TextOnlyTooltip
         TransitionComponent={Zoom}
         placement="top"
