c8aee8674c03bf9072971e22206ed152e8bb6851
eratos <eratos1122@gmail.com>
2021-08-10 11:54:24 -0400

    fix: 'never used' warnings

--- src/pages/VouchingPage/NomineeCard.tsx
+++ src/pages/VouchingPage/NomineeCard.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import moment from 'moment';
 import { transparentize } from 'polished';
 import { NavLink } from 'react-router-dom';
 
--- src/pages/VouchingPage/VouchingPage.tsx
+++ src/pages/VouchingPage/VouchingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import { Button, makeStyles } from '@material-ui/core';
 
