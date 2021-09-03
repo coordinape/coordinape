e906f9181ee79ccd0d3ff32e26c1a2711b396c79
Lawrence Wang <l@lw.io>
2021-09-03 09:54:24 -0700

    show specific error messages

--- src/hooks/useApeSnackbar.ts
+++ src/hooks/useApeSnackbar.ts
@@ -1,5 +1,24 @@
 import { useSnackbar } from 'notistack';
 
+const defaultErrorMessage = 'Something went wrong.';
+
+const displayError = (error: any) => {
+  if (!error) return defaultErrorMessage;
+  if (typeof error === 'string') return error;
+  const data = error.response?.data;
+
+  // when a form submission has validation errors, Laravel responds with:
+  // {
+  //   "message": "The given data was invalid.",
+  //   "errors": {"address": ["The address has already been taken."]}
+  // }
+  //
+  // and we want to show the specific error messages
+  if (data?.errors) return Object.values(data?.errors).join(' ');
+
+  return data?.message || error.message || defaultErrorMessage;
+};
+
 export const useApeSnackbar = (): {
   apeInfo: (message: string) => void;
   apeError: (error: any) => void;
@@ -9,13 +28,6 @@ export const useApeSnackbar = (): {
     apeInfo: (message: string) =>
       enqueueSnackbar(message, { variant: 'default' }),
     apeError: (error: any) =>
-      enqueueSnackbar(
-        typeof error === 'string'
-          ? error
-          : error?.response?.data?.message ||
-              error?.message ||
-              'Something went wrong!',
-        { variant: 'error' }
-      ),
+      enqueueSnackbar(displayError(error), { variant: 'error' }),
   };
 };
