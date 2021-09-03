ea669b81513d6d5bddc626683e9065e132560530
Ethan Blackburn <eblackb1@slu.edu>
2021-09-03 12:33:25 -0700

    Add test to check that content loads on landing page.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import { render, screen } from '@testing-library/react';
 
-import App from './App';
+import AppLandingPage from './AppLandingPage';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders landing page', () => {
+  render(<AppLandingPage />);
+  const descriptionElement = screen.getByText(
+    /Scale your community with tools to reward contributors, incentivize participation and manage resources/i
+  );
+  expect(descriptionElement).toBeInTheDocument();
 });
