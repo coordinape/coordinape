960c4b0e5f58acec7900b0db258fea8983c59490
eratos <eratos1122@gmail.com>
2021-09-13 08:14:30 -0400

    fix: getSignature timeout

--- src/utils/provider.ts
+++ src/utils/provider.ts
@@ -3,35 +3,31 @@ import { Wallet, ethers } from 'ethers';
 export const getSignature = async (data: string, provider?: any) => {
   if (!provider) throw 'Missing provider for getSignature';
 
-  function doSomething(maxExecutionTime: number) {
-    return new Promise((resolve) => {
-      setTimeout(() => resolve(false), maxExecutionTime);
-
-      (async function () {
-        const signer: Wallet = provider.getSigner();
-        const address = await signer.getAddress();
-        const signature = await signer.signMessage(data);
-        const byteCode = await provider.getCode(address);
-        const isSmartContract =
-          byteCode && ethers.utils.hexStripZeros(byteCode) !== '0x';
-        let hash = '';
-        if (isSmartContract) {
-          hash = ethers.utils.hashMessage(data);
-          // validate smart contract signature (might work for other other types of smart contract wallets )
-          // comment out when not testing
-          //_validateContractSignature(signature, hash, provider, address);
-        }
-        resolve({ signature, hash });
-      })();
-    });
-  }
-
-  const result = await doSomething(4500);
-  if (result) {
-    return result as any;
-  } else {
+  const signer: Wallet = provider.getSigner();
+  const address = await signer.getAddress();
+  const signature: any = await (async () =>
+    new Promise((resolve) => {
+      const t = setTimeout(() => resolve(false), 30000);
+      signer.signMessage(data).then((sig) => {
+        clearTimeout(t);
+        resolve(sig);
+      });
+    }))();
+  if (!signature) {
     throw 'There was an error signing the message with your hardware wallet. Try sending a shorter message.';
   }
+
+  const byteCode = await provider.getCode(address);
+  const isSmartContract =
+    byteCode && ethers.utils.hexStripZeros(byteCode) !== '0x';
+  let hash = '';
+  if (isSmartContract) {
+    hash = ethers.utils.hashMessage(data);
+    // validate smart contract signature (might work for other other types of smart contract wallets )
+    // comment out when not testing
+    //_validateContractSignature(signature, hash, provider, address);
+  }
+  return { signature, hash };
 };
 
 export const _validateContractSignature = async (
