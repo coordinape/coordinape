37407a4e0f18aa37f80b1ddc56fe142b3bcb8418
Ryan <rdturner40@gmail.com>
2021-10-29 10:42:12 -0400

    Added circle card component. Still pending data hookup

---
+++ src/pages/OverviewPage/CircleCard.tsx
@@ -0,0 +1,56 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  circleWrapper: {
+    width: '120px',
+    height: '120px',
+    borderRadius: '50%',
+    backgroundColor: theme.colors.lightGray,
+  },
+  innerText: {
+    textAlign: 'center',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignContent: 'center',
+  },
+  titleText: {
+    color: theme.colors.text,
+    fontSize: 14,
+    paddingBottom: 0,
+    marginBottom: 0,
+  },
+  subTitle: {
+    fontSize: 12,
+    color: theme.colors.lightText,
+    marginTop: 0,
+    paddingTop: 0,
+  },
+}));
+
+interface CircleCardProps {
+  name: string;
+  epochTitle: string;
+  startDays: string;
+  startTime: string;
+}
+
+export default function CircleCard({
+  name,
+  epochTitle,
+  startDays,
+  startTime,
+}: CircleCardProps) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.circleWrapper}>
+      <div className={classes.innerText}>
+        <h6 className={classes.titleText}>{name}</h6>
+        <h3 className={classes.subTitle}>
+          Epoch {epochTitle} starts in {startDays} days {startTime} hours
+        </h3>
+      </div>
+    </div>
+  );
+}
