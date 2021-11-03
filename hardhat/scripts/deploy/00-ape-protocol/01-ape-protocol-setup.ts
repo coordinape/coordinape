b21d23e1c26b11c9108a5902086e53bbc25cb0b8
Niraj Kamdar <51387861+Niraj-Kamdar@users.noreply.github.com>
2021-11-04 05:27:57 +0530

    feat: add hardhat support (#230)
    * feat: add hardhat support
    
    * feat: add hardhat readme
    
    * fix: remove circle contracts
    
    * use a git submodule for contract code
    
    * add `git submodule update` step to README
    
    * feat: add tests for apeVault (#248)
    
    * feat: add tests for apeVault
    
    * fix: add apeRouter tests
    
    * add yarn hardhat:install
    
    * disable failing tests
    
    * revert change to timeout
    
    * no need for chai.use
    
    hardhat-waffle takes care of this
    
    Co-authored-by: Lawrence Wang <l@lw.io>
    
    * try running hardhat tests in Github Actions
    
    * check in typechain types
    
    I'm not 100% sure these should be checked in--open to debate about it--but at the moment it does work around an issue in Github Actions with the submodule not being loaded
    
    * add steps to workflow
    
    * don't use a separate .env file
    
    * add --frozen-lockfile to hardhat yarn install
    
    * Delete .env.example
    
    * remove typechain types
    
    CI needs to regenerate them anyway it seems
    
    * re-add typechain to .gitignore
    
    * cache hardhat/node_modules
    
    * fix key for hardhat node_modules cache
    
    * add build step
    
    Co-authored-by: Lawrence Wang <l@lw.io>

---
+++ hardhat/scripts/deploy/00-ape-protocol/01-ape-protocol-setup.ts
@@ -0,0 +1,59 @@
+import { Contract } from '@ethersproject/contracts';
+import { ethers } from 'hardhat';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+
+import { ApeRegistry__factory } from '../../../typechain';
+
+async function executeTimelockedFunction(
+  contract: Contract,
+  method: string,
+  args: Array<unknown>
+) {
+  console.log(
+    `executing "${method}" of Contract: "${
+      contract.address
+    }" with (${args.join()}) arguments`
+  );
+  const ZERO = ethers.utils.zeroPad([0], 32);
+  const data = contract.interface.encodeFunctionData(method, args);
+  await contract.schedule(contract.address, data, ZERO, ZERO, 0);
+  await contract.execute(contract.address, data, ZERO, ZERO, 0);
+}
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployer } = await hre.getNamedAccounts();
+  const useProxy = !hre.network.live;
+  const signer = await ethers.getSigner(deployer);
+
+  const apeRegistry = ApeRegistry__factory.connect(
+    (await hre.deployments.get('ApeRegistry')).address,
+    signer
+  );
+
+  const apeFee = await hre.deployments.get('FeeRegistry');
+  const apeRouter = await hre.deployments.get('ApeRouter');
+  const apeDistributor = await hre.deployments.get('ApeDistributor');
+  const apeVaultFactory = await hre.deployments.get('ApeVaultFactory');
+
+  await executeTimelockedFunction(apeRegistry, 'setFeeRegistry', [
+    apeFee.address,
+  ]);
+
+  await executeTimelockedFunction(apeRegistry, 'setRouter', [
+    apeRouter.address,
+  ]);
+
+  await executeTimelockedFunction(apeRegistry, 'setDistributor', [
+    apeDistributor.address,
+  ]);
+
+  await executeTimelockedFunction(apeRegistry, 'setFactory', [
+    apeVaultFactory.address,
+  ]);
+
+  return !useProxy;
+};
+export default func;
+func.id = 'setup_ape_protocol';
+func.tags = ['SetupApe', 'Ape'];
