e091d2ba871f9217b7eccf7c7a4ac3a69bdac643
Ryan <rdturner40@gmail.com>
2021-11-03 06:43:32 -0400

    Adding contracts to hook

--- src/lib/contracts/contracts.ts
+++ src/lib/contracts/contracts.ts
@@ -8,19 +8,27 @@ import * as ethers from 'ethers';
 // import { Maybe } from 'true-myth';
 
 // - Type Imports
-// import types/contract
-// import types/factories/contract_factory
+import { VaultAPI } from '../../types/VaultAPI';
 
 import addresses from './address';
 import { chainIdToNetworkType, defaultNetworkId } from './networks';
 
+import { ApeVaultFactory } from 'types/ApeVaultFactory';
+import { ApeVaultWrapper } from 'types/ApeVaultWrapper';
+import { ApeVaultFactory__factory } from 'types/factories/ApeVaultFactory__factory';
+import { ApeVaultWrapper__factory } from 'types/factories/ApeVaultWrapper__factory';
+import { VaultAPI__factory } from 'types/factories/VaultAPI__factory';
+
 export interface ContractAddresses {
-  contractName: string;
+  VaultAPI: string;
+  ApeVaultFactory: string;
+  ApeVaultWrapper: string;
 }
 
 export interface Contracts {
-  //Cntrct: ContractName; //(imported from ../../types/contractName)
-  Ralph: string; //Just to get rid of a typescript error
+  vaultApi: VaultAPI; //(imported from ../../types/contractName)
+  vaultFactory: ApeVaultFactory;
+  vaultWrapper: ApeVaultWrapper;
 }
 
 function useVaultContracts(): Contracts | null {
@@ -40,8 +48,6 @@ function useVaultContracts(): Contracts | null {
       }
 
       contracts = addresses[chainIdToNetworkType(chainId)];
-      // Temp disable because signer is used in the return statement
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       signer = library.getSigner();
     }
 
@@ -51,8 +57,15 @@ function useVaultContracts(): Contracts | null {
     }
 
     return {
-      Ralph: 'placeholder',
-      // ContractName: contract_factory.connect(contracts.ContractName, signer)
+      vaultApi: VaultAPI__factory.connect(contracts.VaultAPI, signer),
+      vaultFactory: ApeVaultFactory__factory.connect(
+        contracts.ApeVaultFactory,
+        signer
+      ),
+      vaultWrapper: ApeVaultWrapper__factory.connect(
+        contracts.ApeVaultWrapper,
+        signer
+      ),
     };
   }, [active, library, chainId]);
   return contract;
