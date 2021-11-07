0c1a31369f1be2d2940d57abcda954a830c41ad3
Ryan <rdturner40@gmail.com>
2021-11-07 01:06:53 -0500

    WIP working on useVaultWrapper (uses ApeVaultWrapper and contractTypes)

--- src/lib/contract-hooks/useVaultWrapper.ts
+++ src/lib/contract-hooks/useVaultWrapper.ts
@@ -1,30 +1,119 @@
 // - Contract Imports
 import { ethers } from 'ethers';
 
-import { CreateVault, Maybe } from '../../types/contractTypes';
+import {
+  AddFunds,
+  Maybe,
+  DepositSimpleToken,
+  WithdrawUnderlying,
+  ApproveCircleAdmin,
+  ExitVaultToken,
+} from '../../types/contractTypes';
 import { useVaultContracts } from '../contracts/contracts';
 
-export function UseDbank() {
-  const factory = useVaultContracts()?.ApeVaultFactory;
+//     "hasAccess(address)": FunctionFragment;
+//     "owner()": FunctionFragment;
+//     "profit()": FunctionFragment;
+//     "registry()": FunctionFragment;
+//     "renounceOwnership()": FunctionFragment;
+//     "setRegistry(address)": FunctionFragment;
+//     "simpleToken()": FunctionFragment;
+//     "syncUnderlying()": FunctionFragment;
+//     "tap(uint256,uint8)": FunctionFragment;
+//     "token()": FunctionFragment;
+//     "totalAssets()": FunctionFragment;
+//     "totalVaultBalance(address)": FunctionFragment;
+//     "transferOwnership(address)": FunctionFragment;
+//     "underlyingValue()": FunctionFragment;
+//     "updateAllowance(bytes32,address,uint256,uint256,uint256)": FunctionFragment;
+//     "vault()": FunctionFragment;
 
-  const _createApeVault = async (_params: CreateVault) => {
+const handleError = (e: any) => {
+  /**
+   * Function to handle throwing errors (To reduce code repetition)
+   */
+  if (e.code === 4001) {
+    throw Error('The transaction was rejected by your wallet');
+  } else {
+    throw Error('The transaction Failed');
+  }
+};
+
+export function useVaultWrapper() {
+  const factory = useVaultContracts()?.ApeVaultWrapper;
+
+  //! Leave off on line 305, ApeVaultWrapper.ts
+
+  const _exitVaultToken = async (_params: ExitVaultToken) => {
     let tx: Maybe<ethers.ContractTransaction>;
     try {
-      tx = await factory?.createApeVault(
-        _params._token,
-        _params._simpleToken,
+      tx = await factory?.exitVaultToken(
+        _params._underlying,
+        _params._overrides
+      );
+    } catch (e: any) {
+      handleError(e);
+    }
+    return tx;
+  };
+
+  const _approveCircleAdmin = async (_params: ApproveCircleAdmin) => {
+    let tx: Maybe<ethers.ContractTransaction>;
+    try {
+      tx = await factory?.approveCircleAdmin(
+        _params._circle,
+        _params._admin,
+        _params._overrides
+      );
+    } catch (e: any) {
+      handleError(e);
+    }
+    return tx;
+  };
+
+  const _withdrawUnderlying = async (_params: WithdrawUnderlying) => {
+    let tx: Maybe<ethers.ContractTransaction>;
+    try {
+      tx = await factory?.apeWithdrawUnderlying(
+        _params._amount,
+        _params._overrides
+      );
+    } catch (e: any) {
+      handleError(e);
+    }
+    return tx;
+  };
+
+  const _depositSimpleToken = async (_params: DepositSimpleToken) => {
+    let tx: Maybe<ethers.ContractTransaction>;
+    try {
+      tx = await factory?.apeDepositSimpleToken(
+        _params._amount,
         _params._overrides
       );
     } catch (e: any) {
       console.error(e);
-      if (e.code === 4001) {
-        throw Error(`Transaction rejected by your wallet`);
-      }
-      throw Error(`Failed to submit create vault.`);
+      handleError(e);
+    }
+    return tx;
+  };
+
+  const _addFunds = async (_params: AddFunds) => {
+    let tx: Maybe<ethers.ContractTransaction>;
+    try {
+      tx = await factory?.addFunds(_params._amount, _params._overrides);
+    } catch (e: any) {
+      handleError(e);
     }
 
     return tx;
   };
 
-  return { _createApeVault };
+  return {
+    _depositSimpleToken,
+    _addFunds,
+    _withdrawUnderlying,
+    _approveCircleAdmin,
+    _exitVaultToken,
+  };
 }
--- src/types/contractTypes.ts
+++ src/types/contractTypes.ts
@@ -1,6 +1,34 @@
+import { BigNumberish } from '@ethersproject/bignumber';
+import { BytesLike } from '@ethersproject/bytes';
+
 export interface CreateVault {
   _token: string;
   _simpleToken: string;
+  _overrides?: any;
+}
+
+export interface AddFunds {
+  _amount: BigNumberish;
+  _overrides?: any;
+}
+
+export interface DepositSimpleToken {
+  _amount: BigNumberish;
+  _overrides?: any;
+}
+export interface WithdrawUnderlying {
+  _amount: BigNumberish;
+  _overrides?: any;
+}
+
+export interface ApproveCircleAdmin {
+  _circle: BytesLike;
+  _admin: string;
+  _overrides?: any;
+}
+
+export interface ExitVaultToken {
+  _underlying: boolean;
   _overrides?: any;
 }
 
