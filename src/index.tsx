e351820e3736436ce83ea52b336e9b06fcfd64cd
Alex Warren <exrhizo@gmail.com>
2021-11-07 19:17:23 -0800

    update sentry and lower the sample rate

--- src/index.tsx
+++ src/index.tsx
@@ -27,11 +27,7 @@ Sentry.init({
       levels: ['error'],
     }),
   ],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 0.1,
 });
 Sentry.setTag('landing_page', !RENDER_APP);
 
