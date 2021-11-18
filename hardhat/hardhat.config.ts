2c7376764adf1862773f89c2dec70d54af0a2530
Niraj Kamdar <nirajkamdar27@pm.me>
2021-11-19 00:24:58 +0530

    feat: move to ganache and fix minting script

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -9,6 +9,9 @@ import '@nomiclabs/hardhat-waffle';
 
 dotenv.config({ path: '../.env' });
 
+const USDC_WHALE_ADDRESS = '0x47ac0Fb4F2D84898e4D9E7b4DaB3C24507a6D503';
+const USDC_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
+
 task('accounts', 'Prints the list of accounts', async (args, hre) => {
   const accounts = await hre.ethers.getSigners();
   console.log('\nAvailable Accounts\n==================\n');
@@ -22,6 +25,55 @@ task('accounts', 'Prints the list of accounts', async (args, hre) => {
     );
   });
 });
+
+task('mint', 'Mints the given token to specified account')
+  .addParam('token', 'The token to mint')
+  .addParam('receiver', 'The receiver of the minted token')
+  .addParam('amount', 'The amount of tokens to mint')
+  .setAction(
+    async (args: { token: string; receiver: string; amount: string }, hre) => {
+      const mintEth = async (receiver: string, amount: string) => {
+        const signers = await hre.ethers.getSigners();
+
+        const tx = {
+          to: receiver,
+          value: ethers.utils.parseEther(amount),
+        };
+
+        await signers[0].sendTransaction(tx);
+
+        console.log(`Minted ${amount} ETH to ${receiver} successfully!`);
+      };
+
+      async function mintUsdc(receiver: string, amount: string): Promise<void> {
+        // Todo: fix this
+        const usdcWhale = await hre.ethers.getSigner(USDC_WHALE_ADDRESS);
+        const usdc = new ethers.Contract(
+          USDC_ADDRESS,
+          ['function transfer(address to, uint amount)'],
+          usdcWhale
+        );
+
+        const usdcAmount = ethers.utils.parseUnits(amount, 'mwei');
+
+        await usdc.transfer(receiver, usdcAmount);
+
+        console.log(`Minted ${amount} USDC to ${receiver} successfully!`);
+      }
+
+      switch (args.token) {
+        case 'USDC':
+          await mintUsdc(args.receiver, args.amount);
+          break;
+        case 'ETH':
+          await mintEth(args.receiver, args.amount);
+          break;
+        default:
+          console.error(`Unknown token name: ${args.token}`);
+          process.exit(1);
+      }
+    }
+  );
 
 const config: HardhatUserConfig = {
   solidity: {
--- hardhat/scripts/mint.ts
+++ hardhat/scripts/mint.ts
@@ -1,31 +1,36 @@
-import { argv } from 'process';
-
-import { BigNumberish } from 'ethers';
-import { network } from 'hardhat';
-
 import { ERC20__factory } from '../typechain';
 import { unlockSigner } from '../utils/unlockSigner';
 
 export const USDC_WHALE_ADDRESS = '0x47ac0Fb4F2D84898e4D9E7b4DaB3C24507a6D503';
 export const USDC_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
 
-async function mintUsdc(receiver: string, amount: BigNumberish): Promise<void> {
+async function mintUsdc(receiver: string, amount: string): Promise<void> {
   const usdcWhale = await unlockSigner(USDC_WHALE_ADDRESS);
   const usdc = ERC20__factory.connect(USDC_ADDRESS, usdcWhale);
 
-  await usdc.transfer(receiver, amount);
+  const usdcAmount = ethers.utils.parseUnits(amount, 'mwei');
+
+  await usdc.transfer(receiver, usdcAmount);
 
   console.log(`Minted ${amount} USDC to ${receiver} successfully!`);
 }
 
-async function mintEth(receiver: string, amount: BigNumberish): Promise<void> {
-  await network.provider.send('hardhat_setBalance', [receiver, amount]);
+async function mintEth(receiver: string, amount: string): Promise<void> {
+  const signers = await ethers.getSigners();
+
+  const tx = {
+    to: receiver,
+    // Convert currency unit from ether to wei
+    value: ethers.utils.parseEther(amount),
+  };
+
+  signers[signers.length - 1].sendTransaction(tx);
 
   console.log(`Minted ${amount} ETH to ${receiver} successfully!`);
 }
 
-async function mint(): Promise<void> {
-  const args = argv.slice(2);
+export async function mint(args: string[]): Promise<void> {
+  // const args = argv.slice(2);
   if (args.length < 3) {
     console.error('Please provide a token name, an address and an amount');
     process.exit(1);
@@ -47,10 +52,3 @@ async function mint(): Promise<void> {
       process.exit(1);
   }
 }
-
-mint()
-  .then()
-  .catch(e => {
-    console.error(e);
-    process.exit(1);
-  });
