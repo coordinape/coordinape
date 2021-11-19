ced06adde58b95e4d57717feb10e127c4943942f
Edax Uclés <filanderuclez@gmail.com>
2021-11-18 19:08:56 -0600

    adds the abillity to show tooltip in mobile

---
+++ src/hooks/useMobileDetect.ts
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function useMobileDetect() {
+  const [isMobile, setMobile] = React.useState(false);
+
+  React.useEffect(() => {
+    const userAgent =
+      typeof window.navigator === 'undefined' ? '' : navigator.userAgent;
+    const mobile = Boolean(
+      userAgent.match(
+        /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
+      )
+    );
+    setMobile(mobile);
+  }, []);
+
+  return { isMobile };
+}
