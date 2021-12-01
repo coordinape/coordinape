017a9b9e34a85b5cbf10ba9d2f880e012f238f34
Lawrence Wang <l@lw.io>
2021-11-30 18:21:24 -0800

    remove unused code

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -2,12 +2,8 @@ import { assertDef } from 'utils/tools';
 
 import { IAuth } from 'types';
 
-const STORAGE_KEY_FORCE_OPT_OUT_CIRCLE_ID_ITEM = 'capeForceOptOutCircleId';
 const STORAGE_KEY_CIRCLE_ID = 'capeCircleId';
 const STORAGE_AUTH = 'capeAuth';
-
-const getHasSeenForceOptOutPopupKey = (userId: number) =>
-  `${STORAGE_KEY_FORCE_OPT_OUT_CIRCLE_ID_ITEM}+${userId}`;
 
 export default {
   setCircleId: (id: number) =>
@@ -34,11 +30,4 @@ export default {
     }
   },
   clearAuth: () => localStorage.removeItem(STORAGE_AUTH),
-
-  setHasSeenForceOptOutPopup: (userId: number) =>
-    localStorage.setItem(getHasSeenForceOptOutPopupKey(userId), 'true'),
-  hasSeenForceOptOutPopup: (userId: number) =>
-    !!localStorage.getItem(getHasSeenForceOptOutPopupKey(userId)),
-  unsetHasSeenForceOptOutPopup: (userId: number) =>
-    localStorage.removeItem(getHasSeenForceOptOutPopupKey(userId)),
 };
