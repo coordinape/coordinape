7d5bb17d5b0ee1da6f87ba2636d425240a284cf9
Alex <exrhizo@gmail.com>
2021-12-02 00:21:04 -0700

    Jsur/feature/ape 220 tooltips (#331)
    Authored by: julius.suominen <julius.suominen@gmail.com>
    Rebase of #312

--- src/pages/AdminPage/AdminCircleModal.tsx
+++ src/pages/AdminPage/AdminCircleModal.tsx
@@ -13,6 +13,9 @@ import { getAvatarPath } from 'utils/domain';
 
 import { ICircle } from 'types';
 
+const DOCS_HREF = 'https://docs.coordinape.com/welcome/admin_info';
+const DOCS_TEXT = 'See the docs...';
+
 const useStyles = makeStyles(theme => ({
   logoContainer: {
     position: 'relative',
@@ -123,7 +126,35 @@ const useStyles = makeStyles(theme => ({
     textAlign: 'center',
     marginTop: theme.spacing(2),
   },
+  tooltipLink: {
+    display: 'block',
+    margin: theme.spacing(2, 0, 0),
+    textAlign: 'center',
+    color: theme.colors.linkBlue,
+  },
 }));
+
+const YesNoTooltip = ({ yes = '', no = '', href = '', anchorText = '' }) => {
+  const classes = useStyles();
+  return (
+    <>
+      <strong>Yes</strong> - {yes}
+      <br />
+      <strong>No</strong> - {no}
+      <br />
+      {href && (
+        <a
+          className={classes.tooltipLink}
+          rel="noreferrer"
+          target="_blank"
+          href={href}
+        >
+          {anchorText}
+        </a>
+      )}
+    </>
+  );
+};
 
 export const AdminCircleModal = ({
   circle,
@@ -299,6 +330,16 @@ export const AdminCircleModal = ({
           value={vouching}
           onChange={val => setVouching(val)}
           label="Enable Vouching?"
+          infoTooltip={
+            <YesNoTooltip
+              yes="Circle members can invite new people to the
+          circle; they become new members if enough other members vouch for
+          them"
+              no="Only circle admins may add new members"
+              href={DOCS_HREF}
+              anchorText={DOCS_TEXT}
+            />
+          }
         />
         <ApeTextField
           label="Token name"
@@ -366,17 +407,39 @@ export const AdminCircleModal = ({
           value={defaultOptIn}
           onChange={val => setDefaultOptIn(val)}
           label="Default Opt In?"
+          infoTooltip={
+            <YesNoTooltip
+              yes="All new members are eligible to receive GIVE"
+              no="New members need to log into Coordinape and opt in to receiving GIVE"
+              href={DOCS_HREF}
+              anchorText={DOCS_TEXT}
+            />
+          }
         />
         <ApeToggle
           value={onlyGiverVouch}
           onChange={val => setOnlyGiverVouch(val)}
           className={clsx(classes.vouchingItem, !vouching && 'disabled')}
           label="Only Givers can vouch"
+          infoTooltip={
+            <YesNoTooltip
+              yes="Only members who are eligible to send GIVE can vouch for new members"
+              no="Anyone in the circle can vouch for new members"
+              href={DOCS_HREF}
+              anchorText={DOCS_TEXT}
+            />
+          }
         />
         <ApeToggle
           value={teamSelection}
           onChange={val => setTeamSelection(val)}
           label="Team Selection Enabled"
+          infoTooltip={
+            <YesNoTooltip
+              yes="Members select a team during allocation and make allocations only to that team"
+              no="Members make allocations to anyone in the circle"
+            />
+          }
         />
         <ApeToggle
           value={autoOptOut}
