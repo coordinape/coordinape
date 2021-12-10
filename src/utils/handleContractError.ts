9a9ae274d052df4c9f9b847bd258ab40e5df5ee9
Lawrence Wang <l@lw.io>
2021-12-10 13:14:26 -0800

    remove duplicate handleContractError

--- src/utils/handleContractError.ts
+++
@@ -1,7 +0,0 @@
-export const handleContractError = (e: any) => {
-  console.error(e);
-  if (e.code === 4001) {
-    throw Error(`Transaction rejected by your wallet`);
-  }
-  throw Error(`Failed to submit create vault.`);
-};
