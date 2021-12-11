89b9a8967b5900619bec2d8e5e7978e8df137890
Lawrence Wang <l@lw.io>
2021-12-10 17:43:06 -0800

    Temporarily revert removal of fields
    didn't realize this was still being used as a placeholder for other modals... 😑

--- src/forms/AdminVaultForm.test.tsx
+++ src/forms/AdminVaultForm.test.tsx
@@ -3,7 +3,9 @@ import { schema } from './AdminVaultForm';
 test('accept blank token address', async () => {
   const data = {
     asset: 'DAI',
+    token: 0,
     custom_asset: '',
+    repeat_monthly: false,
   };
   const result = await schema.parseAsync(data);
   expect(result).toEqual(data);
@@ -12,7 +14,9 @@ test('accept blank token address', async () => {
 test('accept valid token address', async () => {
   const data = {
     asset: 'DAI',
+    token: 0,
     custom_asset: '0x47ac0fb4f2d84898e4d9e7b4dab3c24507a6d503',
+    repeat_monthly: false,
   };
 
   const result = await schema.parseAsync(data);
@@ -22,7 +26,9 @@ test('accept valid token address', async () => {
 test('reject invalid token address', async () => {
   const data = {
     asset: 'DAI',
+    token: 0,
     custom_asset: '0xg7ac0fb4f2d84898e4d9e7b4dab3c24507a6d503',
+    repeat_monthly: false,
   };
 
   await expect(async () => schema.parseAsync(data)).rejects.toThrow();
--- src/forms/AdminVaultForm.tsx
+++ src/forms/AdminVaultForm.tsx
@@ -14,15 +14,18 @@ const zAssetEnum = z.enum([
   'OTHER',
 ]);
 export type AssetEnum = z.infer<typeof zAssetEnum>;
+// type TEpochRepeatEnum = typeof AssetEnum['_type'];
 
 export const schema = z
   .object({
-    asset: zAssetEnum.optional(),
+    token: z.number(),
+    asset: zAssetEnum,
     custom_asset: z.string().refine(async val => {
       // TODO should be optional only when asset is not "OTHER"
       if (val == '') return true;
       return zEthAddress.parseAsync(val);
     }),
+    repeat_monthly: z.boolean(),
   })
   .strict();
 
@@ -31,8 +34,10 @@ const AdminVaultForm = createForm({
   getInstanceKey: () => 'new',
   getZodParser: () => schema,
   load: () => ({
+    token: 0,
     asset: 'DAI' as AssetEnum,
     custom_asset: '',
+    repeat_monthly: false,
   }),
   fieldKeys: Object.keys(schema.shape),
   fieldProps: {},
