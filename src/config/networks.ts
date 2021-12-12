0bc2fe8ee3bd997dc6ba34da20d3be0e920e85c2
Ryan <rdturner40@gmail.com>
2021-12-12 11:53:43 -0500

    Added other tokens to knownToken

--- src/config/networks.ts
+++ src/config/networks.ts
@@ -9,7 +9,14 @@ import { IKnownTokenData, INetwork, IToken } from 'types';
 // TODO: This are coupled in a few places, make sure that they all
 // stay compatible.
 export type NetworkId = 1 | 4 | 1337;
-export type KnownToken = 'USDC' | 'yvUSDC' | 'DAI';
+export type KnownToken =
+  | 'USDC'
+  | 'yvUSDC'
+  | 'DAI'
+  | 'YFI'
+  | 'SUSHI'
+  | 'ALUSD'
+  | 'USDT';
 
 export const networkIds = {
   MAINNET: 1,
@@ -86,6 +93,42 @@ export const knownTokens: { [name in KnownToken]: IKnownTokenData } = {
       [networkIds.MAINNET]: '0x5f18C75AbDAe578b483E5F43f12a39cF75b973a9',
       [networkIds.HARDHAT]: '0x5f18C75AbDAe578b483E5F43f12a39cF75b973a9',
       [networkIds.RINKEBY]: '0x7C38c1913A7d512437E7f97D83519C1f9B59239e',
+    },
+  },
+  YFI: {
+    symbol: 'YFI',
+    decimals: 18,
+    addresses: {
+      [networkIds.MAINNET]: '0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e',
+      [networkIds.HARDHAT]: '0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e',
+      [networkIds.RINKEBY]: '0x3efb9863b9b94c200bb8adba46adc8bb9d21e1c9',
+    },
+  },
+  SUSHI: {
+    symbol: 'SUSHI',
+    decimals: 18,
+    addresses: {
+      [networkIds.MAINNET]: '0x6b3595068778dd592e39a122f4f5a5cf09c90fe2',
+      [networkIds.HARDHAT]: '0x6b3595068778dd592e39a122f4f5a5cf09c90fe2',
+      [networkIds.RINKEBY]: '0x0ead1160bd2ca5a653e11fae3d2b39e4948bda4d',
+    },
+  },
+  ALUSD: {
+    symbol: 'ALUSD',
+    decimals: 18,
+    addresses: {
+      [networkIds.MAINNET]: '0xbc6da0fe9ad5f3b0d58160288917aa56653660e9',
+      [networkIds.HARDHAT]: '0xbc6da0fe9ad5f3b0d58160288917aa56653660e9',
+      [networkIds.RINKEBY]: '0xc89e05ad29531c2d8b5291546e1e064d42ff65c1',
+    },
+  },
+  USDT: {
+    symbol: 'USDT',
+    decimals: 18,
+    addresses: {
+      [networkIds.MAINNET]: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+      [networkIds.HARDHAT]: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+      [networkIds.RINKEBY]: '0x01547ef97f9140dbdf5ae50f06b77337b95cf4bb',
     },
   },
 };
