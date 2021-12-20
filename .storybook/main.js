f2746486c9bf2e683ba1bdbc48f7db40df1ab753
f1lander <filanderuclez@gmail.com>
2021-12-20 16:45:41 -0600

    reinstall modules

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,12 @@
 module.exports = {
-  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-create-react-app"],
-  "framework": "@storybook/react",
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+  ],
+  framework: '@storybook/react',
   core: {
-    builder: "webpack5"
-  }
+    builder: 'webpack5',
+  },
 };
