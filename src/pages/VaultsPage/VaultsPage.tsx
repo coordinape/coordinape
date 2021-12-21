926672e0b7529b3add4e2fb82d0a9350837ef591
Niraj Kamdar <nirajkamdar27@pm.me>
2021-12-21 12:46:12 +0530

    chore: make sure vaults is non-empty array before iterating

--- src/pages/VaultsPage/VaultsPage.tsx
+++ src/pages/VaultsPage/VaultsPage.tsx
@@ -298,7 +298,7 @@ const VaultsPage = () => {
         buttonText="Create a Vault"
         onButtonClick={() => setCreateOpen(true)}
       />
-      {vaults ? (
+      {vaults && vaults.length ? (
         vaults.map(vault => (
           <HasVaults
             key={vault.id}
