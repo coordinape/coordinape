b7e0be6c94314ee3bad616d5d7cc606be3b7d621
zashton <17747090+zashton@users.noreply.github.com>
2022-01-06 01:47:56 +0800

    add missing public prefix

--- hasura/migrations/staging/1640679052192_custom_views/down.sql
+++ hasura/migrations/staging/1640679052192_custom_views/down.sql
@@ -1,6 +1,6 @@
 
-DROP VIEW pending_gift_private IF EXISTS
+DROP VIEW public.pending_gift_private IF EXISTS
 
-DROP VIEW gift_private IF EXISTS
+DROP VIEW public.gift_private IF EXISTS
 
-DROP VIEW circle_private IF EXISTS
+DROP VIEW public.circle_private IF EXISTS
