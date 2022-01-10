9d3c659ba13227421333cc9b9b111ff3a5230809
f1lander <filanderuclez@gmail.com>
2022-01-10 13:46:15 -0600

    adds awardIcon

--- src/icons/AwardIcon.tsx
+++ src/icons/AwardIcon.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
+/* eslint-disable react/display-name */
+import * as React from 'react';
 
-import { SvgIcon } from '@material-ui/core';
+import { styled, SvgIconConfig } from 'stitches.config';
 
-export const AwardIcon = (props: any) => (
-  <SvgIcon {...props}>
-    <path
-      d="M12 15.4291C15.7871 15.4291 18.8571 12.3591 18.8571 8.57199C18.8571 4.78489 15.7871 1.71484 12 1.71484C8.21287 1.71484 5.14282 4.78489 5.14282 8.57199C5.14282 12.3591 8.21287 15.4291 12 15.4291Z"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path
-      d="M8.10175 14.5807L6.85718 22.2866L12 19.749L17.1429 22.2866L15.8983 14.5723"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-  </SvgIcon>
+import { IconProps } from 'types';
+
+export const AwardIcon = styled(
+  React.forwardRef<SVGSVGElement, IconProps>(
+    ({ color = 'currentColor', ...props }, forwardedRef) => {
+      return (
+        <svg {...props} ref={forwardedRef}>
+          <path
+            d="M12 15.4291C15.7871 15.4291 18.8571 12.3591 18.8571 8.57199C18.8571 4.78489 15.7871 1.71484 12 1.71484C8.21287 1.71484 5.14282 4.78489 5.14282 8.57199C5.14282 12.3591 8.21287 15.4291 12 15.4291Z"
+            fill="none"
+            stroke={color}
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M8.10175 14.5807L6.85718 22.2866L12 19.749L17.1429 22.2866L15.8983 14.5723"
+            fill="none"
+            stroke={color}
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      );
+    }
+  ),
+  SvgIconConfig
 );
+
+export default AwardIcon;
