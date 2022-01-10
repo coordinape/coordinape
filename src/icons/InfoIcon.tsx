cbd10629d89fd3f83d1d97582c42a22fbc702d44
f1lander <filanderuclez@gmail.com>
2022-01-10 13:22:48 -0600

    adds InfoIcon

--- src/icons/InfoIcon.tsx
+++ src/icons/InfoIcon.tsx
@@ -1,28 +1,42 @@
-import { SvgIcon } from '@material-ui/core';
+/* eslint-disable react/display-name */
+import * as React from 'react';
 
-export const InfoIcon = (props: any) => (
-  <SvgIcon {...props}>
-    <path
-      d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
-      stroke="currentColor"
-      fill="none"
-      strokeWidth="2.25"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path
-      d="M12 16V12"
-      stroke="currentColor"
-      strokeWidth="2.25"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path
-      d="M12 8H12.01"
-      stroke="currentColor"
-      strokeWidth="2.25"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-  </SvgIcon>
+import { styled, SvgIconConfig } from 'stitches.config';
+
+import { IconProps } from 'types';
+
+export const InfoIcon = styled(
+  React.forwardRef<SVGSVGElement, IconProps>(
+    ({ color = 'currentColor', ...props }, forwardedRef) => {
+      return (
+        <svg {...props} ref={forwardedRef}>
+          <path
+            d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+            stroke={color}
+            fill="none"
+            strokeWidth="2.25"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M12 16V12"
+            stroke={color}
+            strokeWidth="2.25"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M12 8H12.01"
+            stroke={color}
+            strokeWidth="2.25"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      );
+    }
+  ),
+  SvgIconConfig
 );
+
+export default InfoIcon;
