d0bc9277424986c70d5438489528b048b4f7247d
Edax Filander Ucles Hernandez <filanderuclez@gmail.com>
2022-01-19 18:36:50 -0600

    adds stitches styles

---
+++ src/ui/Text/Text.tsx
@@ -0,0 +1,11 @@
+import { styled } from '../../stitches.config';
+
+export const Text = styled('span', {
+  lineHeight: '1',
+  margin: '0',
+  fontWeight: 400,
+  fontVariantNumeric: 'tabular-nums',
+  display: 'block',
+});
+
+export default Text;
