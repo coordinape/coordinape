d377db6227b1231145dccda26c0bba3c0a1fce44
Lawrence Wang <l@lw.io>
2022-01-25 19:45:29 +0800

    avoid use of ethers.getDefaultProvider
    avoid using the default API keys and fallback providers

--- src/config/env.ts
+++ src/config/env.ts
@@ -36,10 +36,6 @@ export const REACT_APP_HASURA_URL = getEnvValue(
   'https://missing-hasura-url.edu'
 );
 
-if (!IN_PRODUCTION) {
-  console.log(`Using GraphQL API URL: ${REACT_APP_HASURA_URL}`); // eslint-disable-line no-console
-}
-
 // Unused in practice
 export const REACT_APP_FORTMATIC_API_KEY = 'unused'; // getEnvValue('REACT_APP_FORTMATIC_API_KEY', 'no-formatic-api-key');
 export const REACT_APP_PORTIS_DAPP_ID = 'unused'; // getEnvValue('REACT_APP_PORTIS_DAPP_ID', 'no-portis-api-key');
