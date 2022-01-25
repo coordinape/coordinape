67102d04422bf1a0da3a731c399c0e839ae68317
Lawrence Wang <l@lw.io>
2022-01-25 16:52:01 +0800

    small fixes from code review

--- scripts/generate-zeus.sh
+++ scripts/generate-zeus.sh
@@ -5,12 +5,12 @@ set -o allexport
 source .env
 set +o allexport
 
-export GENERATE_HEADER="authorization:generate-$NODE_HASURA_ADMIN_SECRET"
-export GEN_PATH=./src/lib/gql
+GENERATE_HEADER="authorization:generate-$NODE_HASURA_ADMIN_SECRET"
+GEN_PATH=./src/lib/gql
 
 # Generate the types for different users.
 
-export MV_PATH=$GEN_PATH/zeusHasuraAdmin
+MV_PATH=$GEN_PATH/zeusHasuraAdmin
 zeus $NODE_HASURA_URL $GEN_PATH --ts --rq -h x-hasura-admin-secret:$NODE_HASURA_ADMIN_SECRET
 test -d $GEN_PATH/zeus
 [[ -d "$MV_PATH" ]] && rm -r $MV_PATH
@@ -22,7 +22,7 @@ gsed -i 's,bigint"]:any,bigint"]:number,g' $MV_PATH/index.ts
 gsed -i 's,bigint"]:unknown,bigint"]:number,g' $MV_PATH/index.ts
 gsed -i 's,"'"$NODE_HASURA_URL"'",process.env.REACT_APP_HASURA_URL as string,g' $MV_PATH/reactQuery.ts
 
-export MV_PATH=$GEN_PATH/zeusUser
+MV_PATH=$GEN_PATH/zeusUser
 zeus $NODE_HASURA_URL $GEN_PATH --ts --rq -h $GENERATE_HEADER -h x-hasura-role:user
 test -d $GEN_PATH/zeus
 [[ -d "$MV_PATH" ]] && rm -r $MV_PATH
