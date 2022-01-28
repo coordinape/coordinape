1ab6fb1c120b3244e988f04b1182a268e1579fce
Edax Filander Ucles Hernandez <filanderuclez@gmail.com>
2022-01-28 08:35:10 -0600

    adds example of button story

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.[tj]s', '../src/**/*.stories.[tj]sx'],
   framework: '@storybook/react',
   core: {
     builder: 'webpack5',
---
+++ .storybook/theme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Coordinape Storybook',
+  brandUrl:
+    'https://media.discordapp.net/attachments/892069844156022876/935350552194347078/front.jpg?width=2354&height=280',
+  brandImage:
+    'https://media.discordapp.net/attachments/892069844156022876/935350552194347078/front.jpg?width=2354&height=280',
+});
