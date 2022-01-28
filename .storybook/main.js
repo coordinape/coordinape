a80c505cdf0a9e99114fc793ea8668adcaff4b85
Edax Filander Ucles Hernandez <filanderuclez@gmail.com>
2022-01-27 22:10:14 -0600

    remove preset craco addon from main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,8 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    'preset-create-react-app',
-  ],
   framework: '@storybook/react',
   core: {
     builder: 'webpack5',
   },
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 };
