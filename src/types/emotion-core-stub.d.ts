4ee5c7a688a15267f0e9f5babbc89bae518b0b35
Edax Filander Ucles Hernandez <filanderuclez@gmail.com>
2022-01-28 16:35:42 -0600

    adds comment to emotion type prop fix

---
+++ src/types/emotion-core-stub.d.ts
@@ -0,0 +1,6 @@
+/* 
+This file fixes css prop type definition clash, more info below
+
+https://gist.github.com/sseagull/e2e7fe58f7f236897d342e776bb549a2
+TODO: remove this when stitches updates this emotion dependency to avoid with storybook
+*/
