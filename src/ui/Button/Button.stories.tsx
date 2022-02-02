3fc1f1909aec15a2a651880f049a7eb8f6775f25
Edax Filander Ucles Hernandez <filanderuclez@gmail.com>
2022-02-02 12:32:52 -0600

    adds storybook addon designs

--- src/ui/Button/Button.stories.tsx
+++ src/ui/Button/Button.stories.tsx
@@ -1,10 +1,12 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { withDesign } from 'storybook-addon-designs';
 
 import { Button, ButtonStory } from './Button';
 
 export default {
   title: 'Design System/Components/Button',
   component: Button,
+  decorators: [withDesign],
 } as ComponentMeta<typeof ButtonStory>;
 
 const Template: ComponentStory<typeof ButtonStory> = args => (
@@ -17,4 +19,11 @@ SingleButton.args = {
   color: 'red',
   size: 'medium',
   children: 'Edit',
+};
+
+SingleButton.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/wXF7xGVv1j2SqwWfgbamS8/Coordinape-Design-v8?node-id=338%3A94',
+  },
 };
