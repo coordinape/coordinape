7b065c1eb026fefdacb14d4acfee7bd3aa1fdf30
Edax Filander Ucles Hernandez <filanderuclez@gmail.com>
2022-02-07 23:42:09 -0600

    adds script to retrieve style colors from figma

--- src/ui/scripts/figma-colors.ts
+++ src/ui/scripts/figma-colors.ts
@@ -2,29 +2,90 @@ import dotenv from 'dotenv';
 import * as Figma from 'figma-api';
 dotenv.config();
 
-// const FILL = 'FILL';
+//#region constants
+const FIGMA_FILE_KEY = 'L4oZwYVUdpgacZtwipGaYe';
+const COLORS_DOCUMENT = '3542:11605';
+const FILL = 'FILL';
+const RECTANGLE = 'RECTANGLE';
+//#endregion constants
 
-// const getStylesFromFile = (data: any) => {
-//   const { styles } = data;
-//   return Object.entries(styles)
-//     .filter(([, entry ]) => { const { styleType } = entry; return styleType === FILL; })
-//     .map(([id, { name }]) => ({ name, id }));
-// };
-// this file is the Ape App 1.5
-const COORDINAPE_FIGMA_FILE_KEY = 'L4oZwYVUdpgacZtwipGaYe';
+//#region Types
+type Styles = {
+  name: string;
+  id: string;
+};
+
+type ColorNode = {
+  name: string;
+  color: unknown;
+};
+//#endregion Types
+
+//#region Utils functions
+const rgbToHex = (r: number, g: number, b: number) => {
+  const color =
+    '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+
+  if (color.length > 7) {
+    return color.slice(0, 7);
+  }
+  return color;
+};
+
+const hasChildren = (node: any) => node != null && !('children' in node);
+const isRectangle = (node: any) => node != null && node.type === RECTANGLE;
+
+const findStyleInTree = (root: any, styleId: any) => {
+  if (hasChildren(root)) {
+    return isRectangle(root) && root.styles && root.styles.fill === styleId
+      ? root
+      : undefined;
+  } else {
+    return root.children
+      .map((item: any) => findStyleInTree(item, styleId))
+      .reduce(
+        (accumulator: any, current: any) =>
+          accumulator != null ? accumulator : current,
+        undefined
+      );
+  }
+};
+
+const mapStyleToNode = (file: any, styles: Styles[]) => {
+  const colorNodes: ColorNode[] = [];
+
+  styles.forEach(({ name, id }) => {
+    const node = findStyleInTree(file.document, id);
+    const color =
+      isRectangle(node) && node.fills[0] ? node.fills[0].color : undefined;
+
+    if (color) {
+      const { r, g, b } = color;
+      colorNodes.push({ name, color: rgbToHex(r * 256, g * 256, b * 256) });
+    }
+  });
+
+  return colorNodes;
+};
+//#endregion Utils functions
+
 async function main() {
   const api = new Figma.Api({
     personalAccessToken: process.env.FIGMA_ACCESS_TOKEN || 'token',
   });
 
-  // const { meta } = await api.getFileStyles(COORDINAPE_FIGMA_FILE_KEY);
-  // // eslint-disable-next-line no-console
-  // //const style = meta?.styles.filter(item => item.style_type === FILL)[0];
+  const file = await api.getFile(FIGMA_FILE_KEY, {
+    ids: [COLORS_DOCUMENT],
+  });
 
-  const file = await api.getFile(COORDINAPE_FIGMA_FILE_KEY);
+  const styles: Styles[] = Object.entries(file.styles)
+    .filter(([, { styleType }]) => styleType === FILL)
+    .map(([id, { name }]) => ({ name: name.replace(/\s/g, ''), id }))
+    .sort((a, b) => a.name.localeCompare(b.name));
 
+  const result = mapStyleToNode(file, styles);
   // eslint-disable-next-line no-console
-  console.log(JSON.stringify(file));
+  console.log(result, result.length);
 }
 
 (async function () {
