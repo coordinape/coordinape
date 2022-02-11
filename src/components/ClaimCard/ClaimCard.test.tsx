a226a12c7a53312b804a61ed8057d9e737106a05
Edax Filander Ucles Hernandez <filanderuclez@gmail.com>
2022-02-11 10:02:00 -0600

    adds new expect to the test

--- src/components/ClaimCard/ClaimCard.test.tsx
+++ src/components/ClaimCard/ClaimCard.test.tsx
@@ -20,11 +20,11 @@ describe('ClaimCard', () => {
 
   it('should render properly', () => {
     const { getByText, getByTestId } = renderComponent(props);
-
+    const actionButton = getByTestId('action-button-1');
     expect(getByTestId('action-button-1')).toBeInTheDocument();
     expect(getByTestId('action-button-2')).toBeInTheDocument();
     expect(getByTestId('arrow-diagonal-icon')).toBeInTheDocument();
-
+    expect(actionButton.textContent).toEqual(`Claim ${props.claimAmount} USDC`);
     expect(getByText(props.epochInfo[0].title)).toBeInTheDocument();
     expect(getByText(props.epochInfo[0].subTitle)).toBeInTheDocument();
     expect(getByText(props.epochInfo[0].giveInfo)).toBeInTheDocument();
