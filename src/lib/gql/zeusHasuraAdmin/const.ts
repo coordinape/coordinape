975b2f405ee23c54cf5eb11ea370ffd72609f316
zashton <17747090+zashton@users.noreply.github.com>
2022-03-01 18:49:33 +0800

    User Logout hasura (#576)
    * User Logout hasura
    
    * yarn generate
    
    remove graphql error catch

--- src/lib/gql/zeusHasuraAdmin/const.ts
+++ src/lib/gql/zeusHasuraAdmin/const.ts
@@ -17494,6 +17494,10 @@ export const ReturnTypes: Record<string, any> = {
     id: 'Float',
     user_id: 'Float',
   },
+  logout_response: {
+    id: 'Int',
+    profile: 'profiles',
+  },
   mutation_root: {
     createUser: 'createUserResponse',
     create_circle: 'create_circle_response',
@@ -17566,6 +17570,7 @@ export const ReturnTypes: Record<string, any> = {
     insert_users_one: 'users',
     insert_vouches: 'vouches_mutation_response',
     insert_vouches_one: 'vouches',
+    logout_user: 'logout_response',
     update_burns: 'burns_mutation_response',
     update_burns_by_pk: 'burns',
     update_circle_integrations: 'circle_integrations_mutation_response',
