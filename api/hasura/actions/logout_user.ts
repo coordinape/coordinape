c65a5bb1187fe2fbdc28a7d48d2a6ca3c09e547e
zashton <17747090+zashton@users.noreply.github.com>
2022-03-01 18:49:33 +0800

    User Logout hasura (#576)
    * User Logout hasura
    
    * yarn generate
    
    remove graphql error catch

---
+++ api/hasura/actions/logout_user.ts
@@ -0,0 +1,42 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { z } from 'zod';
+
+import { gql } from '../../../api-lib/Gql';
+import { verifyHasuraRequestMiddleware } from '../../../api-lib/validate';
+import { HasuraUserSessionVariables } from '../../../src/lib/zod';
+
+async function handler(request: VercelRequest, response: VercelResponse) {
+  try {
+    const { hasuraProfileId } = HasuraUserSessionVariables.parse(
+      request.body.session_variables
+    );
+    await gql.q('mutation')({
+      delete_personal_access_tokens: [
+        {
+          where: {
+            tokenable_id: { _eq: hasuraProfileId },
+            tokenable_type: { _eq: 'App\\Models\\Profile' },
+          },
+        },
+        {
+          affected_rows: true,
+        },
+      ],
+    });
+    return response.status(200).json({ id: hasuraProfileId });
+  } catch (err) {
+    if (err instanceof z.ZodError) {
+      return response.status(422).json({
+        extensions: err.issues,
+        message: 'Invalid input',
+        code: '422',
+      });
+    }
+    return response.status(401).json({
+      message: 'Unexpected error',
+      code: '401',
+    });
+  }
+}
+
+export default verifyHasuraRequestMiddleware(handler);
