f3eb04f362623c48d8b3c808d2cb5c75a4cd11ff
Tony Olendo <teeolendo@gmail.com>
2022-03-02 17:03:07 -0400

    Updated Migrations to use timestamp instead of timestamptz

--- hasura/migrations/default/1646251832285_create_table_public_distributions/up.sql
+++ hasura/migrations/default/1646251832285_create_table_public_distributions/up.sql
@@ -1,1 +1,16 @@
-CREATE TABLE "public"."distributions" ("id" bigint NOT NULL, "epoch_id" bigint NOT NULL, "merkle_root" varchar NOT NULL, "total_amount" varchar NOT NULL, "vault_id" bigint NOT NULL, "created_by" bigint NOT NULL, "created_at" Timestamp NOT NULL DEFAULT now(), "updated_at" timestamptz NOT NULL DEFAULT now(), PRIMARY KEY ("id") , FOREIGN KEY ("epoch_id") REFERENCES "public"."epoches"("id") ON UPDATE no action ON DELETE no action, FOREIGN KEY ("created_by") REFERENCES "public"."users"("id") ON UPDATE no action ON DELETE no action, FOREIGN KEY ("vault_id") REFERENCES "public"."vaults"("id") ON UPDATE no action ON DELETE no action, UNIQUE ("id"));
+CREATE TABLE "public"."distributions"
+(
+  "id" bigint NOT NULL,
+  "epoch_id" bigint NOT NULL,
+  "merkle_root" varchar NOT NULL,
+  "total_amount" varchar NOT NULL,
+  "vault_id" bigint NOT NULL,
+  "created_by" bigint NOT NULL,
+  "created_at" Timestamp NOT NULL DEFAULT now(),
+  "updated_at" timestamp NOT NULL DEFAULT now(),
+  PRIMARY KEY ("id") ,
+  FOREIGN KEY ("epoch_id") REFERENCES "public"."epoches"("id") ON UPDATE no action ON DELETE no action,
+  FOREIGN KEY ("created_by") REFERENCES "public"."users"("id") ON UPDATE no action ON DELETE no action,
+  FOREIGN KEY ("vault_id") REFERENCES "public"."vaults"("id") ON UPDATE no action ON DELETE no action,
+  UNIQUE ("id")
+);
