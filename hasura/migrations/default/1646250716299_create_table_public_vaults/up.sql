9b6ea39fbabf7f88c0a3a835f9e02a7271454c16
Tony Olendo <teeolendo@gmail.com>
2022-03-02 22:55:49 -0400

    Migrations for Vaults related tables (#583)
    * Migrations for Vaults related tables
    
    * Updated Migrations to use timestamp instead of timestamptz
    
    * Update hasura/migrations/default/1646250716299_create_table_public_vaults/up.sql
    
    Co-authored-by: Lawrence Wang <l@lw.io>
    
    * Update hasura/migrations/default/1646251192439_create_table_public_vault_transactions/up.sql
    
    Co-authored-by: Lawrence Wang <l@lw.io>
    
    * Update hasura/migrations/default/1646251192439_create_table_public_vault_transactions/up.sql
    
    Co-authored-by: Lawrence Wang <l@lw.io>
    
    * Update up.sql
    
    * Update Vault Transactions
    
    Removed date_type, posneg and active_users, and made value a numeric field
    
    * Added description column
    
    Co-authored-by: Lawrence Wang <l@lw.io>

---
+++ hasura/migrations/default/1646250716299_create_table_public_vaults/up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE "public"."vaults"
+(
+  "id" bigserial NOT NULL,
+  "token_address" varchar,
+  "simple_token_address" varchar,
+  "type" integer NOT NULL,
+  "org_id" bigint NOT NULL,
+  "decimals" integer NOT NULL,
+  "owner_id" bigint NOT NULL,
+  "created_by" bigint NOT NULL,
+  "created_at" timestamp NOT NULL DEFAULT now(),
+  "updated_at" timestamp NOT NULL DEFAULT now(),
+  PRIMARY KEY ("id") ,
+  FOREIGN KEY ("org_id") REFERENCES "public"."protocols"("id") ON UPDATE no action ON DELETE no action,
+  FOREIGN KEY ("vault_owner") REFERENCES "public"."users"("id") ON UPDATE no action ON DELETE no action,
+  UNIQUE ("id")
+);
