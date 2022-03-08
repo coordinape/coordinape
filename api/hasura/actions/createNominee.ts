14a2f754c4a7a879d224e47f98e08dad7eca247b
zashton <17747090+zashton@users.noreply.github.com>
2022-03-08 19:30:44 +0800

    add ErrorResponse

--- api/hasura/actions/createNominee.ts
+++ api/hasura/actions/createNominee.ts
@@ -1,16 +1,17 @@
-import assert from 'assert';
-
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { z } from 'zod';
 
 import { getUserFromAddress } from '../../../api-lib/findUser';
+import {
+  ErrorResponse,
+  ErrorResponseWithStatusCode,
+} from '../../../api-lib/HttpError';
 import {
   insertNominee,
   getUserFromProfileIdWithCircle,
   getNomineeFromAddress,
 } from '../../../api-lib/nominees';
 import { verifyHasuraRequestMiddleware } from '../../../api-lib/validate';
-import { GraphQLError } from '../../../src/lib/gql/zeusHasuraAdmin';
 import {
   createNomineeSchemaInput,
   composeHasuraActionRequestBodyWithSession,
@@ -35,7 +36,14 @@ async function handler(req: VercelRequest, res: VercelResponse) {
       profileId,
       circle_id
     );
-    assert(nominator);
+    if (!nominator) {
+      return ErrorResponseWithStatusCode(
+        res,
+        { message: 'Nominator does not belong to this circle' },
+        422
+      );
+    }
+
     const {
       id: nominated_by_user_id,
       circle: { nomination_days_limit, min_vouches: vouches_required },
@@ -44,19 +52,21 @@ async function handler(req: VercelRequest, res: VercelResponse) {
     // check if address already exists in the circle
     const user = await getUserFromAddress(address, circle_id);
     if (user) {
-      return res.status(422).json({
-        message: 'User with address already exists in the circle',
-        code: '422',
-      });
+      return ErrorResponseWithStatusCode(
+        res,
+        { message: 'User with address already exists in the circle' },
+        422
+      );
     }
 
     // check if user exists in nominee table same circle and not ended
     const checkAddressExists = await getNomineeFromAddress(address, circle_id);
     if (checkAddressExists) {
-      return res.status(422).json({
-        message: 'User with address already exists as a nominee',
-        code: '422',
-      });
+      return ErrorResponseWithStatusCode(
+        res,
+        { message: 'User with address already exists as a nominee' },
+        422
+      );
     }
 
     // add an event trigger to check if vouches are enough and insert an uesr/profile
@@ -72,22 +82,13 @@ async function handler(req: VercelRequest, res: VercelResponse) {
     return res.status(200).json(nominee);
   } catch (err) {
     if (err instanceof z.ZodError) {
-      return res.status(422).json({
-        extensions: err.issues,
-        message: 'Invalid input',
-        code: '422',
-      });
-    } else if (err instanceof GraphQLError) {
-      return res.status(422).json({
-        code: '422',
-        message: 'GQL Query Error',
-        extensions: err.response.errors,
-      });
+      return ErrorResponseWithStatusCode(
+        res,
+        { message: 'Invalid input' },
+        422
+      );
     }
-    return res.status(401).json({
-      message: 'User does not belong to this circle',
-      code: '401',
-    });
+    return ErrorResponse(res, err);
   }
 }
 
