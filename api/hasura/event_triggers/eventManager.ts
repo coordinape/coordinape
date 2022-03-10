0a0688c4ccfee541ba71d4966a0bdb2da4cbb6e0
Kevin Siegler <17910833+topocount@users.noreply.github.com>
2022-03-10 01:41:32 -0600

    fix error from rebase

--- api/hasura/event_triggers/eventManager.ts
+++ api/hasura/event_triggers/eventManager.ts
@@ -11,7 +11,7 @@ import refundGiveTelegram from '../../../api-lib/event_triggers/refundGiveTelegr
 import refundPendingGift from '../../../api-lib/event_triggers/refundPendingGift';
 import { EventTriggerPayload } from '../../../api-lib/types';
 import { verifyHasuraRequestMiddleware } from '../../../api-lib/validate';
-import { GraphQLTypes } from '../../../src/lib/gql/zeusHasuraAdmin';
+import { GraphQLTypes } from '../../../src/lib/gql/__generated__/zeusAdmin';
 
 type HandlerDict = { [handlerName: string]: VercelApiHandler };
 
