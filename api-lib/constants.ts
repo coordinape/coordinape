9154d0925f159419c325801c26c050afcc90fdfd
zashton <17747090+zashton@users.noreply.github.com>
2022-03-14 15:48:26 +0800

    initial create epoch
    validation and checks

--- api-lib/constants.ts
+++ api-lib/constants.ts
@@ -1,3 +1,8 @@
 export const DISCORD_BOT_NAME = 'Caesar The Coordinape Bot';
 export const DISCORD_BOT_AVATAR_URL =
   'https://coordinape-prod.s3.amazonaws.com/assets/static/20220131-coordinape-pfp-ape.jpg';
+
+export const EPOCH_REPEAT = {
+  WEEKLY: 1,
+  MONTHLY: 2,
+};
---
+++ api/hasura/actions/createEpoch.ts
@@ -0,0 +1,95 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { z } from 'zod';
+
+import { authCircleAdminMiddleware } from '../../../api-lib/circleAdmin';
+import { EPOCH_REPEAT } from '../../../api-lib/constants';
+import { adminClient } from '../../../api-lib/gql/adminClient';
+import {
+  errorResponse,
+  errorResponseWithStatusCode,
+} from '../../../api-lib/HttpError';
+import {
+  createEpochInput,
+  composeHasuraActionRequestBody,
+} from '../../../src/lib/zod';
+
+async function handler(request: VercelRequest, response: VercelResponse) {
+  try {
+    const {
+      input: { payload: input },
+    } = composeHasuraActionRequestBody(createEpochInput).parse(request.body);
+    const { circle_id, repeat, start_date, days } = input;
+    //let repeat_day_of_month = 0;
+    if (repeat > 0) {
+      const { epochs } = await adminClient.query({
+        epochs: [
+          {
+            limit: 1,
+            where: {
+              ended: { _eq: false },
+              circle_id: { _eq: circle_id },
+              repeat: { _gte: EPOCH_REPEAT.WEEKLY },
+            },
+          },
+          {
+            id: true,
+          },
+        ],
+      });
+      if (epochs.length) {
+        errorResponseWithStatusCode(
+          response,
+          { message: 'You cannot have more than one repeating active epoch.' },
+          422
+        );
+        return;
+      }
+      // if (repeat === 2) {
+      //   repeat_day_of_month = start_date.day;
+      // }
+    }
+
+    const end_date = start_date.plus({ days: days });
+    const { epochs } = await adminClient.query({
+      epochs: [
+        {
+          limit: 1,
+          where: {
+            start_date: { _lte: end_date },
+            circle_id: { _eq: circle_id },
+            end_date: { _gt: start_date },
+          },
+        },
+        {
+          id: true,
+          start_date: true,
+          end_date: true,
+        },
+      ],
+    });
+    if (epochs.length) {
+      const epoch = epochs.pop();
+      errorResponseWithStatusCode(
+        response,
+        {
+          message:
+            `This epoch overlaps with an existing epoch that occurs between ` +
+            `${new Date(epoch?.start_date).toLocaleString()} and ` +
+            `${new Date(epoch?.end_date).toLocaleString()}. ` +
+            `Please adjust epoch settings to avoid overlapping with existing epochs`,
+        },
+        422
+      );
+      return;
+    }
+  } catch (err) {
+    if (err instanceof z.ZodError) {
+      errorResponseWithStatusCode(response, { message: 'Invalid input' }, 422);
+      return;
+    }
+    errorResponse(response, err);
+    return;
+  }
+}
+
+export default authCircleAdminMiddleware(handler);
