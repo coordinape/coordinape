a44d111b0fa6efd2f9f6a76eb024e7249382b6fc
Kevin Siegler <17910833+topocount@users.noreply.github.com>
2022-03-15 15:48:04 -0500

    Migrate Daily Update Notifications to Hasura (#615)
    This cron job pulls all the data it needs in one GQL query then sends
    messages to all circles with an active epoch and a configured
    notification channel.
    
    I intentionally wrote this monolithically since it's a pretty unique
    query and marshalling sequence as far as our codebase goes. If folks think
    any of this code should be split out, I'd be happy to do so.
    
    test plan:
    
    - check if any circles have an active epoch. This sql should help
    select * from epoches
    where start_date < 'now()'
    and end_date > 'now()';
    
    - if no epochs are active, adjust the dates of 1 or more existing epochs
      (for different circles) or try your luck by re-seeding the DB
    - configure your test discord and/or telegram channel for the relevant
      circle(s)
    - adjust the crontab config so the task runs minutely (* * * * *)
    - collect a message and verify that the GIVE tokens, allocations totals
      and users who gifted in the last day match the database entries and
      totals.
    - if no users have created pending gift entries in the last 24 hours you
      should touch the `updated` at field on some and verify the sender's
      name shows up ONCE in the new allocations today list.

--- api-lib/gql/__generated__/zeus/const.ts
+++ api-lib/gql/__generated__/zeus/const.ts
@@ -4975,6 +4975,70 @@ export const AllTypesProps: Record<string, any> = {
         required: false,
       },
     },
+    epoch_pending_token_gifts: {
+      distinct_on: {
+        type: 'pending_token_gifts_select_column',
+        array: true,
+        arrayRequired: false,
+        required: true,
+      },
+      limit: {
+        type: 'Int',
+        array: false,
+        arrayRequired: false,
+        required: false,
+      },
+      offset: {
+        type: 'Int',
+        array: false,
+        arrayRequired: false,
+        required: false,
+      },
+      order_by: {
+        type: 'pending_token_gifts_order_by',
+        array: true,
+        arrayRequired: false,
+        required: true,
+      },
+      where: {
+        type: 'pending_token_gifts_bool_exp',
+        array: false,
+        arrayRequired: false,
+        required: false,
+      },
+    },
+    epoch_pending_token_gifts_aggregate: {
+      distinct_on: {
+        type: 'pending_token_gifts_select_column',
+        array: true,
+        arrayRequired: false,
+        required: true,
+      },
+      limit: {
+        type: 'Int',
+        array: false,
+        arrayRequired: false,
+        required: false,
+      },
+      offset: {
+        type: 'Int',
+        array: false,
+        arrayRequired: false,
+        required: false,
+      },
+      order_by: {
+        type: 'pending_token_gifts_order_by',
+        array: true,
+        arrayRequired: false,
+        required: true,
+      },
+      where: {
+        type: 'pending_token_gifts_bool_exp',
+        array: false,
+        arrayRequired: false,
+        required: false,
+      },
+    },
   },
   epochs_aggregate_fields: {
     count: {
@@ -5185,6 +5249,12 @@ export const AllTypesProps: Record<string, any> = {
       arrayRequired: false,
       required: false,
     },
+    epoch_pending_token_gifts: {
+      type: 'pending_token_gifts_bool_exp',
+      array: false,
+      arrayRequired: false,
+      required: false,
+    },
     grant: {
       type: 'numeric_comparison_exp',
       array: false,
@@ -5342,6 +5412,12 @@ export const AllTypesProps: Record<string, any> = {
     },
     ended: {
       type: 'Boolean',
+      array: false,
+      arrayRequired: false,
+      required: false,
+    },
+    epoch_pending_token_gifts: {
+      type: 'pending_token_gifts_arr_rel_insert_input',
       array: false,
       arrayRequired: false,
       required: false,
@@ -5670,6 +5746,12 @@ export const AllTypesProps: Record<string, any> = {
     },
     ended: {
       type: 'order_by',
+      array: false,
+      arrayRequired: false,
+      required: false,
+    },
+    epoch_pending_token_gifts_aggregate: {
+      type: 'pending_token_gifts_aggregate_order_by',
       array: false,
       arrayRequired: false,
       required: false,
@@ -19616,6 +19698,8 @@ export const ReturnTypes: Record<string, any> = {
     days: 'Int',
     end_date: 'timestamptz',
     ended: 'Boolean',
+    epoch_pending_token_gifts: 'pending_token_gifts',
+    epoch_pending_token_gifts_aggregate: 'pending_token_gifts_aggregate',
     grant: 'numeric',
     id: 'bigint',
     notified_before_end: 'timestamp',
