0d4928f79aa091d1fdbdbffebdd933fb1b00f7c4
Lawrence Wang <l@lw.io>
2022-03-16 15:36:39 -0700

    Circle ID generator (#626)
    * remove a bunch of unused utils
    * add encode- & decodeCircleId
    * add prefix & version to encoded circle ids

---
+++ src/lib/vaults/index.test.ts
@@ -0,0 +1,18 @@
+import { utils } from 'ethers';
+
+import { encodeCircleId, decodeCircleId } from './index';
+
+test('encode & decode circle id', () => {
+  const encoded = encodeCircleId(777);
+  const expected =
+    '0x636972636c652d312d3737370000000000000000000000000000000000000000';
+  expect(encoded).toEqual(expected);
+  expect(decodeCircleId(encoded)).toEqual(777);
+  expect(encodeCircleId(decodeCircleId(encoded))).toEqual(encoded);
+});
+
+test('throw if prefix not found', () => {
+  expect(() => {
+    decodeCircleId(utils.formatBytes32String('whatever'));
+  }).toThrowError('Could not decode circle ID');
+});
