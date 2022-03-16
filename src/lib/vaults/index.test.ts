1ce8f9f544a400194c7207a3b2b84f24d711f082
Lawrence Wang <l@lw.io>
2022-03-16 15:04:32 -0700

    add prefix & version to encoded circle ids

--- src/lib/vaults/index.test.ts
+++ src/lib/vaults/index.test.ts
@@ -1,12 +1,20 @@
+import { utils } from 'ethers';
+
 import { encodeCircleId, decodeCircleId } from './index';
 
 // this is pretty trivial now but will become more complex when we add version
 // prefixes, etc.
-test('encoding & decoding circle ids', () => {
+test('encode & decode circle id', () => {
   const encoded = encodeCircleId(777);
   const expected =
-    '0x3737370000000000000000000000000000000000000000000000000000000000';
+    '0x636972636c652d312d3737370000000000000000000000000000000000000000';
   expect(encoded).toEqual(expected);
   expect(decodeCircleId(encoded)).toEqual(777);
   expect(encodeCircleId(decodeCircleId(encoded))).toEqual(encoded);
+});
+
+test('throw if prefix not found', () => {
+  expect(() => {
+    decodeCircleId(utils.formatBytes32String('whatever'));
+  }).toThrowError('Could not decode circle ID');
 });
--- src/lib/vaults/index.ts
+++ src/lib/vaults/index.ts
@@ -1,9 +1,23 @@
 import { utils } from 'ethers';
 
+const PREFIX = 'circle';
+const VERSION = '1';
+const DELIMITER = '-';
+
 export const encodeCircleId = (circleId: number) => {
-  return utils.formatBytes32String(circleId.toString());
+  const label = `${PREFIX}${DELIMITER}${VERSION}${DELIMITER}${circleId}`;
+  return utils.formatBytes32String(label);
 };
 
 export const decodeCircleId = (value: string) => {
-  return Number(utils.parseBytes32String(value));
+  const label = utils.parseBytes32String(value);
+
+  // version is not used yet
+  const [prefix, , id] = label.split(DELIMITER);
+
+  if (prefix !== PREFIX) {
+    throw new Error('Could not decode circle ID');
+  }
+
+  return Number(id);
 };
