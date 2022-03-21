a23b254c4eb1d9004acdac5b6706bfd0d78367ce
j-yw <32629001+j-yw@users.noreply.github.com>
2022-03-21 12:21:49 +0800

    add tooltip info to admin user list

--- src/pages/AdminPage/AdminPage.tsx
+++ src/pages/AdminPage/AdminPage.tsx
@@ -178,6 +178,9 @@ const useStyles = makeStyles(theme => ({
       color: theme.colors.text,
     },
   },
+  tooltipWrapper: {
+    marginTop: '6px',
+  },
 }));
 
 const TableLink = styled(Link, {
@@ -366,6 +369,27 @@ const AdminPage = ({ legacy }: { legacy?: boolean }) => {
                   onClick={getToProfile({ address: u.address })}
                 />
                 <span>{u.name}</span>
+                <span>
+                  {u.role === USER_ROLE_COORDINAPE ? (
+                    <div className={classes.tooltipWrapper}>
+                      <ApeInfoTooltip>
+                        <p>Why is Coordinape in your circle?</p>
+                        <p>
+                          We’re experimenting with the gift circle mechanism as
+                          our revenue model. By default, Coordinape appears in
+                          your circle and any user can alllocate to Coordinape.
+                          To remove the Coordinape user click “disable” on the
+                          right side of this row.
+                        </p>
+                        <a href="https://coordinape.notion.site/Why-is-Coordinape-in-my-Circle-fd17133a82ef4cbf84d4738311fb557a">
+                          Let us know what you think
+                        </a>
+                      </ApeInfoTooltip>
+                    </div>
+                  ) : (
+                    ''
+                  )}
+                </span>
               </div>
             );
           },
