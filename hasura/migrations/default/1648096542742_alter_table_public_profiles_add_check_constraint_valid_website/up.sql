ae61d2050e80331facb4bccbba9fe4aa4ad3fefe
zashton <17747090+zashton@users.noreply.github.com>
2022-03-25 11:47:15 +0800

    update script to fix existing data

--- hasura/migrations/default/1648096542742_alter_table_public_profiles_add_check_constraint_valid_website/up.sql
+++ hasura/migrations/default/1648096542742_alter_table_public_profiles_add_check_constraint_valid_website/up.sql
@@ -1,1 +1,3 @@
+update "public"."profiles" set website = CONCAT('https://',website) where website not ilike 'https://%' and website not ilike 'http://%' and website is not null;
+
 alter table "public"."profiles" add constraint "valid_website" check (website::text ~* 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,255}\.[a-z]{2,9}\y([-a-zA-Z0-9@:%_\+.,~#?!&>//=]*)$'::text);
