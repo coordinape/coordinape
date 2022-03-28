a37f8f0cd6dd5682e9a63b56e7b11753fc139f12
CryptoGraffe <graffe@graffe.co>
2022-03-27 21:25:26 -0700

    adding cleanup of the three emoji yat urls

--- hasura/migrations/default/1648096542742_alter_table_public_profiles_add_check_constraint_valid_website/up.sql
+++ hasura/migrations/default/1648096542742_alter_table_public_profiles_add_check_constraint_valid_website/up.sql
@@ -1,3 +1,4 @@
 update "public"."profiles" set website = CONCAT('https://',website) where website not ilike 'https://%' and website not ilike 'http://%' and website is not null;
-
-alter table "public"."profiles" add constraint "valid_website" check (website::text ~* 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,255}\.[a-z]{2,9}\y([-a-zA-Z0-9@:%_\+.,~#?!&>//=]*)$'::text);
+-- update these three specific users, making sure they have these y.at addresses
+update "public"."profiles" set website=null where id in(3117,7056,566) AND website ilike 'https://y.at%';
+alter table "public"."profiles" add constraint "valid_website" check (website::text ~* 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,255}\.[a-z]{2,9}\y([-a-zA-Z0-9@:%_\+.,~#?!&>//=]*)$'::text);
