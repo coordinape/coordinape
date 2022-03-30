51b9685aa5801d9a692299d9835826488229c5da
Lawrence Wang <l@lw.io>
2022-03-30 15:36:14 -0700

    add a test for the circles page
    also modify useConnectedAddress to make it work with the test env

--- src/hooks/useConnectedAddress.ts
+++ src/hooks/useConnectedAddress.ts
@@ -1,7 +1,16 @@
+import { useEffect, useState } from 'react';
+
 import { useWeb3React } from '@web3-react/core';
 
 export default function useConnectedAddress() {
-  const { account } = useWeb3React();
+  const { account, library } = useWeb3React();
 
-  return account ? account : undefined;
+  // fallback for certain providers that don't set account directly
+  const [signerAddress, setSignerAddress] = useState();
+  useEffect(() => {
+    if (account) return;
+    library?.getSigner().getAddress().then(setSignerAddress);
+  }, [library]);
+
+  return account || signerAddress || undefined;
 }
