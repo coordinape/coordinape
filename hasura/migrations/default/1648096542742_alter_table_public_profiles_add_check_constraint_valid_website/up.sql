54d9666c8e77394e86af32dd1b56eebed703fdb0
CryptoGraffe <graffe@graffe.co>
2022-03-30 23:33:08 -0700

    improving migration to migrate y.ats

--- hasura/migrations/default/1648096542742_alter_table_public_profiles_add_check_constraint_valid_website/up.sql
+++ hasura/migrations/default/1648096542742_alter_table_public_profiles_add_check_constraint_valid_website/up.sql
@@ -1,4 +1,8 @@
 update "public"."profiles" set website = CONCAT('https://',website) where website not ilike 'https://%' and website not ilike 'http://%' and website is not null;
 -- update these three specific users, making sure they have these y.at addresses
-update "public"."profiles" set website=null where id in(3117,7056,566) AND website ilike 'https://y.at%';
-alter table "public"."profiles" add constraint "valid_website" check (website::text ~* 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,255}\.[a-z]{2,9}\y([-a-zA-Z0-9@:%_\+.,~#?!&>//=]*)$'::text);
+update "public"."profiles" set website = RTRIM(website) where website is not null;
+update "public"."profiles" set website='https://y.at/mountain.crown.mountain' where id=7056 AND website ilike 'https://y.at%';
+update "public"."profiles" set website='https://y.at/fox.rocket.rainbow' where id=3117 AND website ilike 'https://y.at%';
+update "public"."profiles" set website='https://y.at/teacup.ocean.sparkles.half-moon' where id=566 AND website ilike 'https://y.at%';
+update "public"."profiles" set website=NULL where id=706;
+alter table "public"."profiles" add constraint "valid_website" check (website::text ~* 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,255}\.[a-z]{2,9}\y([-a-zA-Z0-9@:%_\+.,~#?!&>//=]*)$'::text OR website IS NULL);
