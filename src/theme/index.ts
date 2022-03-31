f51754b9889cdb6549c6c22d7b8fe9348d51cf6a
amrro <pushes.flutes-0j@icloud.com>
2022-03-31 23:03:09 +0200

    UPDATE the disabled color, Fix disabled Checkbox not showing
    Disabled Checkboxs wasn't showing, because the disabled color was white.
    Hence, white on white you can't see it.

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -204,7 +204,7 @@ const themeOptions = {
       active: 'rgba(255, 255, 255, 0.54)',
       hover: 'rgba(255, 255, 255, 0.04)',
       selected: 'rgba(255, 255, 255, 0.4)',
-      disabled: 'rgba(255, 255, 255, 0.26)',
+      disabled: 'rgba(0, 0, 0, 0.26)',
       disabledBackground: 'rgba(255, 255, 255, 0.12)',
       focus: 'rgba(255, 255, 255, 0.12)',
     },
