7902a7f63c5636062b2c8f93de2cbd3c01350a9e
amrro <pushes.flutes-0j@icloud.com>
2022-04-06 00:05:33 +0200

    Add new Flex component based on stitches.

---
+++ src/ui/Flex/Flex.tsx
@@ -0,0 +1,3 @@
+import { styled } from '../../stitches.config';
+
+export const Flex = styled('div', { display: 'flex' });
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,6 +2,7 @@ export * from './Box/Box';
 export * from './Button/Button';
 export * from './Divider/Divider';
 export * from './Form';
+export * from './Flex/Flex';
 export * from './Image/Image';
 export * from './Link/Link';
 export * from './IconButton/IconButton';
