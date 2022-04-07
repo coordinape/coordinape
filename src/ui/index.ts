17e72fa53dae4edf05343e799f634b246f741c92
sshmm <34943689+sshmm@users.noreply.github.com>
2022-04-07 22:51:52 +0200

    Bug/671/show errors nominate modal (#735)
    * Initial with mui Modal and makeStyles
    
    * update ui/Modal  to accept open prop optionaly
    
    * use ui/Modal instead of mui/Modal
    
    * NomianteModal with stitches
    
    Update NewNominationModal to use ui components like Modal and Form. Change the styling from make Styles to stitches. AddFormLabel in ui library to use as labels for input fields. update Box.tsx to have variants for input label compound and for gridand errors.Add variant nominate in TextArea.tsx to match other inputs text size and colors.
    
    * remove nominate variant and add inline styles
    
    * Align Name and Eth inputs
    
    * add margin after Nomination text
    
    * align input boxes on narrower widths
    
    * Use UseController with zod to validate the input

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,6 +2,7 @@ export * from './Box/Box';
 export * from './Button/Button';
 export * from './Divider/Divider';
 export * from './Form';
+export * from './FormLabel/FormLabel';
 export * from './Image/Image';
 export * from './Link/Link';
 export * from './IconButton/IconButton';
@@ -9,6 +10,7 @@ export * from './Modal';
 export * from './Panel/Panel';
 export * from './SvgIcon/SvgIcon';
 export * from './Text/Text';
+export * from './TextArea/TextArea';
 export * from './TextField/TextField';
 
 // icons
