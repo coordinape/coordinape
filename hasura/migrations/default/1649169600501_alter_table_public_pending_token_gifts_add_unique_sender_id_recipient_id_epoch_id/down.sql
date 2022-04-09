02d0647d0cf77668d4306e6da7acba530286dc78
zashton <17747090+zashton@users.noreply.github.com>
2022-04-09 12:04:33 +0800

    Update hasura/migrations/default/1649169600501_alter_table_public_pending_token_gifts_add_unique_sender_id_recipient_id_epoch_id/down.sql
    Co-authored-by: Kevin Siegler <17910833+topocount@users.noreply.github.com>

--- hasura/migrations/default/1649169600501_alter_table_public_pending_token_gifts_add_unique_sender_id_recipient_id_epoch_id/down.sql
+++ hasura/migrations/default/1649169600501_alter_table_public_pending_token_gifts_add_unique_sender_id_recipient_id_epoch_id/down.sql
@@ -1,1 +1,1 @@
-alter table "public"."pending_token_gifts" drop constraint "pending_token_gifts_sender_id_recipient_id_epoch_id_key";
+alter table "public"."pending_token_gifts" drop constraint if exists "pending_token_gifts_sender_id_recipient_id_epoch_id_key";
