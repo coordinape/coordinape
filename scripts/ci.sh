7bda92423ce52680a63481a858635f2f09cb64c2
Lawrence Wang <l@lw.io>
2022-04-13 20:57:38 -0700

    show hasura output for debugging CI

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,6 +2,12 @@
 set -e
 export CI=1
 
+EXECARGS=()
+while [[ "$#" > 0 ]]; do case $1 in
+  -v|--verbose-hasura) VERBOSE=1;;
+  *) EXECARGS+=($1);;
+esac; shift; done
+
 if [ ! "$HARDHAT_FORK_BLOCK" ]; then
   echo
   echo "------------------------------------------------------------------------"
@@ -14,10 +20,17 @@ fi
 
 # TODO: skip this if it's already running
 echo "Starting Hasura on port ${CI_HASURA_PORT}..."
-LOGFILE=${TMPDIR:-.}/hasura-ci-$(date +%s).log
-echo "Writing output to" $LOGFILE
-docker compose --profile ci \
-  run --rm --service-ports graphql-engine-ci > $LOGFILE 2>&1 & PID=$!
+
+CMD=(docker compose --profile ci run --rm --service-ports graphql-engine-ci)  
+
+if [ "$VERBOSE" ]; then
+  "${CMD[@]}" 2>&1 & PID=$!
+else
+  LOGFILE=${TMPDIR:-.}/hasura-ci-$(date +%s).log
+  echo "Writing output to" $LOGFILE
+  "${CMD[@]}" > $LOGFILE 2>&1 & PID=$!
+fi
+
 
 sleep 5
 until curl -s -o/dev/null http://localhost:$CI_HASURA_PORT; do
