710c45f36ddecfbf6694692971b32ca83fc6969b
Kevin Siegler <17910833+topocount@users.noreply.github.com>
2022-04-20 10:57:39 -0500

    Rename repo root scripts to use snake case
    Snake_case is a common convention for shell scripts. ts scripts have
    also been renamed to use snake_case to denote that they are scripts, and
    not production code.
    
    I'm unsure if we want to apply this convention in the hardhat subproject
    at this point, and will leave that as-is for now.
    
    Additionally, I also added the backend files to the typecheck yarn
    script. This was an initial oversight, but has since been corrected. The
    unused tsconfig.esrun.json has been deleted as well.
    
    test plan:
    `yarn db-seed-fresh`
    `yarn generate`
    `yarn clean`

--- scripts/seed_hasura.sh
+++ scripts/seed_hasura.sh
@@ -2,17 +2,17 @@
 
 
 # Disable event triggers before re-seeding DB
-./scripts/disable-triggers.sh
+./scripts/disable_triggers.sh
 
 # Re-seed database
 if [ "$1" == "--clean" ]
-  then ts-node ./scripts/db-clean.ts
+  then ts-node ./scripts/db_clean.ts
 fi
 
 ts-node ./scripts/db_seed_test.ts
 
 # Re-enable event triggers post-seeding
-./scripts/enable-triggers.sh
+./scripts/enable_triggers.sh
 
 # disable cron tasks to minimize unexpected CPU spikes
 ./scripts/disable_cron.sh
