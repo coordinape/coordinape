e74613f55e433dd3f1df4814ed527dda3eb82732
amrro <pushes.flutes-0j@icloud.com>
2022-04-22 16:41:13 +0200

    Use `ContributorsTable` in `AdminPage`

--- src/pages/AdminPage/AdminPage.tsx
+++ src/pages/AdminPage/AdminPage.tsx
@@ -4,36 +4,27 @@ import { useNavigate } from 'react-router-dom';
 
 import { makeStyles, Button } from '@material-ui/core';
 
-import {
-  StaticTable,
-  ActionDialog,
-  OrganizationHeader,
-  ApeInfoTooltip,
-} from 'components';
-import { USER_ROLE_ADMIN, USER_ROLE_COORDINAPE } from 'config/constants';
-// import { isFeatureEnabled } from 'config/features';
-import { useNavigation, useApiAdminCircle } from 'hooks';
+import { ActionDialog, OrganizationHeader } from 'components';
+import { useApiAdminCircle } from 'hooks';
 import useMobileDetect from 'hooks/useMobileDetect';
 import { EditIcon, PlusCircleIcon } from 'icons';
 import { useSelectedCircle } from 'recoilState/app';
 import { NEW_CIRCLE_CREATED_PARAMS, paths } from 'routes/paths';
-import { Avatar, Button as UiButton, Text } from 'ui';
-import { shortenAddress } from 'utils';
 
 import { AdminCircleModal } from './AdminCircleModal';
 import { AdminEpochModal } from './AdminEpochModal';
 import { AdminUserModal } from './AdminUserModal';
 import {
   AddContributorButton,
+  ContributorsTable,
   CreateEpochButton,
   EpochsTable,
   EpochsTableHeader,
-  renderUserCard,
   SettingsIconButton,
   UsersTableHeader,
 } from './components';
 
-import { IUser, ITableColumn, IEpoch } from 'types';
+import { IUser, IEpoch } from 'types';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -121,56 +112,7 @@ const useStyles = makeStyles(theme => ({
       width: '100%',
     },
   },
-  avatarCell: {
-    height: 48,
-    display: 'flex',
-    alignItems: 'center',
-    fontSize: 14,
-    lineHeight: 1.5,
-    fontWeight: 600,
-  },
-  tableActions: {
-    display: 'flex',
-    justifyContent: 'center',
-  },
-  actionSpacer: {
-    width: 30,
-  },
-  errorColor: {
-    color: theme.palette.error.main,
-  },
-  tablePlaceholderTitle: {
-    fontSize: 20,
-    lineHeight: 1.2,
-    color: theme.colors.text,
-    opacity: 0.7,
-  },
-  infoIcon: {
-    fontSize: '0.95rem',
-    fontWeight: 300,
-    verticalAlign: 'baseline',
-    // marginLeft: 3,
-    color: theme.colors.white,
-    '&:hover': {
-      color: theme.colors.text,
-    },
-  },
-  tooltipWrapper: {
-    marginTop: '6px',
-  },
-  tooltipTitle: {
-    fontSize: '18px',
-  },
-  tooltipBodyText: {
-    fontSize: '14px',
-  },
-  tooltip: {
-    padding: theme.spacing(4),
-    maxWidth: 296,
-  },
 }));
-
-const englishCollator = new Intl.Collator('en-u-kf-upper');
 
 const AdminPage = ({ legacy }: { legacy?: boolean }) => {
   const classes = useStyles();
@@ -194,7 +136,6 @@ const AdminPage = ({ legacy }: { legacy?: boolean }) => {
   );
 
   const navigate = useNavigate();
-  const { getToProfile } = useNavigation();
 
   const {
     circleId,
@@ -214,27 +155,6 @@ const AdminPage = ({ legacy }: { legacy?: boolean }) => {
     setKeyword(event.target.value);
   };
 
-  const renderActions = (onEdit?: () => void, onDelete?: () => void) => (
-    <div className={classes.tableActions}>
-      {onEdit ? (
-        <UiButton size="small" onClick={onEdit} color="blue">
-          Edit
-        </UiButton>
-      ) : (
-        <div className={classes.actionSpacer} />
-      )}
-
-      {onDelete && (
-        <>
-          <Text color="lightBlue">|</Text>
-          <UiButton size="small" onClick={onDelete} color="blue">
-            Delete
-          </UiButton>
-        </>
-      )}
-    </div>
-  );
-
   // User Columns
   const filterUser = useMemo(
     () => (u: IUser) => {
@@ -242,116 +162,6 @@ const AdminPage = ({ legacy }: { legacy?: boolean }) => {
       return r.test(u.name) || r.test(u.address);
     },
     [keyword]
-  );
-
-  const userColumns = useMemo(
-    () =>
-      [
-        {
-          label: 'Name',
-          accessor: 'name',
-          render: function UserName(u: IUser) {
-            return (
-              <div className={classes.avatarCell}>
-                <Avatar
-                  path={u?.profile?.avatar}
-                  name={u?.name}
-                  onClick={getToProfile(u.address)}
-                />
-                <span>{u.name}</span>
-                <span>
-                  {u.role === USER_ROLE_COORDINAPE ? (
-                    <div className={classes.tooltipWrapper}>
-                      <ApeInfoTooltip classes={{ tooltip: classes.tooltip }}>
-                        <b className={classes.tooltipTitle}>
-                          Why is Coordinape in your circle?
-                        </b>
-                        <p className={classes.tooltipBodyText}>
-                          We&apos;re experimenting with the gift circle
-                          mechanism as our revenue model. By default, Coordinape
-                          appears in your circle and any user can allocate to
-                          Coordinape. To remove the Coordinape user, click the
-                          trash can icon on the right side of this row.
-                        </p>
-                        <a
-                          href="https://coordinape.notion.site/Why-is-Coordinape-in-my-Circle-fd17133a82ef4cbf84d4738311fb557a"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          Let us know what you think
-                        </a>
-                      </ApeInfoTooltip>
-                    </div>
-                  ) : (
-                    ''
-                  )}
-                </span>
-              </div>
-            );
-          },
-          sortFunc: (a: string, b: string) => englishCollator.compare(a, b),
-          wide: true,
-          leftAlign: true,
-        },
-        {
-          label: 'ETH Wallet',
-          accessor: 'address',
-          render: (u: IUser) => shortenAddress(u.address),
-        },
-        {
-          label: 'GIVER',
-          tooltip: 'Circle Member allocating GIVE',
-          render: (u: IUser) => (!u.non_giver ? '✅' : '❌'),
-        },
-        {
-          label: 'GIVE Recipient',
-          tooltip: 'Circle Member receiving GIVE',
-          render: (u: IUser) =>
-            u.fixed_non_receiver
-              ? 'Forced Opt Out'
-              : u.non_receiver
-              ? '✅'
-              : '❌',
-        },
-        {
-          label: 'Admin',
-          tooltip:
-            'As a Circle Admin, you will be able to edit Circle Settings, Edit Epoch settings, edit your users, and create new circles.',
-          tooltipMoreUrl: 'https://docs.coordinape.com/welcome/admin_info',
-          render: (u: IUser) => (u.role === USER_ROLE_ADMIN ? '✅' : '❌'),
-        },
-        {
-          label: 'GIVE sent',
-          accessor: 'give_token_remaining',
-          render: (u: IUser) =>
-            !u.non_giver || u.starting_tokens - u.give_token_remaining != 0
-              ? `${u.starting_tokens - u.give_token_remaining}/${
-                  u.starting_tokens
-                }`
-              : '-',
-        },
-        {
-          label: 'GIVE received',
-          accessor: 'give_token_received',
-          render: (u: IUser) =>
-            u.give_token_received === 0 &&
-            (!!u.fixed_non_receiver || !!u.non_receiver)
-              ? '-'
-              : u.give_token_received,
-        },
-        {
-          label: 'Actions',
-          render: (u: IUser) =>
-            renderActions(
-              u.role !== USER_ROLE_COORDINAPE
-                ? () => setEditUser(u)
-                : undefined,
-              u.id !== me?.id ? () => setDeleteUserDialog(u) : undefined
-            ),
-          noSort: true,
-        },
-      ] as ITableColumn[],
-    []
   );
 
   return (
@@ -412,30 +222,14 @@ const AdminPage = ({ legacy }: { legacy?: boolean }) => {
           />
         </div>
 
-        <StaticTable
-          columns={userColumns}
-          singleColumn={isMobile}
-          renderSingleColumn={renderUserCard}
-          data={visibleUsers}
-          perPage={15}
+        <ContributorsTable
+          users={visibleUsers}
+          myUser={me}
+          setNewUser={setNewUser}
           filter={filterUser}
-          sortable
-          placeholder={
-            <>
-              <h2 className={classes.tablePlaceholderTitle}>
-                You haven’t added any contributors
-              </h2>
-              <Button
-                variant="contained"
-                color="primary"
-                size="small"
-                startIcon={<PlusCircleIcon />}
-                onClick={() => setNewUser(true)}
-              >
-                Add Contributor
-              </Button>
-            </>
-          }
+          setEditUser={setEditUser}
+          setDeleteUserDialog={setDeleteUserDialog}
+          perPage={15}
         />
       </div>
       {(editUser || newUser) && (
