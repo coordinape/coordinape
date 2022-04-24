9cf3f50e30c78301b43ca9e628678e5452af653f
Kevin Siegler <17910833+topocount@users.noreply.github.com>
2022-04-24 14:41:16 -0500

    Implement Stable, Consistent Seed Script
    This new seed script checks every box in #692 and is meant to be at
    least a little bit more maintainable and extensible than its
    predecessor.
    
    Nominees were omitted but can be added if desired. I figured the
    workflow for testing vouching and nominees is simple enough that it's
    not worth setting something up mid-stream.
    
    Avatars are now generated after running `yarn docker:clean`
    
    All accounts used in this seed can be accessed by utilizing the seed
    `test test test test test test test test test test test junk` in one's
    wallet.
    
    test plan:
    
    - yarn docker:clean
    - yarn docker:start
    - yarn db-seed-fresh
    
    Observe that all details in #692 are taken care of

---
+++ scripts/db_seed_test.ts
@@ -0,0 +1,136 @@
+import { faker } from '@faker-js/faker';
+import { DateTime } from 'luxon';
+
+import {
+  getAvatars,
+  getMembershipInput,
+  makeEpoch,
+  createGifts,
+  insertMemberships,
+  getCircleName,
+} from './util/seed';
+
+faker.seed(4);
+
+async function main() {
+  await createFreshOpenEpoch();
+  await createEndedEpochWithGifts();
+  await createCircleWithPendingGifts();
+  await createCircleWithGiftsNotYetEnded();
+  await createFreshOpenEpochNoDev();
+  await createFreshOpenEpochDevAdmin();
+  await getAvatars();
+}
+
+main()
+  // eslint-disable-next-line no-console
+  .then(() => console.log('Finished Seeding DB'))
+  .catch(console.error);
+
+async function createCircleWithGiftsNotYetEnded() {
+  const result = await insertMemberships(
+    getMembershipInput(
+      {
+        protocolInput: { name: 'Closed Epoch not Ended' },
+        circlesInput: [
+          {
+            name: getCircleName() + ' auto opt-out',
+            auto_opt_out: true,
+            default_opt_in: true,
+            min_vouches: 2,
+            nomination_days_limit: 1,
+            only_giver_vouch: false,
+            token_name: 'GIVE',
+            vouching: true,
+          },
+        ],
+      },
+      {}
+    )
+  );
+  const circleId = result[0].circle_id;
+  const epochId = await makeEpoch(
+    circleId,
+    DateTime.now().minus({ days: 8 }),
+    DateTime.now().minus({ days: 1 }),
+    1,
+    false
+  );
+  await createGifts(result, epochId);
+}
+
+async function createFreshOpenEpochNoDev() {
+  const result = await insertMemberships(
+    getMembershipInput({ protocolInput: { name: 'Fresh Open Epoch No Dev' } })
+  );
+  const circleId = result[0].circle_id;
+  await makeEpoch(
+    circleId,
+    DateTime.now().minus({ hours: 1 }),
+    DateTime.now().plus({ days: 6, hours: 23 }),
+    1
+  );
+}
+
+async function createFreshOpenEpochDevAdmin() {
+  const result = await insertMemberships(
+    getMembershipInput(
+      { protocolInput: { name: 'Fresh Open Epoch Admin' } },
+      {}
+    )
+  );
+  const circleId = result[0].circle_id;
+  await makeEpoch(
+    circleId,
+    DateTime.now().minus({ hours: 1 }),
+    DateTime.now().plus({ days: 6, hours: 23 }),
+    1
+  );
+}
+
+async function createFreshOpenEpoch() {
+  const result = await insertMemberships(
+    getMembershipInput(
+      { protocolInput: { name: 'Fresh Open Epoch Regular User' } },
+      { role: 0 }
+    )
+  );
+  const circleId = result[0].circle_id;
+  await makeEpoch(
+    circleId,
+    DateTime.now().minus({ hours: 1 }),
+    DateTime.now().plus({ days: 6, hours: 23 }),
+    1
+  );
+}
+
+async function createEndedEpochWithGifts() {
+  const result = await insertMemberships(
+    getMembershipInput(
+      { protocolInput: { name: 'Ended Epoch With Gifts' } },
+      {}
+    )
+  );
+  const circleId = result[0].circle_id;
+  const epochId = await makeEpoch(
+    circleId,
+    DateTime.now().minus({ days: 8 }),
+    DateTime.now().minus({ days: 1 }),
+    1
+  );
+  await createGifts(result, epochId, false);
+}
+
+async function createCircleWithPendingGifts() {
+  const result = await insertMemberships(
+    getMembershipInput({ protocolInput: { name: 'Open Epoch With Gifts' } }, {})
+  );
+  const circleId = result[0].circle_id;
+  const epochId = await makeEpoch(
+    circleId,
+    DateTime.now().minus({ days: 6 }),
+    DateTime.now().plus({ hours: 6 }),
+    1
+  );
+  await createGifts(result, epochId);
+}
