2ba5da660fc2a19a82d12fa2f105ce14fa9c71b1
META DREAMER <jutt@ualberta.ca>
2022-04-26 08:39:26 -0600

    Change zeus package back to original
    Upstream version published with fixes

--- src/lib/gql/queries.ts
+++ src/lib/gql/queries.ts
@@ -419,14 +419,14 @@ export const getFullCircle = async (
   > & {
     pending_gifts: IApiTokenGift[];
     users: IApiUser[];
-    circle: Omit<typeof circle.circles_by_pk, 'organization'> & {
+    circle: Omit<typeof circle, 'organization'> & {
       protocol: IProtocol;
     };
   } = {
     ...circles_by_pk,
     circle: {
-      ...circle.circles_by_pk,
-      protocol: circle.circles_by_pk.organization,
+      ...circle,
+      protocol: circle.organization,
     },
     pending_gifts: circles_by_pk.pending_token_gifts.map(pg => {
       const notedGift: Omit<typeof pg, 'gift_private'> & {
