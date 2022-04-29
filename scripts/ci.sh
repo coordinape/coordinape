adb67ad6d6f19c7542a6e215698b6a508485498c
Kevin Siegler <17910833+topocount@users.noreply.github.com>
2022-04-28 21:58:05 -0500

    Configure vercel dev in CI

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -3,9 +3,9 @@ set -e
 export CI=1
 
 EXECARGS=()
-while [[ "$#" > 0 ]]; do case $1 in
+while [[ "$#" -gt 0 ]]; do case $1 in
   -v|--verbose-hasura) VERBOSE=1;;
-  *) EXECARGS+=($1);;
+  *) EXECARGS+=("$1");;
 esac; shift; done
 
 if [ ! "$HARDHAT_FORK_BLOCK" ]; then
@@ -27,18 +27,30 @@ if [ "$VERBOSE" ]; then
   "${CMD[@]}" 2>&1 & PID=$!
 else
   LOGFILE=${TMPDIR:-.}/hasura-ci-$(date +%s).log
-  echo "Writing output to" $LOGFILE
-  "${CMD[@]}" > $LOGFILE 2>&1 & PID=$!
+  echo "Writing output to" "$LOGFILE"
+  "${CMD[@]}" > "$LOGFILE" 2>&1 & PID=$!
 fi
 
+VERCEL_CMD=(vercel dev -t "$VERCEL_TOKEN" --confirm)
+
+"${VERCEL_CMD[@]}" 2>&1 & VERCEL_PID=$!
+
 # Kill Hasura when this script exits
-trap "docker compose --profile ci down" EXIT
+trap 'docker compose --profile ci down && kill $VERCEL_PID' EXIT
 
 sleep 5
-until curl -s -o/dev/null http://localhost:$CI_HASURA_PORT; do
+until curl -s -o/dev/null http://localhost:"$CI_HASURA_PORT"; do
   sleep 1
   if [ -z "$(ps -p $PID -o pid=)" ]; then
     echo "Hasura failed to start up."
+    exit 1
+  fi
+done
+
+until curl -s -o/dev/null http://localhost:"$LOCAL_VERCEL_PORT"; do
+  sleep 1
+  if [ -z "$(ps -p $VERCEL_PID -o pid=)" ]; then
+    echo "Vercel failed to start up."
     exit 1
   fi
 done
