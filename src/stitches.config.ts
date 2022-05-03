c5c0b09fc71cfb732502c256fc17162badd4103e
Lawrence Wang <l@lw.io>
2022-05-03 10:10:13 -0700

    remove unused shorthand props
    i like brevity, but it must be balanced against legibility

--- src/stitches.config.ts
+++ src/stitches.config.ts
@@ -216,76 +216,14 @@ export const {
       marginBottom: value,
     }),
 
-    ta: (value: Stitches.PropertyValue<'textAlign'>) => ({ textAlign: value }),
-
-    fd: (value: Stitches.PropertyValue<'flexDirection'>) => ({
-      flexDirection: value,
-    }),
-    fw: (value: Stitches.PropertyValue<'flexWrap'>) => ({ flexWrap: value }),
-
-    ai: (value: Stitches.PropertyValue<'alignItems'>) => ({
-      alignItems: value,
-    }),
-    ac: (value: Stitches.PropertyValue<'alignContent'>) => ({
-      alignContent: value,
-    }),
-    jc: (value: Stitches.PropertyValue<'justifyContent'>) => ({
-      justifyContent: value,
-    }),
-    as: (value: Stitches.PropertyValue<'alignSelf'>) => ({ alignSelf: value }),
-    fg: (value: Stitches.PropertyValue<'flexGrow'>) => ({ flexGrow: value }),
-    fs: (value: Stitches.PropertyValue<'flexShrink'>) => ({
-      flexShrink: value,
-    }),
-    fb: (value: Stitches.PropertyValue<'flexBasis'>) => ({ flexBasis: value }),
-
-    bc: (value: Stitches.PropertyValue<'backgroundColor'>) => ({
-      backgroundColor: value,
-    }),
-
-    br: (value: Stitches.PropertyValue<'borderRadius'>) => ({
-      borderRadius: value,
-    }),
-    btrr: (value: Stitches.PropertyValue<'borderTopRightRadius'>) => ({
-      borderTopRightRadius: value,
-    }),
-    bbrr: (value: Stitches.PropertyValue<'borderBottomRightRadius'>) => ({
-      borderBottomRightRadius: value,
-    }),
-    bblr: (value: Stitches.PropertyValue<'borderBottomLeftRadius'>) => ({
-      borderBottomLeftRadius: value,
-    }),
-    btlr: (value: Stitches.PropertyValue<'borderTopLeftRadius'>) => ({
-      borderTopLeftRadius: value,
-    }),
-
-    bs: (value: Stitches.PropertyValue<'boxShadow'>) => ({ boxShadow: value }),
-
-    lh: (value: Stitches.PropertyValue<'lineHeight'>) => ({
-      lineHeight: value,
-    }),
-
-    ox: (value: Stitches.PropertyValue<'overflowX'>) => ({ overflowX: value }),
-    oy: (value: Stitches.PropertyValue<'overflowY'>) => ({ overflowY: value }),
-
-    pe: (value: Stitches.PropertyValue<'pointerEvents'>) => ({
-      pointerEvents: value,
-    }),
-    us: (value: Stitches.PropertyValue<'userSelect'>) => ({
-      WebkitUserSelect: value,
-      userSelect: value,
-    }),
-
     userSelect: (value: Stitches.PropertyValue<'userSelect'>) => ({
       WebkitUserSelect: value,
       userSelect: value,
     }),
-
     size: (value: Stitches.PropertyValue<'width'>) => ({
       width: value,
       height: value,
     }),
-
     appearance: (value: Stitches.PropertyValue<'appearance'>) => ({
       WebkitAppearance: value,
       appearance: value,
