d6d7528f8fde7cbc8b0d04da515f90eba6c09625
Lawrence Wang <l@lw.io>
2022-05-04 11:20:54 -0700

    make sure vaults tables are deleted
    also create an alternate method of truncating all tables quickly (might be faster in the future)

--- scripts/db_clean.ts
+++ scripts/db_clean.ts
@@ -3,23 +3,24 @@ import { adminClient } from '../api-lib/gql/adminClient';
 
 type MutationName = keyof ValueTypes['mutation_root'];
 
-async function run() {
-  const mutations: Record<string, MutationName> = {
-    token_gifts: 'delete_token_gifts',
-    pending_token_gifts: 'delete_pending_token_gifts',
-    teammates: 'delete_teammates',
-    vouches: 'delete_vouches',
-    nominees: 'delete_nominees',
-    user: 'delete_users',
-    epoch: 'delete_epochs',
-    circle: 'delete_circles',
-    organizations: 'delete_organizations',
-    profile: 'delete_profiles',
-  };
+(async () => {
+  const mutations: MutationName[] = [
+    'delete_claims',
+    'delete_distributions',
+    'delete_vaults',
+    'delete_token_gifts',
+    'delete_pending_token_gifts',
+    'delete_teammates',
+    'delete_vouches',
+    'delete_nominees',
+    'delete_users',
+    'delete_epochs',
+    'delete_circles',
+    'delete_organizations',
+    'delete_profiles',
+  ];
 
-  const events: string[] = [];
-
-  for (const [name, mutation] of Object.entries(mutations)) {
+  for (const mutation of mutations) {
     const res = await adminClient.mutate(
       {
         [mutation]: [{ where: {} }, { affected_rows: true }],
@@ -31,23 +32,14 @@ async function run() {
 
     // @ts-ignore
     const count = res[mutation]?.affected_rows;
-    if (typeof count === 'number') {
-      events.push(`Deleted ${count} from ${name}`);
-    } else {
-      throw `Failed to deleteMany.`;
-    }
+    if (typeof count !== 'number') throw `Failed to run ${mutation}`;
+    console.log(`${mutation}: ${count}`); // eslint-disable-line
   }
-
-  console.log("Coordinape destroyed, I hope you're happy.", events);
-}
-
-(async function () {
-  await run()
-    .catch(error => {
-      console.error(error);
-      process.exit(1);
-    })
-    .then(() => {
-      process.exit(0);
-    });
-})();
+})()
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  })
+  .then(() => {
+    process.exit(0);
+  });
--- scripts/seed_hasura.sh
+++ scripts/seed_hasura.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
+set -e
+SCRIPT_DIR="${0%/*}"
 
+# read .env, filtering out comments
+DOTENV_FILE=$SCRIPT_DIR/../.env 
+if [ -f "$DOTENV_FILE" ]; then
+  export $(cat $DOTENV_FILE | sed 's/^#.*$//' | xargs)
+fi
+
+PG_CXN="postgres://$LOCAL_POSTGRES_USER:$LOCAL_POSTGRES_PASSWORD@localhost/$LOCAL_POSTGRES_DATABASE"
+
+CMD_TRUNCATE_ALL="DO \$\$ BEGIN
+  EXECUTE (SELECT 'TRUNCATE TABLE ' || string_agg(oid::regclass::text, ', ') || ' CASCADE' 
+    FROM pg_class WHERE relkind = 'r' AND relnamespace = 'public'::regnamespace);
+END\$\$"
 
 # Disable event triggers before re-seeding DB
 ./scripts/disable_triggers.sh
 
 # Re-seed database
-if [ "$1" == "--clean" ]
-  then ts-node ./scripts/db_clean.ts
+if [ "$1" == "--clean" ]; then 
+  echo "Truncating all tables..."
+  ts-node ./scripts/db_clean.ts
+  # psql $PG_CXN -c "$CMD_TRUNCATE_ALL" >/dev/null
 fi
 
+echo "Seeding..."
 ts-node ./scripts/db_seed_test.ts
 
 # Re-enable event triggers post-seeding
