37ef89d82e7bb658020da0f6a198cff2c3282b7d
zashton <17747090+zashton@users.noreply.github.com>
2022-05-12 22:04:04 +0800

    allow circle admins to see fixed_payment_amount

--- hasura/migrations/default/1652324936469_user_private_view/up.sql
+++ hasura/migrations/default/1652324936469_user_private_view/up.sql
@@ -2,6 +2,7 @@ CREATE OR REPLACE VIEW "public"."user_private" AS
  SELECT us.id AS user_id,
     cr.fixed_payment_enabled,
     cr.fixed_payment_token_type,
-    us.fixed_payment_amount
+    us.fixed_payment_amount,
+    us.circle_id
    FROM users us
    LEFT JOIN circles cr ON cr.id = us.circle_id;
