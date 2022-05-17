ca02e93d571f0dc2b94cfbb5bec4542f669711e6
zashton <17747090+zashton@users.noreply.github.com>
2022-05-17 11:17:13 +0800

    remove fixed payment enabled col

--- hasura/migrations/default/1652323953300_alter_table_public_circles_add_column_fixed_payment_enabled/up.sql
+++
@@ -1,2 +0,0 @@
-alter table "public"."circles" add column "fixed_payment_enabled" boolean
- not null default 'false';
--- hasura/migrations/default/1652324936469_user_private_view/up.sql
+++ hasura/migrations/default/1652324936469_user_private_view/up.sql
@@ -1,6 +1,5 @@
 CREATE OR REPLACE VIEW "public"."user_private" AS 
  SELECT us.id AS user_id,
-    cr.fixed_payment_enabled,
     cr.fixed_payment_token_type,
     us.fixed_payment_amount,
     us.circle_id
