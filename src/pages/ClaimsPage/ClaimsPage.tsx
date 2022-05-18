33c2e079d8d8b019357b26e1867b47494ca30c2e
Tony Olendo <teeolendo@gmail.com>
2022-05-18 10:56:35 -0400

    WIP: Claims Test
    WIP: Claims
    
    WIP: Claims
    
    fix claims to use profiles instead of users
    
    WIP: Claims Hook and UI styling complete

--- src/pages/ClaimsPage/ClaimsPage.tsx
+++ src/pages/ClaimsPage/ClaimsPage.tsx
@@ -1,212 +1,170 @@
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import assert from 'assert';
 
-import { NewApeAvatar } from 'components';
-import { Box, Link, Panel, Table, Flex, Text } from 'ui';
-import { getCircleAvatar } from 'utils/domain';
+import { BigNumber } from 'ethers';
+import { encodeCircleId } from 'lib/vaults';
+import { useQuery } from 'react-query';
 
-import { makeTable } from './Table';
+import { LoadingModal, makeTable } from 'components';
+import { useContracts } from 'hooks';
+import useConnectedAddress from 'hooks/useConnectedAddress';
+import { Box, Panel, Flex, Text, Button } from 'ui';
+import { SingleColumnLayout } from 'ui/layouts';
 
-type OrgClaims = {
-  org_name: string;
-  org_id: number;
-  claim: {
-    id: number;
-    circle_name: string;
-    epoch: number;
-    rewards: {
-      value: number;
-      symbol?: string;
-      claim_id: number;
-    }[];
-    logo?: string;
-  };
-};
+import { getClaims, ClaimsResult } from './queries';
+import { useClaimAllocation } from './useClaimAllocation';
 
-const ClaimsTable = makeTable<OrgClaims>('ClaimsTable');
+const ClaimsTable = makeTable<ClaimsResult>('ClaimsTable');
 
 export default function ClaimsPage() {
-  const claims: OrgClaims[] = [
-    {
-      org_name: 'Yearn',
-      org_id: 1,
-      claim: {
-        id: 2,
-        circle_name: 'Circle 1',
-        epoch: 2,
-        rewards: [
-          {
-            value: 1200,
-            symbol: 'USDC',
-            claim_id: 110,
-          },
-          {
-            value: 1290,
-            symbol: 'YToken',
-            claim_id: 100,
-          },
-        ],
-      },
-    },
-    {
-      org_name: 'Gitcoin',
-      org_id: 1,
-      claim: {
-        id: 2,
-        circle_name: 'Grants Team',
-        epoch: 2,
-        rewards: [
-          {
-            value: 200,
-            symbol: 'USDC',
-            claim_id: 1,
-          },
-          {
-            value: 750,
-            symbol: 'GTC',
-            claim_id: 2,
-          },
-        ],
-      },
-    },
-  ];
+  const address = useConnectedAddress();
+  const contracts = useContracts();
+  const allocateClaim = useClaimAllocation();
+
+  assert(address || contracts);
+
+  const {
+    isIdle,
+    isLoading,
+    isError,
+    error,
+    data: claims,
+  } = useQuery(['claims', 1], () => getClaims(1), {
+    enabled: !!(contracts && address),
+    retry: false,
+  });
+
+  if (isIdle || isLoading) return <LoadingModal visible />;
+  if (isError)
+    return (
+      <SingleColumnLayout>
+        Error retreiving your claims. {error}
+      </SingleColumnLayout>
+    );
+  if (!claims) return <SingleColumnLayout>No claims found</SingleColumnLayout>;
 
   return (
     <Box
       css={{
         margin: '$lg auto',
+        padding: '$md',
         maxWidth: '$mediumScreen',
       }}
     >
-      <Panel css={{ minHeight: '60vh' }}>
-        <Box
-          css={{
-            display: 'flex',
-            flexWrap: 'nowrap',
-            justifyContent: 'space-between',
+      <Box
+        css={{
+          fontSize: '$h1',
+          color: '$neutral',
+          display: 'flex',
+          alignItems: 'left',
+        }}
+      >
+        Claim Your Allocations
+      </Box>
+      <Box css={{ color: '$neutral', maxWidth: '60%', my: '$lg' }}>
+        You can claim all your rewards from this page. Note that you can claim
+        them for all your epochs in one circle but each token requires its own
+        claim transaction.
+      </Box>
+
+      <Panel css={{ my: '$lg', backgroundColor: '$border' }}>
+        <ClaimsTable
+          headers={[
+            {
+              title: 'Organization',
+              style: { whiteSpace: 'nowrap', textAlign: 'left' },
+            },
+            {
+              title: 'Circle',
+              style: { whiteSpace: 'nowrap', textAlign: 'left' },
+            },
+            {
+              title: 'Epochs',
+              style: { whiteSpace: 'nowrap', textAlign: 'left' },
+            },
+            {
+              title: 'Rewards',
+              style: { textAlign: 'right', width: '98%' },
+            },
+          ]}
+          data={claims}
+          startingSortIndex={2}
+          startingSortDesc
+          sortByColumn={() => {
+            return c => c;
           }}
         >
-          <Link
-            href={'/'}
-            css={{
-              fontSize: '$4',
-              lineHeight: '$shorter',
-              alignSelf: 'center',
-              color: '$text',
-              display: 'flex',
-              alignItems: 'center',
-              ml: '$lg',
-              cursor: 'pointer',
-            }}
-          >
-            <ArrowBackIcon />
-            Back
-          </Link>
-        </Box>
-        <Box
-          css={{
-            fontSize: '$6',
-            color: '$text',
-            display: 'flex',
-            alignItems: 'left',
-            ml: '$lg',
-            mt: '$lg',
-          }}
-        >
-          Claim Your Funds
-        </Box>
-        <Box
-          css={{
-            fontSize: '$4',
-            color: '$text',
-            display: 'flex',
-            alignItems: 'left',
-            ml: '$lg',
-            mt: '$lg',
-          }}
-        >
-          <Box>
-            You can claim all your rewards from this page. Note that you can
-            claim them for all your epochs in one circle but each token requires
-            its own claim transaction.
-          </Box>
-          <Box css={{ minWidth: '40%' }}></Box>
-        </Box>
-        <Box
-          css={{
-            ml: '$lg',
-          }}
-        >
-          {claims.map(claim => (
-            <>
-              <Text css={{ my: '$lg', fontSize: '$6' }}>{claim.org_name}</Text>
-              <ClaimsTable
-                key={claim.org_id}
-                Table={Table}
-                headers={['Circle', 'Epochs', `Rewards`, ``, ``]}
-                data={claims}
-                startingSortIndex={2}
-                startingSortDesc
-                sortByIndex={(index: number) => {
-                  if (index === 0) return (c: OrgClaims) => c.claim.circle_name;
-                  if (index === 1) return (c: OrgClaims) => c.claim.epoch;
-                  return (c: OrgClaims) => c.claim.rewards.length;
-                }}
-              >
-                {claim => (
-                  <tr key={claim.claim.id}>
-                    <td>
-                      <Flex row css={{ alignItems: 'left', gap: '$sm' }}>
-                        <NewApeAvatar
-                          path={getCircleAvatar({
-                            avatar: claim.claim.logo,
-                            circleName: claim.claim.circle_name,
-                          })}
-                          style={{ height: '32px', width: '32px' }}
-                        />
-                        <Text semibold>{claim.claim.circle_name}</Text>
-                      </Flex>
-                    </td>
-                    <td>{claim.claim.epoch}</td>
-                    {claim.claim.rewards.map(reward => (
-                      <td key={reward.claim_id}>
-                        {reward.value} {reward.symbol}
-                      </td>
-                    ))}
-                    <td>
-                      <Flex
-                        row
-                        css={{
-                          alignItems: 'right',
-                          justifyContent: 'flex-end',
-                        }}
-                      >
-                        {claim.claim.rewards.map((reward, index) => (
-                          <Link
-                            key={index}
-                            href={'/'}
-                            css={{
-                              fontSize: '$4',
-                              lineHeight: '$shorter',
-                              color: '$red',
-                              display: 'flex',
-                              alignItems: 'center',
-                              ml: '$md',
-                              mr: '$md',
-                              cursor: 'pointer',
-                              minWidth: '8vw',
-                            }}
-                          >
-                            Claim {reward.symbol}
-                          </Link>
-                        ))}
-                      </Flex>
-                    </td>
-                  </tr>
-                )}
-              </ClaimsTable>
-            </>
-          ))}
-        </Box>
+          {claim => (
+            <tr key={claim.id}>
+              <td>
+                <Text>{claim.distribution.epoch.circle?.name}</Text>
+              </td>
+              <td>
+                <Flex row css={{ gap: '$sm' }}>
+                  <Text>{claim.distribution.epoch.circle?.name}</Text>
+                </Flex>
+              </td>
+              <td>
+                <Text>{3}</Text>
+              </td>
+              <td>
+                <Flex
+                  css={{
+                    justifyContent: 'flex-end',
+                  }}
+                >
+                  <Flex
+                    css={{
+                      minWidth: '10vw',
+                      justifyContent: 'flex-end',
+                      gap: '$md',
+                      mr: '$md',
+                      '@sm': {
+                        minWidth: '20vw',
+                      },
+                    }}
+                  >
+                    <Text>
+                      {claim.amount} {claim.distribution.vault.symbol}
+                    </Text>
+                    <Button
+                      color="primary"
+                      outlined
+                      css={{
+                        fontWeight: '$normal',
+                        minHeight: '$xs',
+                        px: '$sm',
+                        minWidth: '5vw',
+                        borderRadius: '$2',
+                      }}
+                      onClick={() => {
+                        allocateClaim({
+                          claimId: claim.id,
+                          circleId: encodeCircleId(
+                            claim.distribution.epoch.circle?.id
+                          ),
+                          vault: claim.distribution.vault,
+                          merkleIndex: BigNumber.from(claim.index),
+                          address: address as string,
+                          amount: BigNumber.from(claim.amount).mul(
+                            BigNumber.from(10).pow(
+                              claim.distribution.vault.decimals
+                            )
+                          ),
+                          proof: claim.proof.split(','),
+                          distributionEpochId: BigNumber.from(
+                            claim.distribution.distribution_epoch_id
+                          ),
+                        });
+                      }}
+                    >
+                      Claim {claim.distribution.vault.symbol}
+                    </Button>
+                  </Flex>
+                </Flex>
+              </td>
+            </tr>
+          )}
+        </ClaimsTable>
       </Panel>
     </Box>
   );
