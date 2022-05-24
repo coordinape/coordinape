34fcca1e615d4207321c88b1d2c2fce468047f20
Wing Feather <wingfeatherwave@gmail.com>
2022-05-24 09:49:31 -0700

    simple radix popover setup

--- src/ui/Popover.tsx
+++ src/ui/Popover.tsx
@@ -1,17 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import { ReactNode } from 'react';
 
 import * as Popover from '@radix-ui/react-popover';
 import { CSS, styled } from 'stitches.config';
 
-const Content = styled(Popover.Content, {
-  backgroundColor: '$white',
-  borderRadius: '$3',
-  width: '90vw',
-  maxWidth: '650px',
-  padding: '$2xl',
-  margin: 'calc($xl * 2) auto $xl',
-  position: 'relative',
-});
+// const Content = styled(Popover.Content, {
+//   backgroundColor: '$white',
+//   borderRadius: '$3',
+//   width: '90vw',
+//   maxWidth: '650px',
+//   padding: '$2xl',
+//   margin: 'calc($xl * 2) auto $xl',
+//   position: 'relative',
+// });
 
 type PopoverProps = {
   children: ReactNode;
@@ -29,16 +31,11 @@ export const PopoverRadix = ({
 PopoverProps) => {
   return (
     <Popover.Root>
-      <Popover.Trigger>open me</Popover.Trigger>
-      <Popover.Anchor>whyyy</Popover.Anchor>
+      <Popover.Trigger>trigger</Popover.Trigger>
       <Popover.Content>
+        what is this
         <Popover.Close />
         <Popover.Arrow />
-        <Content css={css}>
-          {children}
-          stuff in here
-        </Content>
-        stuff in here
       </Popover.Content>
     </Popover.Root>
   );
