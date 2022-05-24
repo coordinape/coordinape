15291c430d04c1e1ca6907c87f8b4f9d6a26081d
Wing Feather <wingfeatherwave@gmail.com>
2022-05-24 13:54:08 -0700

    add radix primitives

--- src/ui/Popover.tsx
+++ src/ui/Popover.tsx
@@ -1,7 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
-import React from 'react';
-
 import * as PopoverPrimitive from '@radix-ui/react-popover';
 import { styled } from '@stitches/react';
 
@@ -9,8 +5,7 @@ const StyledContent = styled(PopoverPrimitive.Content, {
   padding: 0,
   borderRadius: 8,
   background: '$white',
-  boxShadow:
-    '0px 0px 3px 0px #0000001C, 0px 0px 16px 0px #0000001F, 0px 0px 87px 0px #0000003D',
+  boxShadow: '$heavy',
   display: 'flex',
   flexDirection: 'column',
 });
@@ -19,3 +14,4 @@ export const Popover = PopoverPrimitive.Root;
 export const PopoverTrigger = PopoverPrimitive.Trigger;
 export const PopoverContent = StyledContent;
 export const PopoverClose = PopoverPrimitive.Close;
+export const PopoverArrow = PopoverPrimitive.Arrow;
