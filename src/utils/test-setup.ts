4bf2f8b960b6e695a3da06de7b82ff83eac2ac51
Lawrence Wang <l@lw.io>
2022-06-09 13:16:20 -0400

    suppress Material-UI deprecation warning in tests
    closes #843

--- src/utils/test-setup.ts
+++ src/utils/test-setup.ts
@@ -1,1 +1,12 @@
 import 'jest-ts-auto-mock';
+
+const ignoreWarnings = [
+  'Material-UI: The `css` function is deprecated. Use the `styleFunctionSx` instead.',
+];
+
+(console as any).origWarn = console.warn;
+
+console.warn = (...args) => {
+  if (ignoreWarnings.some(x => x === args[0])) return;
+  return (console as any).origWarn(...args);
+};
