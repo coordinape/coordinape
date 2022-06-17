4d64507bbd9b4dc08fcd6448487951e30f974975
Lawrence Wang <l@lw.io>
2022-06-17 15:15:54 -0400

    fix down migrations

--- hasura/migrations/default/1653334589429_useprofilesonclaims/down.sql
+++ hasura/migrations/default/1653334589429_useprofilesonclaims/down.sql
@@ -1,11 +1,13 @@
 
+alter table "public"."claims" add column "user_id" int8;
+alter table "public"."claims" alter column "user_id" drop not null;
 alter table "public"."claims"
   add constraint "claims_user_id_fkey"
   foreign key (user_id)
   references "public"."users"
   (id) on update no action on delete no action;
-alter table "public"."claims" alter column "user_id" drop not null;
-alter table "public"."claims" add column "user_id" int8;
+
+alter table "public"."claims" drop column "profile_id";
 
 -- Could not auto-generate a down migration.
 -- Please write an appropriate down migration for the SQL below:
--- hasura/migrations/default/1653573437548_alter_table_public_claims_add_column_txHash/down.sql
+++ hasura/migrations/default/1653573437548_alter_table_public_claims_add_column_txHash/down.sql
@@ -2,3 +2,4 @@
 -- Please write an appropriate down migration for the SQL below:
 -- alter table "public"."claims" add column "txHash" varchar
 --  null;
+alter table "public"."claims" drop column "txHash";
