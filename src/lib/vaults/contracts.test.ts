23a676f00050e83ad19893e619749656506f08fc
Lawrence Wang <l@lw.io>
2022-06-20 20:42:51 -0400

    fix hasSimpleToken
    can't determine this by the token symbol, because a user could use e.g. USDC as a simple token by putting in the token address directly

--- src/lib/vaults/contracts.test.ts
+++ src/lib/vaults/contracts.test.ts
@@ -14,6 +14,29 @@ test('getPricePerShare', async () => {
   const event = receipt.events?.find(e => e.event === 'VaultCreated');
   const vaultAddress = event?.args?.vault;
 
-  const price = await contracts.getPricePerShare(vaultAddress, 'DAI', 18);
+  const price = await contracts.getPricePerShare(
+    vaultAddress,
+    ZERO_ADDRESS,
+    18
+  );
   expect(price.toUnsafeFloat()).toBeGreaterThan(1);
+});
+
+test('getPricePerShare with simple token', async () => {
+  const contracts = new Contracts(chainId, provider);
+  const tokenAddress = contracts.getToken('DAI').address;
+  const tx = await contracts.vaultFactory.createApeVault(
+    ZERO_ADDRESS,
+    tokenAddress
+  );
+  const receipt = await tx.wait();
+  const event = receipt.events?.find(e => e.event === 'VaultCreated');
+  const vaultAddress = event?.args?.vault;
+
+  const price = await contracts.getPricePerShare(
+    vaultAddress,
+    tokenAddress,
+    18
+  );
+  expect(price.toUnsafeFloat()).toEqual(1);
 });
