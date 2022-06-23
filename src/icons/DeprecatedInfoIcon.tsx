3f413b2f6429e9fcb9ead8d3cbe90c8719afd074
CryptoGraffe <graffe@graffe.co>
2022-06-22 23:17:22 -0700

    renaming mui icons to deprecatedX

--- src/icons/DeprecatedInfoIcon.tsx
+++ src/icons/DeprecatedInfoIcon.tsx
@@ -1,6 +1,6 @@
 import { SvgIcon } from '@material-ui/core';
 
-export const InfoIcon = (props: any) => (
+export const DeprecatedInfoIcon = (props: any) => (
   <SvgIcon {...props}>
     <path
       d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
