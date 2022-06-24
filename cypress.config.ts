0bd07f76da6ff156d6183fbb283ecf19295c3193
CryptoGraffe <graffe@graffe.co>
2022-06-24 10:18:56 -0700

    cleaning up cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,6 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
+
+import plugins from './cypress/plugins/index';
 
 export default defineConfig({
   defaultCommandTimeout: 15000,
@@ -9,8 +11,8 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.ts').default(on, config)
+      return plugins(on, config);
     },
     baseUrl: 'http://localhost:undefined',
   },
-})
+});
