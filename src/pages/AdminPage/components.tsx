a64ae27b60d65bbab4476a36670cfc1fa3944e6d
Amr Elghobary <amr.elghobary@gmail.com>
2022-06-24 17:42:58 +0200

    Update Coordinape's tooltip description

--- src/pages/AdminPage/components.tsx
+++ src/pages/AdminPage/components.tsx
@@ -312,8 +312,8 @@ const coordinapeTooltipContent = () => {
       <p>
         We&apos;re experimenting with the gift circle mechanism as our revenue
         model. By default, Coordinape appears in your circle and any user can
-        allocate to Coordinape. To remove the Coordinape user, click the trash
-        can icon on the right side of this row.
+        allocate to Coordinape. To show or hide the Coordinape user, use the
+        links on the right side of this row.
       </p>
       <a
         href="https://coordinape.notion.site/Why-is-Coordinape-in-my-Circle-fd17133a82ef4cbf84d4738311fb557a"
