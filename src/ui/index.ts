7da18c10a00c27d706ea7fcadd479dba9abc1e56
sshmm <s.shawky92@gmail.com>
2022-07-05 19:11:52 +0200

    add radix collapsible ui component

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,6 +2,7 @@ export * from './Avatar/Avatar';
 export * from './Box/Box';
 export * from './Button/Button';
 export * from './Checkbox/CheckBox';
+export * from './Collapsible/Collapsible';
 export * from './Table/Table';
 export * from './Divider/Divider';
 export * from './Flex';
