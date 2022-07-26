4431761c2f38e917f8dd546a01d42c2595d1c28c
CryptoGraffe <graffe@graffe.co>
2022-07-25 23:55:45 -0700

    trying to use field array / react form hooks

--- src/pages/AddMembersPage/NewMemberList.tsx
+++ src/pages/AddMembersPage/NewMemberList.tsx
@@ -6,43 +6,68 @@ import { Box } from '../../ui';
 
 import { NewMember } from './AddMembersPage';
 import EthAndNameEntry from './NewMemberEntry';
+import { z } from 'zod';
+import { zEthAddressOnly } from '../../forms/formHelpers';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useFieldArray, useForm } from 'react-hook-form';
 
-const NewMemberList = ({
-  newMembers,
-  setNewMembers,
-}: {
-  newMembers: NewMember[];
-  setNewMembers: React.Dispatch<React.SetStateAction<NewMember[]>>;
+const NewMemberList = ({}: // newMembers,
+// setNewMembers,
+{
+  // newMembers: NewMember[];
+  // setNewMembers: React.Dispatch<React.SetStateAction<NewMember[]>>;
 }) => {
+  const newMemberSchema = z.object({
+    newMembers: z.array(
+      z.object({
+        name: z.string(),
+        address: zEthAddressOnly,
+      })
+    ),
+  });
+
+  const formOptions = { resolver: zodResolver(newMemberSchema) };
+  const { register, control, handleSubmit, reset, formState, watch } =
+    useForm(formOptions);
+  const { errors } = formState;
+  const { fields, append, remove } = useFieldArray({
+    name: 'tickets',
+    control,
+  });
+
   const ethNameFocused = (idx: number) => {
     // make sure there is at least idx+1 elements in the namesToAdd Array
-    setNewMembers(prevState => {
-      if (prevState.length - 1 <= idx) {
-        return [
-          ...prevState,
-          {
-            address: '',
-            name: '',
-          },
-        ];
-      }
-      return prevState;
-    });
+    if (fields.length - 1 <= idx) {
+      append({ name: '', address: '' });
+    }
+    // setNewMembers(prevState => {
+    //   if (prevState.length - 1 <= idx) {
+    //     return [
+    //       ...prevState,
+    //       {
+    //         address: '',
+    //         name: '',
+    //       },
+    //     ];
+    //   }
+    //   return prevState;
+    // });
   };
 
   const removeNewMember = (idx: number) => {
-    setNewMembers(prevState => {
-      console.log('original, deleitng', idx);
-      console.log([...prevState]);
-      const changed = [...prevState].filter((value, index) => {
-        console.log('checking value', value);
-        console.log('idx:' + idx + ' index:' + index);
-        return index != idx;
-      });
-      console.log('CHANGED');
-      console.log(changed);
-      return changed;
-    });
+    remove(idx);
+    // setNewMembers(prevState => {
+    //   console.log('original, deleitng', idx);
+    //   console.log([...prevState]);
+    //   const changed = [...prevState].filter((value, index) => {
+    //     console.log('checking value', value);
+    //     console.log('idx:' + idx + ' index:' + index);
+    //     return index != idx;
+    //   });
+    //   console.log('CHANGED');
+    //   console.log(changed);
+    //   return changed;
+    // });
   };
 
   const updateNewMember = (idx: number, nameToAdd: NewMember) => {
@@ -53,10 +78,28 @@ const NewMemberList = ({
     });
   };
 
+  // TODO: typing
+  const onSubmit = (data: any) => {
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(data, null, 4));
+  };
+
   return (
-    <Box>
-      {newMembers.map((nameToAdd, idx) => {
-        return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Box>
+        {/*{newMembers.map((nameToAdd, idx) => {*/}
+        {/*  return (*/}
+        {/*    <EthAndNameEntry*/}
+        {/*      key={idx}*/}
+        {/*      onFocus={() => ethNameFocused(idx)}*/}
+        {/*      onRemove={idx > 0 ? () => removeNewMember(idx) : undefined}*/}
+        {/*      nameToAdd={nameToAdd}*/}
+        {/*      setNameToAdd={(nameToAdd: NewMember) =>*/}
+        {/*        updateNewMember(idx, nameToAdd)*/}
+        {/*      }*/}
+        {/*    />*/}
+        {/*  );*/}
+        {/*})}*/}
+        {fields.map((item, idx) => (
           <EthAndNameEntry
             key={idx}
             onFocus={() => ethNameFocused(idx)}
@@ -66,9 +109,10 @@ const NewMemberList = ({
               updateNewMember(idx, nameToAdd)
             }
           />
-        );
-      })}
-    </Box>
+        ))}
+      </Box>
+      <button type="submit">submitters</button>
+    </form>
   );
 };
 
