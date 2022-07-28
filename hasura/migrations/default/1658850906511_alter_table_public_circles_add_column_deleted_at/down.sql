249a0c71751ade02a026ae4b94f4d532500cf4a3
sshmm <s.shawky92@gmail.com>
2022-07-29 01:38:00 +0200

    revert deleted triggers

--- hasura/migrations/default/1658850906511_alter_table_public_circles_add_column_deleted_at/down.sql
+++ hasura/migrations/default/1658850906511_alter_table_public_circles_add_column_deleted_at/down.sql
@@ -1,4 +1,1 @@
--- Could not auto-generate a down migration.
--- Please write an appropriate down migration for the SQL below:
--- alter table "public"."circles" add column "deleted_at" timestamp
---  null;
+alter table "public"."circles" add column "deleted_at" timestamp;
