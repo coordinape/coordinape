c955fd2d8f19bcc8cc78e53564ac17d876756139
Lawrence Wang <l@lw.io>
2022-08-10 15:03:05 +0200

    don't misidentify errors as "User not circle admin"
    i was getting "User not circle admin" when the real error was that I didn't have AWS configured locally

--- api-lib/circleAdmin.ts
+++ api-lib/circleAdmin.ts
@@ -1,53 +1,41 @@
-import assert from 'assert';
-
 import type {
   VercelRequest,
   VercelResponse,
   VercelApiHandler,
 } from '@vercel/node';
-import { z } from 'zod';
 
 import { composeHasuraActionRequestBody, circleIdInput } from '../src/lib/zod';
 
 import { getUserFromProfileId } from './findUser';
-import { GraphQLError } from './gql/__generated__/zeus';
 import { UnauthorizedError } from './HttpError';
 import { verifyHasuraRequestMiddleware } from './validate';
 
 const middleware =
   (handler: VercelApiHandler) =>
   async (req: VercelRequest, res: VercelResponse): Promise<void> => {
-    try {
-      const {
-        input: { payload: input },
-        session_variables: sessionVariables,
-      } = composeHasuraActionRequestBody(circleIdInput).parse(req.body);
+    const {
+      input: { payload: input },
+      session_variables: sessionVariables,
+    } = composeHasuraActionRequestBody(circleIdInput).parse(req.body);
 
-      // the admin role is validated early by zod
-      if (sessionVariables.hasuraRole === 'admin') {
-        await handler(req, res);
-        return;
-      }
-
-      if (sessionVariables.hasuraRole === 'user') {
-        const { circle_id } = input;
-        const profileId = sessionVariables.hasuraProfileId;
-
-        const { role } = await getUserFromProfileId(profileId, circle_id);
-        assert(isCircleAdmin(role));
-
-        await handler(req, res);
-        return;
-      }
-    } catch (err) {
-      if (err instanceof z.ZodError || err instanceof GraphQLError) {
-        // let the default errorHandlers handle this
-        throw err;
-      }
-      throw new UnauthorizedError('User not circle admin', err);
+    // the admin role is validated early by zod
+    if (sessionVariables.hasuraRole === 'admin') {
+      await handler(req, res);
+      return;
     }
 
-    // Reject request by if not validated above
+    if (sessionVariables.hasuraRole === 'user') {
+      const { circle_id } = input;
+      const profileId = sessionVariables.hasuraProfileId;
+
+      const { role } = await getUserFromProfileId(profileId, circle_id);
+      if (isCircleAdmin(role)) {
+        await handler(req, res);
+        return;
+      }
+    }
+
+    // Reject request if not validated above
     throw new UnauthorizedError('User not circle admin');
   };
 
