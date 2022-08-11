e96d0c33942731fa437e6dc0fae85ce232a27984
CryptoGraffe <graffe@graffe.co>
2022-08-11 00:59:58 -0700

    fixing generated icons to support css stitches prop, switching to some generated icons, deleting old unused icons

--- src/icons/__generated/Plus.tsx
+++ src/icons/__generated/Plus.tsx
@@ -16,13 +16,11 @@ export const SvgPlus = (props: SvgIconProps) => (
     height={16}
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    css={css}
+    css={{ ...css, ...(props.css ?? {}) }}
     viewBox="0 0 16 16"
     {...props}
   >
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
       d="M9 1a1 1 0 1 0-2 0v6H1a1 1 0 0 0 0 2h6v6a1 1 0 1 0 2 0V9h6a1 1 0 1 0 0-2H9V1Z"
       fill="#000"
     />
