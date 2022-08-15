7ee713178436c4d538230cfbc907a39ca1bc0b85
crabsinger <83605543+crabsinger@users.noreply.github.com>
2022-08-15 09:17:13 -0700

    Parse out - as workaround for bash's handling of spaces (#1265)

--- scripts/util/eth.ts
+++ scripts/util/eth.ts
@@ -1,4 +1,9 @@
+function swapDashSpace(s?: string) {
+  return s?.replace(/-/g, ' ');
+}
+
 export const SEED_PHRASE =
+  swapDashSpace(process.env.DEV_SEED_PHRASE) ||
   'test test test test test test test test test test test junk';
 export function getAccountPath(index: number): string {
   return `m/44'/60'/0'/0/${index}`;
