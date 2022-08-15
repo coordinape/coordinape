9e73cd755be62078d357777697f2561e33f7e038
CryptoGraffe <95149165+CryptoGraffe@users.noreply.github.com>
2022-08-15 09:22:09 -0700

    Get started banners for epoch overview page for admins and non-admins (#1269)

---
+++ src/ui/HintBanner.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { styled } from '../stitches.config';
+
+import { Box, Flex, Panel, Text } from '.';
+
+const HintBanner = ({
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <Panel
+      info
+      css={{
+        display: 'grid',
+        gridTemplateColumns: '1fr 3fr',
+        gap: '$md',
+        mb: '$xl',
+        '@sm': { gridTemplateColumns: '1fr' },
+      }}
+    >
+      <Box>
+        <Text h2 normal>
+          {title}
+        </Text>
+      </Box>
+      <Flex column css={{ width: '65%', '@sm': { width: '100%' } }}>
+        {children}
+      </Flex>
+    </Panel>
+  );
+};
+
+export default styled(HintBanner);
