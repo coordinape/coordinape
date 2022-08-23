6b8127a241a6facf3504014ea625fa6563e395f2
Lawrence Wang <l@lw.io>
2022-08-23 14:03:06 +0200

    show only claims for current chain

--- src/components/MainLayout/getMainHeaderData.ts
+++ src/components/MainLayout/getMainHeaderData.ts
@@ -1,10 +1,11 @@
+import { useWeb3React } from '@web3-react/core';
 import { client } from 'lib/gql/client';
 import { useQuery } from 'react-query';
 
 import { useAuthStep } from 'hooks/login';
 import useConnectedAddress from 'hooks/useConnectedAddress';
 
-export const getMainHeaderData = (address: string) =>
+export const getMainHeaderData = (address: string, chainId: number) =>
   client.query(
     {
       organizations: [
@@ -31,6 +32,10 @@ export const getMainHeaderData = (address: string) =>
           where: {
             profile: { address: { _eq: address.toLowerCase() } },
             txHash: { _is_null: true },
+            distribution: {
+              tx_hash: { _is_null: false },
+              vault: { chain_id: { _eq: chainId } },
+            },
           },
         },
         { aggregate: { count: [{}, true] } },
@@ -44,12 +49,13 @@ export const getMainHeaderData = (address: string) =>
 // we can reuse it
 export const useMainHeaderQuery = () => {
   const address = useConnectedAddress();
+  const { chainId } = useWeb3React();
   const [authStep] = useAuthStep();
   return useQuery(
     ['OverviewMenu', address],
-    () => getMainHeaderData(address as string),
+    () => getMainHeaderData(address as string, chainId as number),
     {
-      enabled: !!address && authStep === 'done',
+      enabled: !!address && !!chainId && authStep === 'done',
       staleTime: Infinity,
     }
   );
