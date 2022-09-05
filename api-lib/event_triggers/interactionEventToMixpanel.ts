27e9412853d95bc68b049babba2854003a0554a0
CryptoGraffe <graffe@graffe.co>
2022-09-05 15:40:27 -0600

    pushing event data up to top level instead of nesting

--- api-lib/event_triggers/interactionEventToMixpanel.ts
+++ api-lib/event_triggers/interactionEventToMixpanel.ts
@@ -30,10 +30,10 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
         {
           event_subtype: event.event_subtype,
           id: event.id,
-          data: event.data,
           circle_id: event.circle_id,
           profile_id: event.profile_id,
           org_id: event.org_id,
+          ...(event.data ?? {}),
         }
       );
     }
@@ -46,13 +46,13 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
         {
           event_subtype: event.event_subtype,
           id: event.id,
-          data: event.data,
           circle_id: event.circle_id,
           user_id: event.user_id ? sha256(`${event.user_id}`) : undefined,
           profile_id: event.profile_id
             ? sha256(`${event.profile_id}`)
             : undefined,
           org_id: event.org_id,
+          ...event.data,
         }
       );
     }
