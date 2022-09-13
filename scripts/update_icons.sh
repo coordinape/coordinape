28a5fdab0ba3a28527f15ab2d6731793f6ebf21f
CryptoGraffe <graffe@graffe.co>
2022-09-12 22:13:42 -0700

    chore: updating icons script to handle multiple input dirs, rename to update-icons

---
+++ scripts/update_icons.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# make a tmp dir, copy all the icons into there
+
+rm -rf ./tmpicons
+mkdir tmpicons
+WORK_DIR=./tmpicons
+
+OUT_DIR=src/icons/__generated
+
+# deletes the temp directory
+function cleanup {
+  rm -rf "$WORK_DIR"
+  echo "Deleted temp working directory $WORK_DIR"
+}
+
+# register the cleanup function to be called on the EXIT signal
+trap cleanup EXIT
+
+
+# copy all the various icons into the tmpdir
+# if there are new icon dirs, add them here
+cp src/icons/figma/*svg $WORK_DIR
+#cp src/icons/brands/*svg $WORK_DIR
+
+# cleanup whatever old ones are there
+rm -rf src/icons/__generated/*
+
+npx @svgr/cli --typescript  $WORK_DIR --template ./src/icons/figma/svgr-template.js --svg-props css="{CSS_REPLACE}" --svg-props viewBox="0 0 16 16" --out-dir $OUT_DIR
+sed -i '.bak' 's/\<svg/\<SvgIcon/g' src/icons/__generated/*.tsx
+sed -i '.bak' 's/\<\/svg/\<\/SvgIcon/g' src/icons/__generated/*.tsx
+sed -i '.bak' 's/{CSS_REPLACE}/\{\{ \.\.\.css, \.\.\.\(props\.css \?\? \{\}\) \}\}/g' src/icons/__generated/*.tsx
+rm src/icons/__generated/*.bak
