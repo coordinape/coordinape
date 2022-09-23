ae1681f667db1528327360c88fb486c425cde5d4
Lawrence Wang <l@lw.io>
2022-09-23 03:57:47 -0700

    remove old landing page (#1375)

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,16 @@
 import React from 'react';
 
-import LandingPageApp from 'lib/LandingPageApp';
 import ReactDOM from 'react-dom';
 
 import { initSentry } from 'utils/reporting';
 
 import App from './App';
-import { RENDER_APP } from './utils/domain';
 
 initSentry();
 
-if (RENDER_APP) {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-} else {
-  ReactDOM.render(
-    <React.StrictMode>
-      <LandingPageApp />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
