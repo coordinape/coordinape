ebf91c6cf891a9ce74857db57c297d5705a92fa8
Lawrence Wang <l@lw.io>
2022-09-23 12:17:20 +0800

    remove obsolete OrgLayout

--- src/ui/layouts.tsx
+++ src/ui/layouts.tsx
@@ -54,38 +54,3 @@ export const TwoColumnLayout = ({
     </Box>
   );
 };
-
-export const OrgLayout = ({
-  name,
-  children,
-  css = {},
-}: SingleColumnLayoutProps & { name?: string }) => {
-  return (
-    <SingleColumnLayout css={css}>
-      <Box
-        css={{
-          display: 'flex',
-          alignItems: 'center',
-          width: '100%',
-          mb: '$xl',
-        }}
-      >
-        <ApeAvatar
-          alt="organization"
-          src="/imgs/avatar/placeholder.jpg"
-          style={{
-            width: 36,
-            height: 36,
-            borderRadius: '50%',
-            border: '1px solid rgba(94, 111, 116, 0.7)',
-            marginRight: '16px',
-          }}
-        />
-        <Text css={{ fontWeight: '$bold', fontSize: '$h2', flexGrow: 1 }}>
-          {name}
-        </Text>
-      </Box>
-      {children}
-    </SingleColumnLayout>
-  );
-};
