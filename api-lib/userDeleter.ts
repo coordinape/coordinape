fc63c8eef1bb3d120333d38d5934f2eb40e8dbf4
sshmm <s.shawky92@gmail.com>
2022-10-04 01:01:17 +0200

    Add userDeleter middleware to allow self delete

---
+++ api-lib/userDeleter.ts
@@ -0,0 +1,75 @@
+import type {
+  VercelRequest,
+  VercelResponse,
+  VercelApiHandler,
+} from '@vercel/node';
+
+import {
+  circleIdInput,
+  composeHasuraActionRequestBodyWithApiPermissions,
+} from '../src/lib/zod';
+
+import { getUserFromProfileId } from './findUser';
+import { errorResponseWithStatusCode, UnauthorizedError } from './HttpError';
+import { verifyHasuraRequestMiddleware } from './validate';
+
+const requestSchema = composeHasuraActionRequestBodyWithApiPermissions(
+  circleIdInput,
+  []
+);
+
+const middleware =
+  (handler: VercelApiHandler) =>
+  async (req: VercelRequest, res: VercelResponse): Promise<void> => {
+    const {
+      input: { payload: input },
+      session_variables: sessionVariables,
+    } = await requestSchema.parseAsync(req.body);
+
+    // the admin role is validated early by zod
+    if (sessionVariables.hasuraRole === 'admin') {
+      await handler(req, res);
+      return;
+    }
+
+    if (sessionVariables.hasuraRole === 'user') {
+      const { circle_id } = input;
+      const profileId = sessionVariables.hasuraProfileId;
+
+      const { role, address } = await getUserFromProfileId(
+        profileId,
+        circle_id
+      );
+      if (isCircleAdmin(role)) {
+        await handler(req, res);
+        return;
+      }
+      if (sessionVariables.hasuraAddress === address) {
+        await handler(req, res);
+        return;
+      }
+    }
+
+    if (sessionVariables.hasuraRole === 'api-user') {
+      const { circle_id } = input;
+
+      if (circle_id !== sessionVariables.hasuraCircleId) {
+        return errorResponseWithStatusCode(
+          res,
+          { message: `API Key does not belong to circle ID ${circle_id}` },
+          403
+        );
+      }
+
+      await handler(req, res);
+      return;
+    }
+
+    // Reject request if not validated above
+    throw new UnauthorizedError('User not circle admin');
+  };
+
+const isCircleAdmin = (role: number): boolean => role === 1;
+
+export const authUserDeleterMiddleware = (handler: VercelApiHandler) =>
+  verifyHasuraRequestMiddleware(middleware(handler));
