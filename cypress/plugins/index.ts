0f1ef9a2ff77974b7dcbc66871f25c26875721c6
crabsinger <83605543+crabsinger@users.noreply.github.com>
2022-10-03 17:48:38 -0700

    Revert "add datadog CI tracking"
    This reverts commit e0f9a8789842e2f01b03cc1e9d8eb3438b599fbd.

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -3,8 +3,6 @@
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
 import dotenvPlugin from 'cypress-dotenv';
-import ddPlugin from 'dd-trace/ci/cypress/plugin';
-
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
@@ -14,8 +12,6 @@ import ddPlugin from 'dd-trace/ci/cypress/plugin';
 export default (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-  ddPlugin(on, config);
-
   config = dotenvPlugin(config, { debug: true }, true);
   if (config.env.CI || process.env.CI) {
     config.baseUrl = 'http://localhost:' + config.env.CI_WEB_PORT;
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -4,7 +4,6 @@
 // ***********************************************************
 
 import './commands';
-import 'dd-trace/ci/cypress/support';
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
