477029133880bdd450c3a42c0d6c57b3c1cc40e9
Kevin Siegler <17910833+topocount@users.noreply.github.com>
2022-10-04 12:22:20 -0500

    delete duplicated impl

--- src/pages/ContributionsPage/index.ts
+++ src/pages/ContributionsPage/index.ts
@@ -1,25 +1,3 @@
 import ContributionsPage from './ContributionsPage';
 
-interface LinkedElement {
-  next: () => LinkedElement;
-  prev: () => LinkedElement;
-  idx: number;
-}
-
-type Obj = Record<string, unknown>;
-
-export function createLinkedArray<T extends Obj>(
-  a: Array<T>
-): Array<T & LinkedElement> {
-  const newA: Array<T & LinkedElement> = Array(a.length);
-  for (let i = 0; i < a.length; i++) {
-    newA[i] = {
-      ...a[i],
-      idx: i,
-      next: () => newA[i + 1],
-      prev: () => newA[i - 1],
-    };
-  }
-  return newA;
-}
 export default ContributionsPage;
