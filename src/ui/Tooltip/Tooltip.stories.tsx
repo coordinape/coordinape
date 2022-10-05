aa6fc994727ac191ec5173522d57d0245587f62d
Karelian Pie <devkarelianpie@gmail.com>
2022-10-05 10:00:20 +0300

    feat: Tooltip story

---
+++ src/ui/Tooltip/Tooltip.stories.tsx
@@ -0,0 +1,26 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Text } from '../Text/Text';
+import { Info } from 'icons/__generated';
+
+import { Tooltip as TooltipComponent } from './Tooltip';
+
+export default {
+  title: 'Design System/Components/Tooltip',
+  component: TooltipComponent,
+} as ComponentMeta<typeof TooltipComponent>;
+
+const Template: ComponentStory<typeof TooltipComponent> = () => (
+  <TooltipComponent
+    content={
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+        lobortis consequat orci, quis feugiat sem suscipit sed.
+      </Text>
+    }
+  >
+    <Info />
+  </TooltipComponent>
+);
+
+export const Tooltip = Template.bind({});
