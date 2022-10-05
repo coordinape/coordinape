60df31e08ba339320247a3bb39481d624ae3bab9
Karelian Pie <devkarelianpie@gmail.com>
2022-10-05 12:14:19 +0300

    feat: Icons story

--- src/ui/SvgIcon.stories.tsx
+++ src/ui/SvgIcon.stories.tsx
@@ -1,47 +1,65 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { ArrowLeft } from 'icons/__generated';
+import { theme } from '../stitches.config';
+import { X } from 'icons/__generated';
 
-import { Box, Text } from './index';
+import { Flex, Text } from './index';
 
 export default {
-  title: 'Design System/Components/SvgIcon',
-  component: Box,
-} as ComponentMeta<typeof Box>;
+  title: 'Design System/Components/Icons',
+  component: X,
+  argTypes: {
+    color: {
+      options: Object.keys(theme.colors),
+      control: { type: 'select' },
+      defaultValue: 'black',
+    },
+    size: {
+      options: ['xs', 'sm', 'md', 'lg', 'xl'],
+      control: { type: 'inline-radio' },
+      defaultValue: 'lg',
+    },
+    css: {
+      table: {
+        disable: true,
+      },
+    },
+    nostroke: { control: 'boolean', defaultValue: false },
+  },
+} as ComponentMeta<typeof X>;
 
-const Template: ComponentStory<typeof Box> = args => (
-  <Box
-    css={{
-      display: 'flex',
-      flexDirection: 'column',
-      gap: '$sm',
-    }}
-  >
-    {args.children}
-  </Box>
+const context = require.context('../icons/__generated', true, /.tsx$/);
+const components = context.keys().reduce((accum, path) => {
+  const name = path.substring(2).replace('.tsx', '');
+  return {
+    ...accum,
+    [name]: context(path),
+  };
+}, {});
+
+const Template: ComponentStory<typeof X> = args => (
+  <Flex css={{ flexWrap: 'wrap', gap: '$lg' }}>
+    {Object.keys(components).map(name => {
+      const Icon = (components as any)[name].default;
+      return (
+        <Flex
+          key={name}
+          css={{
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: '$sm',
+          }}
+        >
+          <Icon
+            {...args}
+            size={args.size ?? 'lg'}
+            color={args.color ?? 'black'}
+          />
+          <Text size="small">{name}</Text>
+        </Flex>
+      );
+    })}
+  </Flex>
 );
 
-export const SingleArrowLeft = Template.bind({});
-
-SingleArrowLeft.args = {
-  children: (
-    <>
-      <Box css={{ display: 'flex', gap: '$sm' }}>
-        <Text>xs (10px)</Text>
-        <ArrowLeft size="xs" />
-      </Box>
-      <Box css={{ display: 'flex', gap: '$sm' }}>
-        <Text>sm (12px)</Text>
-        <ArrowLeft size="sm" />
-      </Box>
-      <Box css={{ display: 'flex', gap: '$sm' }}>
-        <Text>md (16px)</Text>
-        <ArrowLeft size="md" />
-      </Box>
-      <Box css={{ display: 'flex', gap: '$sm' }}>
-        <Text>lg (24px)</Text>
-        <ArrowLeft size="lg" />
-      </Box>
-    </>
-  ),
-};
+export const Icons = Template.bind({});
