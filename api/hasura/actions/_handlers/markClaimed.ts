f9e5169154ccec873be9a33f887d22b0bfa9e482
Lawrence Wang <l@lw.io>
2022-10-06 21:46:56 +0800

    make a long method shorter

--- api/hasura/actions/_handlers/markClaimed.ts
+++ api/hasura/actions/_handlers/markClaimed.ts
@@ -5,6 +5,7 @@ import { adminClient } from '../../../../api-lib/gql/adminClient';
 import { getPropsWithUserSession } from '../../../../api-lib/handlerHelpers';
 import { UnprocessableError } from '../../../../api-lib/HttpError';
 import { getProvider } from '../../../../api-lib/provider';
+import { Awaited } from '../../../../api-lib/ts4.5shim';
 import { verifyHasuraRequestMiddleware } from '../../../../api-lib/validate';
 import { Contracts, hasSimpleToken } from '../../../../src/lib/vaults';
 
@@ -33,54 +34,25 @@ export const updateClaims = async (
   txHash: string,
   contracts?: Contracts
 ) => {
+  // 1. query: get data about the given claim
+
   const {
     claims: [claim],
-  } = await adminClient.query({
-    claims: [
-      {
-        where: {
-          id: { _eq: claimId },
-          profile_id: { _eq: profileId },
-        },
-        limit: 1,
-      },
-      {
-        distribution: {
-          id: true,
-          vault_id: true,
-          vault: {
-            symbol: true,
-            chain_id: true,
-            decimals: true,
-            vault_address: true,
-            simple_token_address: true,
-          },
-          epoch: { circle_id: true },
-        },
-      },
-    ],
-  });
-
-  if (!claim) {
+  } = await getClaimData(claimId, profileId);
+  if (!claim)
     throw new UnprocessableError(
       `no claim with id ${claimId} & profile_id ${profileId}`
     );
-  }
 
   const {
     distribution: {
       id: distribution_id,
-      vault_id,
-      vault: {
-        vault_address,
-        decimals,
-        symbol,
-        simple_token_address,
-        chain_id,
-      },
+      vault,
       epoch: { circle_id },
     },
   } = claim;
+
+  // 2. mutate: update all the matching claims with the tx hash
 
   const { update_claims: result } = await adminClient.mutate({
     update_claims: [
@@ -91,7 +63,7 @@ export const updateClaims = async (
           id: { _lte: claimId },
           profile_id: { _eq: profileId },
           distribution: {
-            vault_id: { _eq: vault_id },
+            vault_id: { _eq: vault.id },
             epoch: { circle_id: { _eq: circle_id } },
           },
         },
@@ -104,24 +76,14 @@ export const updateClaims = async (
     ],
   });
 
-  if (!result?.returning.length) {
+  if (!result?.returning.length)
     throw new UnprocessableError('updated 0 claims');
-  }
 
+  // 3. mutate: insert interaction event
+
+  const simple = hasSimpleToken(vault);
   let amount = result?.returning.reduce((a, b) => a + b.new_amount, 0);
-  const simple = hasSimpleToken({ simple_token_address });
-  if (!simple) {
-    if (!contracts) {
-      const provider = getProvider(chain_id);
-      contracts = new Contracts(chain_id, provider, true);
-    }
-    const pps = await contracts.getPricePerShare(
-      vault_address,
-      simple_token_address,
-      decimals
-    );
-    amount = amount * pps.toUnsafeFloat();
-  }
+  if (!simple) amount = await unwrap(amount, vault, contracts);
 
   await adminClient.mutate(
     {
@@ -131,8 +93,8 @@ export const updateClaims = async (
             event_type: 'vault_claim',
             profile_id: profileId,
             data: {
-              vault_id,
-              symbol: simple ? symbol : `Yearn ${symbol}`,
+              vault_id: vault.id,
+              symbol: simple ? vault.symbol : `Yearn ${vault.symbol}`,
               tx_hash: txHash,
               circle_id,
               distribution_id,
@@ -147,4 +109,51 @@ export const updateClaims = async (
   );
 
   return result?.returning.map(x => x.id as number) || [];
+};
+
+const getClaimData = (claimId: number, profileId: number) =>
+  adminClient.query({
+    claims: [
+      {
+        where: {
+          id: { _eq: claimId },
+          profile_id: { _eq: profileId },
+        },
+        limit: 1,
+      },
+      {
+        distribution: {
+          id: true,
+          vault: {
+            id: true,
+            symbol: true,
+            chain_id: true,
+            decimals: true,
+            vault_address: true,
+            simple_token_address: true,
+          },
+          epoch: { circle_id: true },
+        },
+      },
+    ],
+  });
+
+const unwrap = async (
+  amount: number,
+  vault: Awaited<
+    ReturnType<typeof getClaimData>
+  >['claims'][0]['distribution']['vault'],
+  contracts?: Contracts
+) => {
+  const { chain_id, vault_address, simple_token_address, decimals } = vault;
+  if (!contracts) {
+    const provider = getProvider(chain_id);
+    contracts = new Contracts(chain_id, provider, true);
+  }
+  const pps = await contracts.getPricePerShare(
+    vault_address,
+    simple_token_address,
+    decimals
+  );
+  return amount * pps.toUnsafeFloat();
 };
