f9cb4389d8731699cc1c168e7b3f34aaea42cd00
crabsinger <83605543+crabsinger@users.noreply.github.com>
2022-10-28 12:03:21 -0700

    Add vercel request logging

--- api-lib/validate.ts
+++ api-lib/validate.ts
@@ -9,6 +9,11 @@ import {
 
 export const verifyHasuraRequestMiddleware = (handler: VercelApiHandler) => {
   return async (req: VercelRequest, res: VercelResponse) => {
+    // eslint-disable-next-line no-console
+    console.log({
+      'X-Hasura-Request-Id': req.headers.request_id,
+      'X-Vercel-Request-ID': 'X-Vercel-Id',
+    });
     if (
       !req.headers.verification_key ||
       (req.headers.verification_key as string) !== HASURA_EVENT_SECRET
