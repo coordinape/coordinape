5de0600c25810b82865314bacb9ca0ae7c5966f1
Karelian Pie <devkarelianpie@gmail.com>
2022-11-02 10:14:02 +0200

    refactor: Remove unnecessary `title` and `decorators` props

--- src/ui/Image/Image.stories.tsx
+++ src/ui/Image/Image.stories.tsx
@@ -1,29 +1,9 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { withDesign } from 'storybook-addon-designs';
 
 import { Image as ImageComponent } from './Image';
 
 export default {
-  title: 'Design System/Components/Image',
   component: ImageComponent,
-  decorators: [withDesign, Story => <Story />],
-  argTypes: {
-    ref: {
-      table: {
-        disable: true,
-      },
-    },
-    as: {
-      table: {
-        disable: true,
-      },
-    },
-    css: {
-      table: {
-        disable: true,
-      },
-    },
-  },
 } as ComponentMeta<typeof ImageComponent>;
 
 const Template: ComponentStory<typeof ImageComponent> = args => (
