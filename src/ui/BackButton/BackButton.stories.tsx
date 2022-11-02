22c3ea4a8f46069ce3686f07e54daa1f74e82279
Karelian Pie <devkarelianpie@gmail.com>
2022-11-02 09:55:16 +0200

    refactor: Remove unnecessary `title` and `decorators` props

--- src/ui/BackButton/BackButton.stories.tsx
+++ src/ui/BackButton/BackButton.stories.tsx
@@ -1,12 +1,9 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { withDesign } from 'storybook-addon-designs';
 
 import BackButtonComponent from './BackButton';
 
 export default {
-  title: 'Design System/Components/BackButton',
   component: BackButtonComponent,
-  decorators: [withDesign, Story => <Story />],
 } as ComponentMeta<typeof BackButtonComponent>;
 
 const Template: ComponentStory<typeof BackButtonComponent> = () => (
