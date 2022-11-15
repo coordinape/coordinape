c9592d595face610eb049755aa0d1027a70b2e56
CryptoGraffe <graffe@graffe.co>
2022-11-14 21:39:05 -0800

    adding 45s timeout to all adminClient graphql calls

--- api-lib/gql/makeThunder.ts
+++ api-lib/gql/makeThunder.ts
@@ -3,9 +3,10 @@ import { apiFetch, Thunder } from './__generated__/zeus';
 type ThunderOptions = {
   url: string;
   headers: Record<string, string>;
+  timeout?: number;
 };
 
-export const makeThunder = ({ url, headers }: ThunderOptions) =>
+export const makeThunder = ({ url, headers, timeout = 0 }: ThunderOptions) =>
   Thunder(async (...params) =>
-    apiFetch([url, { method: 'POST', headers }])(...params)
+    apiFetch([url, { method: 'POST', headers, timeout: timeout }])(...params)
   );
