4b8455bdf4064c79957babd7443dc2fb7eceedb0
Karelian Pie <devkarelianpie@gmail.com>
2022-11-15 20:01:21 +0200

    refactor: Add down migration

--- hasura/migrations/default/1654202757203_alter_table_public_circle_api_keys_add_column_read_discord/down.sql
+++ hasura/migrations/default/1654202757203_alter_table_public_circle_api_keys_add_column_read_discord/down.sql
@@ -1,4 +1,1 @@
--- Could not auto-generate a down migration.
--- Please write an appropriate down migration for the SQL below:
--- alter table "public"."circle_api_keys" add column "read_discord" boolean
---  not null default 'false';
+alter table "public"."circle_api_keys" drop column "read_discord";
