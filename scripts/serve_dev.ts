251a0384b6c065f384d98f18f18090332c95ad17
Wing <wingfeatherwave@gmail.com>
2022-11-16 10:37:11 -0800

    serve dev icon

--- scripts/serve_dev.ts
+++ scripts/serve_dev.ts
@@ -20,7 +20,7 @@ app.use(express.json({ limit: '10mb' })); // for parsing application/json
 
 if (process.env.DEV_LOGGING) {
   // log all requests to STDOUT
-  app.use(morgan('🚀 :method :url :status :response-time ms'));
+  app.use(morgan('ϟ :method :url :status :response-time ms'));
 }
 
 const port = process.argv[2];
