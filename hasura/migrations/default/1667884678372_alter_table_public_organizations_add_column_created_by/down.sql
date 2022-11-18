943321ec96c2a6c4e73f49f946d844eaa5664298
CryptoGraffe <graffe@graffe.co>
2022-11-18 14:25:17 -0800

    fixing down migration

--- hasura/migrations/default/1667884678372_alter_table_public_organizations_add_column_created_by/down.sql
+++ hasura/migrations/default/1667884678372_alter_table_public_organizations_add_column_created_by/down.sql
@@ -1,4 +1,1 @@
--- Could not auto-generate a down migration.
--- Please write an appropriate down migration for the SQL below:
--- alter table "public"."organizations" add column "created_by" integer
---  null;
+alter table "public"."organizations" drop column "created_by";
