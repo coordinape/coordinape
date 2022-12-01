5d4f05498cb26e0b57e48d2f23f922ab96badb88
Lawrence Wang <l@lw.io>
2022-12-01 13:41:47 -0800

    use contracts.createVault helper more

--- src/lib/vaults/contracts.test.ts
+++ src/lib/vaults/contracts.test.ts
@@ -22,33 +22,16 @@ test('getAvailableTokens does not throw on unrecognized input', () => {
 });
 
 test('getPricePerShare', async () => {
-  const tokenAddress = contracts.getToken('DAI').address;
-  const tx = await contracts.vaultFactory.createCoVault(
-    tokenAddress,
-    AddressZero
-  );
-  const receipt = await tx.wait();
-  const event = receipt.events?.find(e => e.event === 'VaultCreated');
-  const vaultAddress = event?.args?.vault;
+  const { address } = await contracts.createVault('DAI', true);
 
-  const price = await contracts.getPricePerShare(vaultAddress, AddressZero, 18);
+  const price = await contracts.getPricePerShare(address, AddressZero, 18);
   expect(price.toUnsafeFloat()).toBeGreaterThan(1);
 });
 
 test('getPricePerShare with simple token', async () => {
   const tokenAddress = contracts.getToken('DAI').address;
-  const tx = await contracts.vaultFactory.createCoVault(
-    AddressZero,
-    tokenAddress
-  );
-  const receipt = await tx.wait();
-  const event = receipt.events?.find(e => e.event === 'VaultCreated');
-  const vaultAddress = event?.args?.vault;
+  const { address } = await contracts.createVault('DAI');
 
-  const price = await contracts.getPricePerShare(
-    vaultAddress,
-    tokenAddress,
-    18
-  );
+  const price = await contracts.getPricePerShare(address, tokenAddress, 18);
   expect(price.toUnsafeFloat()).toEqual(1);
 });
