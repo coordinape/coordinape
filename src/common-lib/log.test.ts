b23a83a8f915e93f0530978df2b1cdaaec459ce5
topocount <17910833+topocount@users.noreply.github.com>
2022-12-02 11:52:21 -0600

    Implement a Debug Logger (#1720)
    * Implement a new Logger class
    
    Why implement a new logger class? In short, to improve dev experience
    when it comes to:
    - implementing nuanced, complex logic that might need to be extended or
      debugged further later
    - easily muting logging in unit tests (without using a mock)
    - runtime debugging locally
    
    This is all possible because the class uses the debug package
    under the hood: https://github.com/debug-js/debug#debug
    
    Test Plan
    
    unit tests are included and demonstrate how the logger can be used
    
    * Add DEBUG to .env.example
    
    * Rename from Logger to DebugLogger
    
    Logger already exists

---
+++ src/common-lib/log.test.ts
@@ -0,0 +1,35 @@
+import { DebugLogger } from './log';
+
+jest.mock('debug', () => ({
+  __esModule: true,
+  default: jest.fn(() => {
+    function logger(...args: string[]) {
+      logger.log(...args);
+    }
+    logger.log = console.error;
+    return logger;
+  }),
+}));
+
+const consoleLogMock = jest.spyOn(console, 'log').mockImplementation();
+const consoleErrorMock = jest.spyOn(console, 'error').mockImplementation();
+
+afterEach(() => {
+  consoleLogMock.mockClear();
+  consoleErrorMock.mockClear();
+});
+it('can log to stdout', () => {
+  const logger = new DebugLogger('test');
+  logger.log('hello');
+  expect(consoleLogMock).toBeCalledTimes(1);
+  expect(consoleErrorMock).toBeCalledTimes(0);
+  expect(consoleLogMock).toBeCalledWith('hello');
+});
+
+it('can log to stderr', () => {
+  const logger = new DebugLogger('test');
+  logger.error('alert!');
+  expect(consoleLogMock).toBeCalledTimes(0);
+  expect(consoleErrorMock).toBeCalledTimes(1);
+  expect(consoleErrorMock).toBeCalledWith('alert!');
+});
