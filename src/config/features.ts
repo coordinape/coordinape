c33ffa82a56ca00841551fc1bbfc58f940c4ea82
CryptoGraffe <graffe@graffe.co>
2022-12-01 22:06:19 -0800

    add ability to disable distribute evenly for particular circles using our feature flagging mechanism

--- src/config/features.ts
+++ src/config/features.ts
@@ -6,7 +6,8 @@ export type FeatureName =
   | 'theme_switcher'
   | 'discord'
   | 'multichain_login'
-  | 'email_login';
+  | 'email_login'
+  | 'disable_distribute_evenly';
 
 // this is a very simple implementation of build-time feature flags that you can
 // hardcode or set with environment variables
@@ -20,16 +21,27 @@ const staticFeatureFlags: Partial<Record<FeatureName, boolean>> = {
 // this code is safe to use in a non-browser environment because of the typeof
 // check, but our setup in tsconfig-backend.json still flags the use of `window`
 // as an error, so we explicitly ignore it.
-const isLocallyOn = (name: FeatureName) =>
-  // @ts-ignore
-  typeof window !== 'undefined' &&
-  // @ts-ignore
-  window.localStorage.getItem('feature:' + name) === 'true';
+const isLocallyOn = (name: FeatureName, circleId?: number) => {
+  // This is to explicitly (experimentally) disable distribute evenly for Bankless DAO-WIDE (GP,L1,L2) -g
+  if (circleId == 3575 && name == 'disable_distribute_evenly') {
+    return true;
+  }
+
+  return (
+    // @ts-ignore
+    typeof window !== 'undefined' &&
+    // @ts-ignore
+    window.localStorage.getItem('feature:' + name) === 'true'
+  );
+};
 
 // we make the export a function so that we can implement run-time feature flags
 // in the future without having to change calling code
 
-export const isFeatureEnabled = (featureName: FeatureName): boolean =>
-  !!staticFeatureFlags[featureName] || isLocallyOn(featureName);
+export const isFeatureEnabled = (
+  featureName: FeatureName,
+  circleId?: number
+): boolean =>
+  !!staticFeatureFlags[featureName] || isLocallyOn(featureName, circleId);
 
 export default isFeatureEnabled;
