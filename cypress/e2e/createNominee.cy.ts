05e2e2a02ffe98c486082cec04ebc1881b058268
zashton <17747090+zashton@users.noreply.github.com>
2022-12-14 20:44:49 +0800

    Update createNominee.cy.ts

--- cypress/e2e/createNominee.cy.ts
+++ cypress/e2e/createNominee.cy.ts
@@ -17,26 +17,26 @@ context('Coordinape', () => {
     });
   });
   it('can create a nominee', () => {
-    // cy.visit(`/circles/${circleId}/members`);
-    // cy.login();
-    // cy.contains('Nominate Member', { timeout: 120000 }).click();
-    // // enter the nominee creation modal and fill it out
-    // cy.get('[name=name]').click().type('Satoshi');
-    // cy.get('[name=address]')
-    //   .click()
-    //   .wait(1000)
-    //   .type('0x51508887C3FEf0b4390091C5A4B2B91562881526');
-    // cy.contains('Why are you nominating')
-    //   .click()
-    //   .wait(1000)
-    //   .type("It's annoying that I need to fill forty whole characters here.");
-    // cy.get('button').contains('Nominate Member').click();
-    // cy.contains('You have successfully Nominated Satoshi', {
-    //   timeout: 120000,
-    // }).should('be.visible');
-    // cy.get('button').contains('Back').click().wait(5000);
-    // cy.contains('Satoshi').click();
-    // cy.contains('Vouch for Satoshi', { timeout: 120000 });
+    cy.visit(`/circles/${circleId}/members`);
+    cy.login();
+    cy.contains('Nominate Member', { timeout: 120000 }).click();
+    // enter the nominee creation modal and fill it out
+    cy.get('[name=name]').click().type('Satoshi');
+    cy.get('[name=address]')
+      .click()
+      .wait(1000)
+      .type('0x51508887C3FEf0b4390091C5A4B2B91562881526');
+    cy.contains('Why are you nominating')
+      .click()
+      .wait(1000)
+      .type("It's annoying that I need to fill forty whole characters here.");
+    cy.get('button').contains('Nominate Member').click();
+    cy.contains('You have successfully Nominated Satoshi', {
+      timeout: 120000,
+    }).should('be.visible');
+    cy.get('button').contains('Back').click().wait(5000);
+    cy.contains('Satoshi').click();
+    cy.contains('Vouch for Satoshi', { timeout: 120000 });
     //cy.contains('GQL Query Error', { timeout: 45000 }).should('not.exist');
   });
 });
