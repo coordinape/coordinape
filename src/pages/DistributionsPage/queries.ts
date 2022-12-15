79bc677b31ddcfc07adcbdec311dcfbc46634c34
Mark Howard <bark4mark@gmail.com>
2022-12-15 10:02:49 +0000

    feat: adding hedgey table to the claims page

--- src/pages/DistributionsPage/queries.ts
+++ src/pages/DistributionsPage/queries.ts
@@ -6,6 +6,19 @@ import type { Contracts } from 'lib/vaults';
 
 import type { Awaited } from 'types/shim';
 
+export const getProfileIds = async (addresses: string[]) => {
+  const { profiles } = await client.query({
+    profiles: [
+      { where: { address: { _in: addresses } } },
+      {
+        id: true,
+        address: true,
+      },
+    ],
+  });
+  return profiles;
+};
+
 export const getEpochData = async (
   epochId: number,
   myAddress?: string,
@@ -129,8 +142,8 @@ export const getPreviousLockedTokenDistribution = async (epochId: number) => {
         },
         {
           id: true,
-          distribution_json: [{}, true],
           tx_hash: true,
+          chain_id: true,
         },
       ],
     },
