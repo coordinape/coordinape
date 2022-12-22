26da66ff7183c35093aba78ef5cbcb319e441518
Mark Howard <bark4mark@gmail.com>
2022-12-22 13:37:05 +0000

    fix: select contents should show above other elements

--- src/ui/Select/Select.tsx
+++ src/ui/Select/Select.tsx
@@ -152,7 +152,7 @@ export const Select = (
             <ChevronDown color="neutral" />
           </SelectIcon>
         </SelectTrigger>
-        <SelectContent>
+        <SelectContent style={{ zIndex: 6 }}>
           <SelectScrollUpButton>
             <ChevronUp color="neutral" />
           </SelectScrollUpButton>
