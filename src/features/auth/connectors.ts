007813a3e8e922bb7198dc3f306c545df206e5ba
Lawrence Wang <l@lw.io>
2022-12-23 14:50:41 -0800

    Clean up some environment variables (#1791)
    * use REACT_APP_INFURA_PROJECT_ID everywhere when setting up providers, for consistency
    * rename ETHEREUM_RPC_URL to HARDHAT_ARCHIVE_RPC_URL, to better express its purpose

--- src/features/auth/connectors.ts
+++ src/features/auth/connectors.ts
@@ -9,8 +9,8 @@ import { loginSupportedChainIds } from 'common-lib/constants';
 import { EConnectorNames } from 'config/constants';
 import { INFURA_PROJECT_ID } from 'config/env';
 
-export const MAINNET_RPC_URL = `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`;
-export const GOERLI_RPC_URL = `https://goerli.infura.io/v3/${INFURA_PROJECT_ID}`;
+const MAINNET_RPC_URL = `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`;
+const GOERLI_RPC_URL = `https://goerli.infura.io/v3/${INFURA_PROJECT_ID}`;
 
 const injected = new InjectedConnector({
   supportedChainIds: Object.keys(loginSupportedChainIds).map(n =>
