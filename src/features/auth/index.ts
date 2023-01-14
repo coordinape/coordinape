346d9db7d3e74ad4b912cf50ed7d54fe1b5d579e
Lawrence Wang <l@lw.io>
2023-01-13 17:42:32 -0800

    show warning on vaults page for email-based wallets (#1822)

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,10 +1,16 @@
+import { PROVIDER_TYPE as MAGIC_PROVIDER_TYPE } from './magic';
 import { useAuthStore } from './store';
 
-export { getAuthToken, setAuthToken } from './token';
-export { useWalletStatus } from './useWalletStatus';
 export { RequireAuth } from './RequireAuth';
 export { useAuthStore } from './store';
+export { getAuthToken, setAuthToken } from './token';
+export { useWalletStatus } from './useWalletStatus';
 
 export const useIsLoggedIn = () => {
   return useAuthStore(state => state.step) === 'done';
+};
+
+export const useIsEmailWallet = () => {
+  const providerType = useAuthStore(state => state.providerType);
+  return providerType === MAGIC_PROVIDER_TYPE;
 };
