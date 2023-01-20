a1a5dbb6b6679250dcfeaaa096a3d0d4bc9d952a
CryptoGraffe <graffe@graffe.co>
2023-01-20 12:40:13 -0800

    require operation name for frontend zeus client

--- src/lib/gql/client.ts
+++ src/lib/gql/client.ts
@@ -4,7 +4,17 @@ import isEmpty from 'lodash/isEmpty';
 import { REACT_APP_HASURA_URL } from '../../config/env';
 import { TEST_SKIP_AUTH } from 'utils/testing/api';
 
-import { apiFetch, Thunder } from './__generated__/zeus';
+import {
+  apiFetch,
+  FetchFunction,
+  fullChainConstruct,
+  GenericOperation,
+  GraphQLTypes,
+  InputType,
+  OperationOptions,
+  ValueTypes,
+} from './__generated__/zeus';
+import { Ops } from './__generated__/zeus/const';
 
 let mockHeaders: Record<string, string> = {};
 
@@ -12,8 +22,24 @@ export const setMockHeaders = (h: Record<string, string>) => {
   mockHeaders = h;
 };
 
+export const ThunderButCool =
+  (fn: FetchFunction) =>
+  <
+    O extends keyof typeof Ops,
+    R extends keyof ValueTypes = GenericOperation<O>
+  >(
+    operation: O
+  ) =>
+  <Z extends ValueTypes[R]>(
+    o: Z | ValueTypes[R],
+    ops: Omit<OperationOptions, 'operationName'> & { operationName: string }
+  ) =>
+    fullChainConstruct(fn)(operation)(o as any, ops) as Promise<
+      InputType<GraphQLTypes[R], Z>
+    >;
+
 const makeThunder = (headers = {}) =>
-  Thunder(async (...params) =>
+  ThunderButCool(async (...params) =>
     apiFetch([
       REACT_APP_HASURA_URL,
       {
@@ -31,6 +57,9 @@ const makeThunder = (headers = {}) =>
   );
 
 const thunder = makeThunder();
+
+// const t = ReturnType<thunder('query')>
+// type thunderParams = Parameters<typeof Return thunder('query')>;
 
 export const client = {
   query: thunder('query'),
