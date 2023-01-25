8d2d94a013185aff523db617dea38a998d01c39c
crabsinger <83605543+crabsinger@users.noreply.github.com>
2023-01-25 13:03:02 -0800

    Add distributions table `updated_at` trigger

--- hasura/migrations/default/1674672458652_add_updated_at_trigger_missing_tables/down.sql
+++ hasura/migrations/default/1674672458652_add_updated_at_trigger_missing_tables/down.sql
@@ -1,1 +1,2 @@
 DROP TRIGGER IF EXISTS set_public_profiles_updated_at on "public"."profiles";
+DROP TRIGGER IF EXISTS set_public_distributions_updated_at on "public"."distributions";
--- hasura/migrations/default/1674672458652_add_updated_at_trigger_missing_tables/up.sql
+++ hasura/migrations/default/1674672458652_add_updated_at_trigger_missing_tables/up.sql
@@ -1,6 +1,27 @@
-CREATE TRIGGER "set_public_profiles_updated_at"
-BEFORE UPDATE ON "public"."profiles"
-FOR EACH ROW
-EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
-COMMENT ON TRIGGER "set_public_profiles_updated_at" ON "public"."profiles"
-IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+-- profiles
+DO $$
+BEGIN
+  CREATE TRIGGER "set_public_profiles_updated_at"
+    BEFORE UPDATE ON "public"."profiles"
+  FOR EACH ROW
+    EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
+    COMMENT ON TRIGGER "set_public_profiles_updated_at" ON "public"."profiles"
+  IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+EXCEPTION
+    WHEN duplicate_object
+      THEN RAISE NOTICE 'updated_at trigger already exists. Ignoring...';
+END$$;
+
+-- distributions
+DO $$
+BEGIN
+  CREATE TRIGGER "set_public_distributions_updated_at"
+    BEFORE UPDATE ON "public"."distributions"
+  FOR EACH ROW
+    EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
+    COMMENT ON TRIGGER "set_public_distributions_updated_at" ON "public"."distributions"
+  IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+EXCEPTION
+    WHEN duplicate_object
+      THEN RAISE NOTICE 'updated_at trigger already exists. Ignoring...';
+END$$;
