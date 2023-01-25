e7aa7afc3c1003c0e89bf17fae35b6a0bf7390f4
CryptoGraffe <graffe@graffe.co>
2023-01-24 22:08:26 -0800

    new theme based loading animation, removed a bit of MUI

--- src/components/LoadingModal.tsx
+++ src/components/LoadingModal.tsx
@@ -1,30 +1,24 @@
-import {
-  CircularProgress,
-  Modal,
-  Typography,
-  makeStyles,
-} from '@material-ui/core';
+import { Modal, makeStyles } from '@material-ui/core';
 
-const useStyles = makeStyles(theme => ({
+import { keyframes } from '../stitches.config';
+import { Box, Text } from '../ui';
+
+const useStyles = makeStyles(() => ({
   modal: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  content: {
-    outline: 'none',
-    padding: theme.spacing(4),
-    userSelect: `none`,
-    textAlign: 'center',
-  },
-  title: {
-    fontSize: 16,
-    textAlign: 'center',
-    color: theme.colors.secondaryText,
-    fontWeight: 500,
-    marginTop: 16,
-  },
 }));
+
+const rotation = keyframes({
+  '0%': { transform: 'rotate(0deg)' },
+  '100%': { transform: 'rotate(360deg)' },
+});
+
+const animloader = keyframes({
+  '50%': { transform: 'scale(1) translate(-50%, -50%);' },
+});
 
 export const LoadingModal = (props: {
   visible: boolean;
@@ -42,18 +36,46 @@ export const LoadingModal = (props: {
       onClose={onClose}
       open={visible}
     >
-      <div
-        className={classes.content}
+      <Box
+        css={{
+          outline: 'none',
+          padding: '$sm',
+          userSelect: `none`,
+          textAlign: 'center',
+        }}
         data-note={`loading-${props.note || ''}`}
         data-testid={`loading-${props.note || ''}`}
       >
-        <CircularProgress size={80} />
-        {text && (
-          <Typography className={classes.title} component="div">
-            {text}
-          </Typography>
-        )}
-      </div>
+        <Box
+          css={{
+            width: '96px',
+            height: '96px',
+            margin: '15px auto',
+            position: 'relative',
+            color: '#FFF',
+            boxSizing: 'border-box',
+            animation: `${rotation} 1s linear infinite`,
+            '&::after, &::before': {
+              content: '',
+              boxSizing: 'border-box',
+              position: 'absolute',
+              width: '48px',
+              height: '48px',
+              top: '50%',
+              left: '50%',
+              transform: 'scale(0.5) translate(0, 0)',
+              backgroundColor: '#FFF',
+              borderRadius: '50%',
+              animation: `${animloader} 1s infinite ease-in-out`,
+            },
+            '&::before': {
+              backgroundColor: '$cta',
+              transform: 'scale(0.5) translate(-96px, -96px)',
+            },
+          }}
+        ></Box>
+        {text && <Text>{text}</Text>}
+      </Box>
     </Modal>
   );
 };
