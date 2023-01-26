9aba36fcf5373c8dd02973d111bba6854b9a3c8a
sshmm <34943689+sshmm@users.noreply.github.com>
2023-01-26 19:37:29 +0200

    Remove user name field from create circle form (#1864)
    * Remove user name field
    
    * fetch data using react-query
    
    * fix rebase problems
    
    * handle landing on new-circle page without name
    
    * fix createcirclepage unit test
    
    * fix upload circle avatar saving
    
    * revert useConnectedAddress changes

---
+++ src/pages/CreateCirclePage/queries.ts
@@ -0,0 +1,50 @@
+import { client } from 'lib/gql/client';
+
+import { Awaited } from 'types/shim';
+
+export const getCreateCircleData = async (address: string) => {
+  const { users: myUsers, profiles } = await client.query(
+    {
+      users: [
+        {
+          where: {
+            _and: [
+              { circle: { deleted_at: { _is_null: true } } },
+              { address: { _ilike: address } },
+            ],
+          },
+        },
+        {
+          name: true,
+          role: true,
+          circle: {
+            organization: {
+              id: true,
+              name: true,
+              sample: true,
+            },
+          },
+        },
+      ],
+      profiles: [
+        {
+          where: { address: { _ilike: address } },
+          limit: 1,
+        },
+        {
+          name: true,
+        },
+      ],
+    },
+    {
+      operationName: 'getCreateCircleData',
+    }
+  );
+  const myProfile = profiles.pop();
+  return { myUsers, myProfile };
+};
+
+export const QUERY_KEY_CREATE_CIRCLE = 'getCreateCircleData';
+export type CreateCircleQueryData = Awaited<
+  ReturnType<typeof getCreateCircleData>
+>;
