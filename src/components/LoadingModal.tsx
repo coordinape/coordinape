e5798f239bf0e29f1f28a75a7bdc52f98c109087
Graffe <95149165+CryptoGraffe@users.noreply.github.com>
2023-01-25 18:35:46 -0800

    New stitches friendly loading modal (#1867)
    * new theme based loading animation, removed a bit of MUI
    
    * use radix for loading modal
    
    * use radix
    
    * Fix CirclesPage test
    
    The jest.mock wasn't estup properly
    
    * adding role="progressbar" to the new loading modal
    
    Co-authored-by: Wing <wingfeatherwave@gmail.com>
    Co-authored-by: crabsinger <83605543+crabsinger@users.noreply.github.com>

--- src/components/LoadingModal.tsx
+++ src/components/LoadingModal.tsx
@@ -1,30 +1,14 @@
-import {
-  CircularProgress,
-  Modal,
-  Typography,
-  makeStyles,
-} from '@material-ui/core';
+import { keyframes } from '../stitches.config';
+import { Box, Text, Modal } from 'ui';
 
-const useStyles = makeStyles(theme => ({
-  modal: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  content: {
-    outline: 'none',
-    padding: theme.spacing(4),
-    userSelect: `none`,
-    textAlign: 'center',
-  },
-  title: {
-    fontSize: 16,
-    textAlign: 'center',
-    color: theme.colors.secondaryText,
-    fontWeight: 500,
-    marginTop: 16,
-  },
-}));
+const rotation = keyframes({
+  '0%': { transform: 'rotate(0deg)' },
+  '100%': { transform: 'rotate(360deg)' },
+});
+
+const animloader = keyframes({
+  '50%': { transform: 'scale(1) translate(-50%, -50%);' },
+});
 
 export const LoadingModal = (props: {
   visible: boolean;
@@ -32,28 +16,50 @@ export const LoadingModal = (props: {
   text?: string;
   note?: string;
 }) => {
-  const classes = useStyles();
   const { onClose, text, visible } = props;
 
   return (
-    <Modal
-      className={classes.modal}
-      disableBackdropClick
-      onClose={onClose}
-      open={visible}
-    >
-      <div
-        className={classes.content}
+    <Modal loader open={visible} onOpenChange={onClose}>
+      <Box
+        css={{
+          outline: 'none',
+          userSelect: `none`,
+          textAlign: 'center',
+        }}
         data-note={`loading-${props.note || ''}`}
         data-testid={`loading-${props.note || ''}`}
+        role="progressbar"
       >
-        <CircularProgress size={80} />
-        {text && (
-          <Typography className={classes.title} component="div">
-            {text}
-          </Typography>
-        )}
-      </div>
+        <Box
+          css={{
+            width: '96px',
+            height: '96px',
+            margin: '15px auto',
+            position: 'relative',
+            color: '#FFF',
+            boxSizing: 'border-box',
+            animation: `${rotation} 1s linear infinite`,
+            '&::after, &::before': {
+              content: '',
+              boxSizing: 'border-box',
+              position: 'absolute',
+              width: '48px',
+              height: '48px',
+              top: '50%',
+              left: '50%',
+              transform: 'scale(0.5) translate(0, 0)',
+              backgroundColor: '$cta',
+              borderRadius: '50%',
+              animation: `${animloader} 1s infinite ease-in-out`,
+            },
+            '&::before': {
+              backgroundColor: '#fff',
+              transform: 'scale(0.5) translate(-96px, -96px)',
+            },
+          }}
+        ></Box>
+        {text && <Text>{text}</Text>}
+      </Box>
     </Modal>
   );
 };
