328088e053839f469d856a0f2ab418140aa9e481
crabsinger <83605543+crabsinger@users.noreply.github.com>
2023-02-09 17:33:40 -0800

    test setup for epochs and users

--- api-test/hasura/event_triggers/createActivity.payloads.ts
+++ api-test/hasura/event_triggers/createActivity.payloads.ts
@@ -1,35 +1,107 @@
-export const contributions_payload = {
-  event: {
-    data: {
-      new: {
-        circle_id: 373,
-        created_at: '2023-02-08T19:25:47.755097+00:00',
-        created_with_api_key_hash: null,
-        datetime_created: '2023-02-08T19:25:47.755097+00:00',
-        deleted_at: null,
-        description: '123123123',
-        id: 538,
-        updated_at: '2023-02-08T19:25:47.755097+00:00',
-        user_id: 187,
+export const payloads = {
+  contribution_insert: {
+    event: {
+      data: {
+        new: {
+          circle_id: 373,
+          created_at: '2023-02-08T19:25:47.755097+00:00',
+          created_with_api_key_hash: null,
+          datetime_created: '2023-02-08T19:25:47.755097+00:00',
+          deleted_at: null,
+          description: '123123123',
+          id: 538,
+          updated_at: '2023-02-08T19:25:47.755097+00:00',
+          user_id: 187,
+        },
+        old: null,
       },
-      old: null,
+      op: 'INSERT',
     },
-    op: 'INSERT',
+    table: { name: 'contributions', schema: 'public' },
+    trigger: { name: 'createActivity' },
   },
-  table: { name: 'contributions', schema: 'public' },
-  trigger: { name: 'createActivity' },
-};
+  user_insert: {
+    event: {
+      data: {
+        new: {
+          address: '0xfd12fc66a395245f404a58729425472369b771a8',
+          bio: null,
+          circle_id: 15,
+          created_at: '2023-02-08T19:47:32.740765',
+          deleted_at: null,
+          entrance: 'manual-address-entry',
+          epoch_first_visit: true,
+          fixed_non_receiver: false,
+          fixed_payment_amount: 0,
+          give_token_received: 0,
+          give_token_remaining: 100,
+          id: 267,
+          name: 'testtt',
+          non_giver: false,
+          non_receiver: false,
+          role: 0,
+          starting_tokens: 100,
+          updated_at: '2023-02-08T19:47:32.740765',
+        },
+        old: null,
+      },
+      op: 'INSERT',
+    },
+    table: {
+      name: 'users',
+      schema: 'public',
+    },
+    trigger: {
+      name: 'usersInsertCreateActivity',
+    },
+  },
+  epoch_insert: {
+    event: {
+      data: {
+        new: {
+          circle_id: 15,
+          created_at: '2023-02-08T19:46:37.227413',
+          days: null,
+          description: null,
+          end_date: '2023-03-08T19:46:00+00:00',
+          ended: false,
+          grant: 0,
+          id: 47,
+          notified_before_end: null,
+          notified_end: null,
+          notified_start: null,
+          number: null,
+          regift_days: 1,
+          repeat: 0,
+          repeat_data: null,
+          repeat_day_of_month: 0,
+          start_date: '2023-02-09T19:46:00+00:00',
+          updated_at: '2023-02-08T19:46:37.227413',
+        },
+        old: null,
+      },
+      op: 'INSERT',
+    },
+    table: {
+      name: 'epoches',
+      schema: 'public',
+    },
+    trigger: {
+      name: 'epochInsertCreateActivity',
+    },
+  },
 
-export const invalid_payload = {
-  event: {
-    data: {
-      new: {
-        user_id: 187,
+  invalid_payload: {
+    event: {
+      data: {
+        new: {
+          user_id: 187,
+        },
+        old: null,
       },
-      old: null,
+      op: 'INSERT',
     },
-    op: 'INSERT',
+    table: { name: 'unknown', schema: 'public' },
+    trigger: { name: 'createActivity' },
   },
-  table: { name: 'unknown', schema: 'public' },
-  trigger: { name: 'createActivity' },
 };
--- api-test/hasura/event_triggers/createActivity.test.ts
+++ api-test/hasura/event_triggers/createActivity.test.ts
@@ -4,10 +4,9 @@ import handler from '../../../api-lib/event_triggers/createActivity';
 import { adminClient } from '../../../api-lib/gql/adminClient';
 import { insertActivity } from '../../../api-lib/gql/mutations';
 
-import {
-  contributions_payload,
-  invalid_payload,
-} from './createActivity.payloads';
+import { payloads } from './createActivity.payloads';
+
+const { invalid_payload, contribution_insert } = payloads;
 
 jest.mock('../../../api-lib/gql/adminClient', () => ({
   adminClient: { query: jest.fn() },
@@ -19,7 +18,7 @@ jest.mock('../../../api-lib/gql/mutations', () => ({
 
 describe('Create Activity Event Trigger', () => {
   describe('#handler', () => {
-    test('throws an error on unknown table name', async () => {
+    beforeEach(() => {
       (adminClient.query as jest.Mock).mockImplementation(() =>
         Promise.resolve({
           circles_by_pk: {
@@ -29,7 +28,8 @@ describe('Create Activity Event Trigger', () => {
           },
         })
       );
-
+    });
+    test('throws an error on unknown table name', async () => {
       try {
         const req = { body: invalid_payload } as VercelRequest;
         const res = {
@@ -47,6 +47,70 @@ describe('Create Activity Event Trigger', () => {
     });
 
     test('can receive contribution insert and inserts new activty', async () => {
+      try {
+        const req = { body: payloads.contribution_insert } as VercelRequest;
+        const res = {
+          status: jest.fn(() => res),
+          json: jest.fn(),
+        } as unknown as VercelResponse;
+
+        await handler(req, res);
+
+        expect(insertActivity as jest.Mock).toBeCalledTimes(1);
+        expect(insertActivity as jest.Mock).toBeCalledWith(
+          expect.objectContaining({
+            circle_id: 373,
+            organization_id: 9,
+            contribution_id: 538,
+            action: 'contributions_insert',
+            actor_profile_id: 187,
+          })
+        );
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({ message: 'activity recorded' });
+      } catch (e) {
+        console.error(e);
+      }
+    });
+
+    xtest('can receive users insert and inserts new activty', async () => {
+      (adminClient.query as jest.Mock).mockImplementation(() =>
+        Promise.resolve({
+          circles_by_pk: {
+            id: 3,
+            organization_id: 9,
+            users: [{ profile: { id: 187 } }],
+          },
+        })
+      );
+
+      try {
+        const req = { body: contribution_insert } as VercelRequest;
+        const res = {
+          status: jest.fn(() => res),
+          json: jest.fn(),
+        } as unknown as VercelResponse;
+
+        await handler(req, res);
+
+        expect(insertActivity as jest.Mock).toBeCalledTimes(1);
+        expect(insertActivity as jest.Mock).toBeCalledWith(
+          expect.objectContaining({
+            circle_id: 373,
+            organization_id: 9,
+            contribution_id: 538,
+            action: 'contributions_insert',
+            actor_profile_id: 187,
+          })
+        );
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({ message: 'activity recorded' });
+      } catch (e) {
+        console.error(e);
+      }
+    });
+
+    xtest('can receive epochs insert and inserts new activty', async () => {
       (adminClient.query as jest.Mock).mockImplementation(() =>
         Promise.resolve({
           circles_by_pk: {
