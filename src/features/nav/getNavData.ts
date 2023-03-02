a07412986e236afe33e1956fb465372954151432
Lawrence Wang <l@lw.io>
2023-03-02 15:57:48 +0800

    show selected circle in side nav
    ...even when you're not a member
    - hide + button (create a circle) if you're not a circle admin in the org
    - hide Map link (for now)
    - rename isMember to isCircleMember for clarity

--- src/features/nav/getNavData.ts
+++ src/features/nav/getNavData.ts
@@ -15,14 +15,7 @@ export const getNavData = (address: string, chainId: number) =>
           name: true,
           logo: true,
           circles: [
-            {
-              where: {
-                users: {
-                  address: { _eq: address.toLowerCase() },
-                  deleted_at: { _is_null: true },
-                },
-              },
-            },
+            {},
             {
               id: true,
               name: true,
