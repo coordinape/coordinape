f6b7e1cd35819c71133655e2068175205c8d1a86
Wing <wingfeatherwave@gmail.com>
2023-03-03 10:23:31 -0800

    mint blob

--- src/features/cosoul/MintPage.tsx
+++ src/features/cosoul/MintPage.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 
+import { rotate } from 'keyframes';
 import { NavLink } from 'react-router-dom';
 
 import isFeatureEnabled from 'config/features';
 import { paths } from 'routes/paths';
-import { Flex, Button, Text, Modal } from 'ui';
+import { Flex, Button, Text, Modal, Panel, Box } from 'ui';
 import { SingleColumnLayout } from 'ui/layouts';
 
 export const MintPage = () => {
@@ -19,9 +20,23 @@ export const MintPage = () => {
     <>
       <SingleColumnLayout css={{ m: 'auto' }}>
         <Flex column alignItems="start" css={{ gap: '$md' }}>
-          <Text color="cta" h1 css={{ fontSize: '110px' }}>
+          <Text h2 display color="secondary">
             View pGIVE
           </Text>
+          <Flex css={{ width: '100%', justifyContent: 'space-around' }}>
+            <Box
+              css={{
+                background:
+                  'linear-gradient(rgb(198 219 137), rgb(34 119 127))',
+                animation: `${rotate} 50s cubic-bezier(0.8, 0.2, 0.2, 0.8) alternate infinite`,
+                borderRadius: '30% 70% 70% 30% / 30% 30% 70% 70%;',
+                width: '400px',
+                height: '400px',
+                filter: 'blur(calc(400px / 5))',
+              }}
+            ></Box>
+            <Panel>deets</Panel>
+          </Flex>
           <Flex css={{ mt: '$lg', gap: '$md' }}>
             <Button
               color="secondary"
