0e456adace29621c9008f020cac6bb930ccc9bdb
CryptoGraffe <graffe@graffe.co>
2023-03-09 12:36:10 -0800

    removing old migrations

--- hasura/migrations/default/1678169491523_alter_table_public_reactions_add_unique_profile_id_activity_id_reaction/down.sql
+++
@@ -1,1 +0,0 @@
-alter table "public"."reactions" drop constraint "reactions_profile_id_activity_id_reaction_key";
--- hasura/migrations/default/1678169491523_alter_table_public_reactions_add_unique_profile_id_activity_id_reaction/up.sql
+++
@@ -1,1 +0,0 @@
-alter table "public"."reactions" add constraint "reactions_profile_id_activity_id_reaction_key" unique ("profile_id", "activity_id", "reaction");
