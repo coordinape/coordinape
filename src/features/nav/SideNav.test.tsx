aa1670aa54e4ccae48082e2cda082c05eed36da1
crabsinger <83605543+crabsinger@users.noreply.github.com>
2023-03-16 11:54:51 -0700

    Add test for Other Circes in nav

--- src/features/nav/SideNav.test.tsx
+++ src/features/nav/SideNav.test.tsx
@@ -60,11 +60,64 @@ test('show circle links for distributions route', async () => {
     );
   });
 
-  expect(screen.getByText('tonka')).toBeTruthy();
-  expect(screen.getByText(fixtures.organization.name)).toBeTruthy();
-  expect(screen.getByText(fixtures.circle.name)).toBeTruthy();
-  expect(screen.getByText('GIVE')).toBeTruthy();
-  expect(screen.getByText('Members')).toBeTruthy();
-  expect(screen.getByText('Admin')).toBeTruthy();
-  expect(screen.getByText('Overview')).toBeTruthy();
+  expect(screen.getByText('tonka'));
+  expect(screen.getByText(fixtures.organization.name));
+  expect(screen.getByText(fixtures.circle.name));
+  expect(screen.getByText('My Circles'));
+  expect(screen.getByText('GIVE'));
+  expect(screen.getByText('Members'));
+  expect(screen.getByText('Admin'));
+  expect(screen.getByText('Overview'));
+});
+
+test('show circle links for org members under "other circles"', async () => {
+  (useNavQuery as any).mockReturnValue({
+    data: {
+      organizations: [
+        {
+          id: fixtures.circle.organization_id,
+          name: fixtures.organization.name,
+          logo: fixtures.organization.logo,
+          myCircles: [],
+          otherCircles: [
+            {
+              id: fixtures.circle.id,
+              name: fixtures.circle.name,
+              logo: fixtures.circle.logo,
+            },
+          ],
+          members: [],
+        },
+      ],
+      profile: { name: 'tonka' },
+      claims_aggregate: { aggregate: { count: 0 } },
+    },
+  });
+
+  const Harness = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      navigate(`/circles/${fixtures.circle.id}/members`);
+    }, []);
+
+    return <SideNav />;
+  };
+
+  await act(async () => {
+    await render(
+      <TestWrapper withWeb3>
+        <Harness />
+      </TestWrapper>
+    );
+  });
+
+  expect(screen.getByText('tonka'));
+  expect(screen.getByText('Other Circles'));
+  expect(screen.getByText(fixtures.organization.name));
+  expect(screen.getByText(fixtures.circle.name));
+  expect(screen.getByText('Members'));
+
+  expect(screen.queryByText('Admin')).toBeFalsy();
+  expect(screen.queryByText('GIVE')).toBeFalsy();
 });
