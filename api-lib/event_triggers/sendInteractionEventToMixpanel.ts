fa19450f5b56949d406f7462f282223e5eecc430
Lawrence Wang <l@lw.io>
2023-03-22 19:17:06 -0700

    add circle and/or org name to Mixpanel events

--- api-lib/event_triggers/sendInteractionEventToMixpanel.ts
+++ api-lib/event_triggers/sendInteractionEventToMixpanel.ts
@@ -1,3 +1,4 @@
+import assert from 'assert';
 import * as crypto from 'crypto';
 
 import type { VercelRequest, VercelResponse } from '@vercel/node';
@@ -20,11 +21,46 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       req.body;
     const { new: event } = payload.event.data;
 
-    const eventData: { [key: string]: any } = event.data ?? {};
+    const eventData: Record<string, any> = {
+      ...(event.data ?? {}),
+      event_subtype: event.event_subtype,
+      id: event.id,
+      circle_id: event.circle_id,
+      org_id: event.org_id,
+    };
 
-    // need to see if the org is a sample org to change how we process funnels/events etc
-    if (event.org_id !== undefined && event.org_id !== null) {
-      eventData['sample'] = await isOrgSample(event.org_id);
+    if (event.circle_id) {
+      const { circles_by_pk: circle } = await adminClient.query(
+        {
+          circles_by_pk: [
+            { id: event.circle_id },
+            {
+              name: true,
+              organization_id: true,
+              organization: { sample: true, name: true },
+            },
+          ],
+        },
+        { operationName: 'mixpanel_getOrgId' }
+      );
+      assert(circle);
+      eventData.org_id = circle.organization_id;
+      eventData.circle_name = circle.name;
+      eventData.organization_name = circle.organization.name;
+      eventData.sample = circle.organization.sample;
+    } else if (event.org_id) {
+      const { organizations_by_pk: org } = await adminClient.query(
+        {
+          organizations_by_pk: [
+            { id: event.org_id },
+            { sample: true, name: true },
+          ],
+        },
+        { operationName: 'mixpanel_getOrgName' }
+      );
+      assert(org);
+      eventData.organization_name = org.name;
+      eventData.sample = org.sample;
     }
 
     if (event.profile_id) {
@@ -33,11 +69,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
         event.event_type,
         'profile_' + sha256(`${event.profile_id}`),
         {
-          event_subtype: event.event_subtype,
-          id: event.id,
-          circle_id: event.circle_id,
           profile_id: event.profile_id,
-          org_id: event.org_id,
           ...eventData,
         }
       );
@@ -49,21 +81,15 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
         `circle_${event.event_type}`,
         `circle_${event.circle_id}`,
         {
-          event_subtype: event.event_subtype,
-          id: event.id,
-          circle_id: event.circle_id,
           profile_id: event.profile_id
             ? sha256(`${event.profile_id}`)
             : undefined,
-          org_id: event.org_id,
           ...eventData,
         }
       );
     }
 
-    res.status(200).json({
-      message: `user event recorded`,
-    });
+    res.status(200).json({ message: `user event recorded` });
   } catch (e) {
     return errorResponse(res, e);
   }
@@ -75,37 +101,9 @@ const sha256 = (input: string) =>
 const track = (
   mp: Mixpanel,
   eventName: string,
-  distinctId: string,
+  distinct_id: string,
   props: Record<string, unknown>
-): Promise<void> => {
-  return new Promise<void>(resolve => {
-    mp.track(
-      eventName,
-      {
-        distinct_id: distinctId,
-        ...props,
-      },
-      () => {
-        resolve();
-      }
-    );
-  });
-};
-
-async function isOrgSample(orgId: number): Promise<boolean> {
-  const { organizations_by_pk } = await adminClient.query(
-    {
-      organizations_by_pk: [
-        {
-          id: orgId,
-        },
-        { sample: true },
-      ],
-    },
-    {
-      operationName: 'updateOrgLogo_getPreviousLogo',
-    }
+) =>
+  new Promise<void>(resolve =>
+    mp.track(eventName, { distinct_id, ...props }, () => resolve())
   );
-
-  return organizations_by_pk?.sample ?? false;
-}
