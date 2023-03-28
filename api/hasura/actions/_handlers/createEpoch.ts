e1b4fd1bbba376b09f12e0fd851d17d0d9a72882
sshmm <34943689+sshmm@users.noreply.github.com>
2023-03-28 03:56:48 +0200

    fix creating epoch during if DST is included (#2051)
    * allow epoch_end time diff to be off by one hour
    * account for DST in custom cycles epochs

--- api/hasura/actions/_handlers/createEpoch.ts
+++ api/hasura/actions/_handlers/createEpoch.ts
@@ -137,9 +137,14 @@ export function validateMonthlyInput(
 ): ErrorReturn {
   const { start_date, end_date } = input;
 
-  const endDateIsValid = findSameDayNextMonth(start_date, input).equals(
-    end_date
-  );
+  const endDateIsValid =
+    findSameDayNextMonth(start_date, input).equals(end_date) ||
+    findSameDayNextMonth(start_date, input).equals(
+      end_date.plus({ hours: 1 })
+    ) ||
+    findSameDayNextMonth(start_date, input).equals(
+      end_date.minus({ hours: 1 })
+    );
   if (!endDateIsValid)
     return new Error(
       dedent`
@@ -181,7 +186,10 @@ export function validateCustomInput({
   const frequencyDuration = Duration.fromObject({
     [frequency_unit]: frequency,
   });
-  if (interval.length(frequency_unit) > frequencyDuration.as(frequency_unit))
+  if (
+    interval.length(frequency_unit) >
+    frequencyDuration.plus({ hours: 1 }).as(frequency_unit)
+  )
     return new Error(
       dedent`
         epoch duration ${
