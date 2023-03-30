9741ad40c2e100ec8066f53e0703b2a30a713b22
crabsinger <83605543+crabsinger@users.noreply.github.com>
2023-03-30 13:14:18 -0700

    Fix table name typo in down migration

--- hasura/migrations/default/1680036748448_create_org_share_tokens_table/down.sql
+++ hasura/migrations/default/1680036748448_create_org_share_tokens_table/down.sql
@@ -1,2 +1,2 @@
-DROP TRIGGER "set_public_org_share_tokens_updated_at" ON "public"."org_share_token";
+DROP TRIGGER "set_public_org_share_tokens_updated_at" ON "public"."org_share_tokens";
 DROP TABLE "public"."org_share_tokens";
