05825120b59f8c469ce34ee9393b363d9045334b
Lawrence Wang <l@lw.io>
2023-04-10 11:58:11 -0700

    update org members repl script to create members for all orgs

--- scripts/repl/org_membership.ts
+++ scripts/repl/org_membership.ts
@@ -7,21 +7,29 @@ import fp from 'lodash/fp';
 import { adminClient as client } from '../../api-lib/gql/adminClient';
 
 export const init = async () => {
-  const createMembersFromCircles = async (orgId: number) => {
+  const createMembersForOrg = async (orgId: number) => {
     const { organizations_by_pk: org } = await client.query(
       {
         organizations_by_pk: [
           { id: orgId },
           {
             members: [{}, { profile: { id: true } }],
-            circles: [{}, { users: [{}, { profile: { id: true } }] }],
+            circles: [
+              { where: { deleted_at: { _is_null: true } } },
+              {
+                users: [
+                  { where: { deleted_at: { _is_null: true } } },
+                  { profile: { id: true } },
+                ],
+              },
+            ],
           },
         ],
       },
-      { operationName: 'repl' }
+      { operationName: 'repl_getUsers' }
     );
 
-    assert(org);
+    assert(org, `no org with id ${orgId}`);
 
     const existing = org.members.map(m => m.profile.id);
     const all = org.circles.flatMap(c => c.users.map(u => u.profile.id));
@@ -34,11 +42,25 @@ export const init = async () => {
           { affected_rows: true },
         ],
       },
-      { operationName: 'repl' }
+      { operationName: 'repl_insertOrgMembers' }
     );
   };
 
+  const createMembersForAllOrgs = async () => {
+    // TODO
+    const { organizations } = await client.query(
+      { organizations: [{}, { id: true, name: true }] },
+      { operationName: 'repl_getOrgs' }
+    );
+
+    for (const org of organizations) {
+      console.log(`${org.id}: ${org.name}`);
+      await createMembersForOrg(org.id);
+    }
+  };
+
   return {
-    createMembersFromCircles,
+    createMembersForOrg,
+    createMembersForAllOrgs,
   };
 };
