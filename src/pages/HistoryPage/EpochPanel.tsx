36b9a8edabf94b5b8d26d8b45222d7abfb48c188
tiffany6.eth <tiffanyy.dev@gmail.com>
2023-04-15 16:34:26 -0400

    addede view map button on epoch panel for admin view

--- src/pages/HistoryPage/EpochPanel.tsx
+++ src/pages/HistoryPage/EpochPanel.tsx
@@ -3,10 +3,12 @@ import round from 'lodash/round';
 import { DateTime } from 'luxon';
 import { CSS } from 'stitches.config';
 
+import isFeatureEnabled from 'config/features';
+import { useApiAdminCircle } from 'hooks';
 import { paths } from 'routes/paths';
-import { Box, Panel, Text, AppLink, Flex, Button } from 'ui';
+import { AppLink, Box, Button, Flex, Link, Panel, Text } from 'ui';
 
-import type { QueryPastEpoch, QueryDistribution } from './getHistoryData';
+import type { QueryDistribution, QueryPastEpoch } from './getHistoryData';
 import { NotesSection } from './Notes';
 
 type EpochPanelProps = {
@@ -27,6 +29,8 @@ export const EpochPanel = ({
   const startDate = DateTime.fromISO(epoch.start_date);
   const endDate = DateTime.fromISO(epoch.end_date);
   const endDateFormat = endDate.month === startDate.month ? 'd' : 'MMM d';
+
+  const { downloadCSV } = useApiAdminCircle(circleId);
 
   const received = epoch.receivedGifts;
   const sent = epoch.sentGifts;
@@ -105,13 +109,47 @@ export const EpochPanel = ({
             />
             {isAdmin && (
               <Box>
-                <Button
-                  color="cta"
-                  as={AppLink}
-                  to={paths.distributions(circleId, epoch.id)}
-                >
-                  Review & Export
-                </Button>
+                {isFeatureEnabled('vaults') ? (
+                  <Flex css={{ gap: '$sm' }}>
+                    <Button
+                      color="cta"
+                      as={AppLink}
+                      to={paths.distributions(circleId, epoch.id)}
+                    >
+                      Review & Export
+                    </Button>
+                    <Button
+                      color="secondary"
+                      as={AppLink}
+                      to={paths.map(circleId)}
+                    >
+                      View Map
+                    </Button>
+                  </Flex>
+                ) : (
+                  <Link
+                    href="#"
+                    css={{ fontWeight: '$semibold' }}
+                    onClick={e => {
+                      e.stopPropagation(),
+                        (async () => {
+                          // use the authed api to download the CSV
+                          const csv = await downloadCSV(epoch.number, epoch.id);
+
+                          if (csv?.file) {
+                            const a = document.createElement('a');
+                            a.href = csv.file;
+                            a.click();
+                            a.href = '';
+                          }
+
+                          return false;
+                        })();
+                    }}
+                  >
+                    Export CSV
+                  </Link>
+                )}
               </Box>
             )}
           </Flex>
