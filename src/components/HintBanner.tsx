4d3b2fef26d5ed30bdbb6f38d37e6fa04301a2b2
Wing <wingfeatherwave@gmail.com>
2023-05-03 16:24:20 -0700

    adding dismissible banners for adding members to org feature

--- src/components/HintBanner.tsx
+++ src/components/HintBanner.tsx
@@ -1,36 +1,64 @@
-import React from 'react';
+import { useState } from 'react';
 
 import { styled } from '../stitches.config';
-import { Flex, Panel, Text } from '../ui';
+import { Flex, IconButton, Panel, Text } from '../ui';
+import { X } from 'icons/__generated';
 
 const HintBanner = ({
   title,
+  dismissible,
   type = 'info',
   children,
 }: {
   title?: string;
+  dismissible?: string;
   type?: 'info' | 'alert';
   children: React.ReactNode;
 }) => {
+  const dismissibleAs = `banner-${dismissible}` || '';
+  const isDismissible = dismissible && dismissible.length > 0;
+  const localStorageBannerVisibility =
+    window.localStorage.getItem(dismissibleAs) === 'false' ? false : true;
+  const [bannerVisibility, setBannerVisibility] = useState(
+    localStorageBannerVisibility
+  );
+  const toggleBanner = () => {
+    setBannerVisibility(!bannerVisibility);
+    if (isDismissible) {
+      window.localStorage.setItem(dismissibleAs, `${!bannerVisibility}`);
+    }
+  };
   return (
-    <Panel {...{ [type]: true }} css={{ mb: '$lg' }}>
-      <Flex
-        column
-        css={{
-          gap: '$md',
-          width: '100%',
-          maxWidth: '40em',
-          alignItems: 'flex-start',
-        }}
-      >
-        {title && (
-          <Text h2 as="h2">
-            {title}
-          </Text>
-        )}
-        {children}
-      </Flex>
-    </Panel>
+    <>
+      {localStorageBannerVisibility && (
+        <Panel {...{ [type]: true }} css={{ mb: '$lg', position: 'relative' }}>
+          {isDismissible && (
+            <IconButton
+              onClick={toggleBanner}
+              css={{ position: 'absolute', right: '$md', top: '$md' }}
+            >
+              <X size="lg" />
+            </IconButton>
+          )}
+          <Flex
+            column
+            css={{
+              gap: '$md',
+              width: '100%',
+              maxWidth: '50em',
+              alignItems: 'flex-start',
+            }}
+          >
+            {title && (
+              <Text h2 as="h2">
+                {title}
+              </Text>
+            )}
+            {children}
+          </Flex>
+        </Panel>
+      )}
+    </>
   );
 };
 
---
+++ src/pages/CirclesPage/OrgBannerNoCta.tsx
@@ -0,0 +1,33 @@
+import HintBanner from 'components/HintBanner';
+import { EXTERNAL_URL_DOCS } from 'routes/paths';
+import { Box, Link, Text } from 'ui';
+
+export const OrgBannerOverview = () => {
+  return (
+    <HintBanner title={'New: Org Membership!'} dismissible="org-banner">
+      <Text p as="p" css={{ color: 'inherit' }}>
+        Invite your community into Coordinape Organizations! Increase engagement
+        and transparency by granting Org membership to your whole DAO/Community,
+        without them needing to be in circles. Org members can:
+      </Text>
+      <Box
+        as="ul"
+        css={{ my: 0, li: { display: 'list-item', lineHeight: '$base' } }}
+      >
+        <Text as="li">
+          See Activity in the Org and signal support with emoji reactions
+        </Text>
+        <Text as="li">
+          See Activity, membership, and maps of circles within the org
+        </Text>
+      </Box>
+      <Text inline>
+        <Link inlineLink href={EXTERNAL_URL_DOCS} target="_blank">
+          Check out the docs
+        </Link>{' '}
+        for more details, and keep an eye out for more new ways for Coordinape
+        Org members to engage!
+      </Text>
+    </HintBanner>
+  );
+};
