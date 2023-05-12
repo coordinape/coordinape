707cc3402f486d299928738111833c4954aeff30
crabsinger <83605543+crabsinger@users.noreply.github.com>
2023-05-12 16:06:39 -0700

    Add cosoul contracts

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -165,6 +165,7 @@ const sharedNetworkSettings = {
   gasMultiplier: 1,
   accounts: {
     mnemonic: 'test test test test test test test test test test test junk',
+    initialBalance: '10900000000000000000',
   },
   deploy: ['./scripts/deploy'],
 };
@@ -183,13 +184,19 @@ const config: HardhatUserConfig = {
       },
     ],
   },
+  typechain: {
+    outDir: 'typechain',
+  },
   namedAccounts: {
     deployer: {
       default: 0,
     },
+    proxyAdmin: {
+      default: 10,
+    },
   },
   paths: {
-    sources: './contracts/coordinape-protocol/contracts/ApeProtocol',
+    sources: './contracts/coordinape-protocol/contracts',
   },
   mocha: {
     timeout: 60000,
