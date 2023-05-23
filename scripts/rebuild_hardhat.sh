e6e94fdeb42e67533d11cf7c290c225b9f8f8abc
Lawrence Wang <l@lw.io>
2023-05-23 10:51:41 -0700

    update rebuild_hardhat.sh to use ganache

--- scripts/rebuild_hardhat.sh
+++ scripts/rebuild_hardhat.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 set -e
 SCRIPT_DIR="$(dirname $BASH_SOURCE[0])"
-FIND_DEV_PID="lsof -t -i:8545 -sTCP:LISTEN"
+PORT=${HARDHAT_GANACHE_PORT:-8546}
+FIND_GANACHE_PID="lsof -t -i:$PORT -sTCP:LISTEN"
 
 while [[ "$#" > 0 ]]; do case $1 in
   --full) FULL=1;;
 esac; shift; done
 
 if [ "$FULL" ]; then
-#  git submodule update --init --recursive
+  # git submodule update --init --recursive
   yarn --cwd hardhat install --frozen-lockfile
   yarn --cwd hardhat compile
 
-  echo "Starting node for auto deployment..."
-  yarn hardhat:dev >/dev/null &
+  echo "Starting Ganache..."
+  yarn hardhat:ganache --no-reuse 2>&1 & PID=$!
 
-  while [ ! $(eval $FIND_DEV_PID) ]; do
+  until curl -s -o/dev/null http://localhost:$PORT; do
     sleep 1
+    if [ -z "$(ps -p $PID -o pid=)" ]; then
+      echo "Ganache failed to start up."
+      exit 1
+    fi
   done
-  yarn --cwd hardhat deploy
+
+  echo ============================================
+  echo Press Enter after you see \"Testchain is up\".
+  echo ============================================
+  read -p '' ok
 fi
 
 yarn --cwd hardhat codegen
 yarn --cwd hardhat build
 
 if [ "$FULL" ]; then
-  echo "Stopping node. The 'Command failed' messages that follow are normal."
-  kill $(eval $FIND_DEV_PID)
+  echo "Stopping Ganache. The 'Command failed' message that follows is normal."
+  kill $(eval $FIND_GANACHE_PID)
 fi
 
 $SCRIPT_DIR/link_hardhat.sh
