756c7ad543c17eb5b0b31d1f05a9d79cb42607a8
crabsinger <83605543+crabsinger@users.noreply.github.com>
2023-05-24 12:55:23 -0700

    Hook up view page to public api endpoint

---
+++ api/cosoul/[address].ts
@@ -0,0 +1,236 @@
+import assert from 'assert';
+
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+import { member_epoch_pgives_select_column } from '../../api-lib/gql/__generated__/zeus';
+import { adminClient } from '../../api-lib/gql/adminClient';
+import { errorResponse } from '../../api-lib/HttpError';
+import { Awaited } from '../../api-lib/ts4.5shim';
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  try {
+    let address: string | undefined;
+    if (typeof req.query.address == 'string') {
+      address = req.query.address;
+    } else if (Array.isArray(req.query.address)) {
+      address = req.query.address.pop();
+    }
+
+    assert(address, 'no address provided');
+    const data = await getCosoulData(address);
+    return res.status(200).send(data);
+  } catch (error: any) {
+    return errorResponse(res, error);
+  }
+}
+
+async function getCosoulData(address: string) {
+  // fetch profileId from address
+  const { profiles } = await adminClient.query(
+    {
+      profiles: [
+        {
+          where: {
+            address: { _ilike: address },
+          },
+          limit: 1,
+        },
+        {
+          id: true,
+        },
+      ],
+    },
+    {
+      operationName: 'cosoulApi__fetchProfileId',
+    }
+  );
+
+  const profileId = profiles[0].id;
+  assert(profileId, 'error fetching profileId');
+  assert(address, 'error fetching address');
+
+  const cosoulData = await clientGetCoSoulData(profileId, address);
+
+  return cosoulData;
+
+  // const circleData = circle_share_tokens?.pop();
+  // const orgData = org_share_tokens?.pop();
+  // assert(circleData?.circle || orgData?.organization, 'invalid token');
+  // return { ...circleData, ...orgData, token };
+}
+
+export const clientGetCoSoulData = async (
+  profileId: number,
+  address: string
+) => {
+  const {
+    totalPgive,
+    epochCount,
+    organizationCount,
+    circleCount,
+    organizations,
+    circles,
+    noteCount,
+    contributionCount,
+  } = await adminClient.query(
+    {
+      __alias: {
+        totalPgive: {
+          member_epoch_pgives_aggregate: [
+            {
+              where: {
+                user: { profile: { address: { _eq: address } } },
+              },
+            },
+            // what is the diff between pgive and normalized_pgive.
+            // I thought pgive was normalized give, plus stuff
+            { aggregate: { sum: [{}, { normalized_pgive: true }] } },
+          ],
+        },
+        epochCount: {
+          member_epoch_pgives_aggregate: [
+            {
+              where: {
+                user: { profile: { address: { _eq: address } } },
+              },
+            },
+            { aggregate: { count: [{}, true] } },
+          ],
+        },
+        organizationCount: {
+          member_epoch_pgives_aggregate: [
+            {
+              where: {
+                user: { profile: { address: { _eq: address } } },
+              },
+              distinct_on: [member_epoch_pgives_select_column.organization_id],
+            },
+            { aggregate: { count: [{}, true] } },
+          ],
+        },
+        circleCount: {
+          member_epoch_pgives_aggregate: [
+            {
+              where: {
+                user: { profile: { address: { _eq: address } } },
+              },
+              distinct_on: [member_epoch_pgives_select_column.circle_id],
+            },
+            { aggregate: { count: [{}, true] } },
+          ],
+        },
+        organizations: {
+          member_epoch_pgives: [
+            {
+              where: {
+                user: { profile: { address: { _eq: address } } },
+              },
+              distinct_on: [member_epoch_pgives_select_column.organization_id],
+            },
+            {
+              organization: {
+                id: true,
+                name: true,
+                logo: true,
+              },
+            },
+          ],
+        },
+        circles: {
+          member_circle_pgives: [
+            {
+              where: {
+                user: { profile: { address: { _eq: address } } },
+              },
+            },
+            {
+              circle: {
+                id: true,
+                name: true,
+                organization_id: true,
+              },
+              pgive: true,
+              epochs: true,
+            },
+          ],
+        },
+        noteCount: {
+          note_count: [
+            {
+              where: {
+                profile_id: { _eq: profileId },
+              },
+            },
+            { notes: true },
+          ],
+        },
+        contributionCount: {
+          contribution_count: [
+            {
+              where: {
+                profile_id: { _eq: profileId },
+              },
+            },
+            { contributions: true },
+          ],
+        },
+      },
+    },
+    { operationName: 'getCoSoulData' }
+  );
+
+  console.log('totalPgive', totalPgive);
+
+  const orgs = organizations.map(o => o.organization);
+  const orgRollup: Record<
+    number,
+    typeof orgs[number] & {
+      circles: {
+        name: string;
+        pgive: number;
+        id: number;
+        epochs: number;
+      }[];
+    }
+  > = {};
+  for (const org of orgs) {
+    if (!org) {
+      continue;
+    }
+    orgRollup[org.id] = { ...org, circles: [] };
+  }
+  for (const circle of circles) {
+    if (!circle.circle) {
+      continue;
+    }
+    const o = orgRollup[circle.circle.organization_id];
+    if (!o) {
+      continue;
+    }
+    o.circles.push({
+      ...circle.circle,
+      pgive: circle.pgive,
+      epochs: circle.epochs,
+    });
+  }
+  const orgArray = Object.values(orgRollup).sort((a, b) =>
+    a.name > b.name ? -1 : 1
+  );
+
+  return {
+    // FIXME as any, wut?
+    totalPgive: (totalPgive.aggregate?.sum as any).normalized_pgive,
+    epochCount: epochCount.aggregate?.count,
+    organizationCount: organizationCount.aggregate?.count,
+    circleCount: circleCount.aggregate?.count,
+    organizations: orgArray,
+    noteCount: noteCount[0]?.notes ?? 0,
+    contributionCount: contributionCount[0]?.contributions ?? 0,
+  };
+};
+
+export const QUERY_KEY_COSOUL_PAGE = 'cosoulPageQuery';
+
+export type QueryCoSoulResult = Awaited<ReturnType<typeof getCosoulData>>;
+
+export type CosoulData = Awaited<ReturnType<typeof getCosoulData>>;
--- src/features/cosoul/ViewPage.tsx
+++ src/features/cosoul/ViewPage.tsx
@@ -1,15 +1,79 @@
-import isFeatureEnabled from 'config/features';
-import { Text } from 'ui';
+/* eslint-disable no-console */
+import { useEffect, useState } from 'react';
 
+import { useParams } from 'react-router-dom';
+
+import { CosoulData } from '../../../api/cosoul/[address]';
+import { LoadingModal } from 'components';
+import isFeatureEnabled from 'config/features';
+import { SingleColumnLayout } from 'ui/layouts';
+
+import { CoSoulArt } from './art/CoSoulArt';
+import { CoSoulArtContainer } from './CoSoulArtContainer';
+import { CoSoulComposition } from './CoSoulComposition';
+import { CoSoulDetails } from './CoSoulDetails';
 export const ViewPage = () => {
+  const { address } = useParams();
+
+  const [cosoulDataError, setCosoulDataError] = useState<string | undefined>();
+  const [cosoulData, setCosoulData] = useState<CosoulData | undefined>();
+
+  useEffect(() => {
+    try {
+      fetch('/api/cosoul/' + address).then(res => {
+        if (!res.ok) {
+          setCosoulDataError(
+            "Error fetching cosoul data, cosoul for this address doesn't exist"
+          );
+          return;
+        }
+        res.json().then((data: CosoulData) => {
+          setCosoulData(data);
+        });
+      });
+    } catch (e) {
+      if (e instanceof Error) {
+        setCosoulDataError(e.message ?? 'unknown error');
+      } else {
+        setCosoulDataError('Invalid address or network error');
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (cosoulData) {
+      console.log({ cosoulData });
+    }
+  }, [cosoulData]);
+
   if (!isFeatureEnabled('cosoul')) {
     return <></>;
   }
+
+  // Waiting to validate the token
+  if (!cosoulDataError && !cosoulData) {
+    return <LoadingModal visible={true} note="cosoul-lookup" />;
+  }
+
   return (
     <>
-      <Text color="cta" h1>
-        test
-      </Text>
+      {cosoulData && (
+        <SingleColumnLayout
+          css={{
+            m: 'auto',
+            alignItems: 'center',
+            gap: '$1xl',
+            maxWidth: '1200px',
+          }}
+        >
+          <CoSoulComposition cosoul_data={cosoulData}>
+            <CoSoulArtContainer>
+              <CoSoulArt pGive={cosoulData.totalPgive} address={address} />
+            </CoSoulArtContainer>
+          </CoSoulComposition>
+          <CoSoulDetails cosoul_data={cosoulData} />
+        </SingleColumnLayout>
+      )}
     </>
   );
 };
