b8be57ccbedb3ca74f2b197d40d27a27f516e753
crabsinger <83605543+crabsinger@users.noreply.github.com>
2023-05-25 13:24:32 -0700

    Cleanup cosoul fetch to use useQuery

--- src/features/cosoul/ViewPage.tsx
+++ src/features/cosoul/ViewPage.tsx
@@ -1,63 +1,70 @@
-/* eslint-disable no-console */
-import { useEffect, useState } from 'react';
-
+import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 
 import { CosoulData } from '../../../api/cosoul/[address]';
 import { LoadingModal } from 'components';
 import isFeatureEnabled from 'config/features';
+import { Text } from 'ui';
 import { SingleColumnLayout } from 'ui/layouts';
 
 import { CoSoulArt } from './art/CoSoulArt';
 import { CoSoulArtContainer } from './CoSoulArtContainer';
 import { CoSoulComposition } from './CoSoulComposition';
 import { CoSoulDetails } from './CoSoulDetails';
+
+export const QUERY_KEY_COSOUL_VIEW = 'cosoul_view';
 export const ViewPage = () => {
   const { address } = useParams();
 
-  const [cosoulDataError, setCosoulDataError] = useState<string | undefined>();
-  const [cosoulData, setCosoulData] = useState<CosoulData | undefined>();
-
-  useEffect(() => {
-    try {
-      fetch('/api/cosoul/' + address).then(res => {
-        if (!res.ok) {
-          setCosoulDataError(
-            "Error fetching cosoul data, cosoul for this address doesn't exist"
-          );
-          return;
-        }
-        res.json().then((data: CosoulData) => {
-          setCosoulData(data);
-        });
-      });
-    } catch (e) {
-      if (e instanceof Error) {
-        setCosoulDataError(e.message ?? 'unknown error');
-      } else {
-        setCosoulDataError('Invalid address or network error');
+  const { data, isError, isLoading, error } = useQuery(
+    [QUERY_KEY_COSOUL_VIEW, address],
+    async (): Promise<CosoulData> => {
+      const res = await fetch('/api/cosoul/' + address);
+      if (!res.ok) {
+        throw new Error('Failed to fetch cosoul data');
       }
+      return res.json();
+    },
+    {
+      enabled: !!address,
+      retry: false,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+      staleTime: Infinity,
     }
-  }, []);
-
-  useEffect(() => {
-    if (cosoulData) {
-      console.log({ cosoulData });
-    }
-  }, [cosoulData]);
+  );
 
   if (!isFeatureEnabled('cosoul')) {
     return <></>;
   }
 
   // Waiting to validate the token
-  if (!cosoulDataError && !cosoulData) {
+  if (isLoading) {
     return <LoadingModal visible={true} note="cosoul-lookup" />;
+  }
+
+  // Error
+  if (isError) {
+    return (
+      <SingleColumnLayout
+        css={{
+          m: 'auto',
+          alignItems: 'center',
+          gap: '$1xl',
+          maxWidth: '1200px',
+          minHeight: '100vh',
+        }}
+      >
+        <Text h1 color="alert">
+          {(error as string).toString()}
+        </Text>
+      </SingleColumnLayout>
+    );
   }
 
   return (
     <>
-      {cosoulData && (
+      {data && (
         <SingleColumnLayout
           css={{
             m: 'auto',
@@ -66,12 +73,12 @@ export const ViewPage = () => {
             maxWidth: '1200px',
           }}
         >
-          <CoSoulComposition cosoul_data={cosoulData}>
+          <CoSoulComposition cosoul_data={data}>
             <CoSoulArtContainer>
-              <CoSoulArt pGive={cosoulData.totalPgive} address={address} />
+              <CoSoulArt pGive={data.totalPgive} address={address} />
             </CoSoulArtContainer>
           </CoSoulComposition>
-          <CoSoulDetails cosoul_data={cosoulData} />
+          <CoSoulDetails cosoul_data={data} />
         </SingleColumnLayout>
       )}
     </>
