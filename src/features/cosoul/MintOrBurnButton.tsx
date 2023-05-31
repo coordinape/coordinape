66e132328655367a2317f3e3f85158e6d29ad354
crabsinger <83605543+crabsinger@users.noreply.github.com>
2023-05-31 16:45:49 -0700

    Fetch events in FE

--- src/features/cosoul/MintOrBurnButton.tsx
+++ src/features/cosoul/MintOrBurnButton.tsx
@@ -1,4 +1,15 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import assert from 'assert';
+import { useEffect, useState } from 'react';
+
+import { TypedEvent } from '@coordinape/hardhat/dist/typechain/commons';
+import { BigNumber } from 'ethers';
+import { provider } from 'lib/zod/formHelpers';
+import { useQuery } from 'react-query';
+
 import { useToast } from '../../hooks';
+import { useWeb3React } from '../../hooks/useWeb3React';
 import { Button, Text } from '../../ui';
 import { sendAndTrackTx } from '../../utils/contractHelpers';
 
@@ -24,17 +35,48 @@ export const MintOrBurnButton = ({
       <BurnButton contracts={contracts} tokenId={tokenId} onSuccess={refresh} />
     );
   }
-  return <MintButton contracts={contracts} onSuccess={refresh} />;
+  return (
+    <MintButton contracts={contracts} onSuccess={refresh} account={account} />
+  );
 };
 
 const MintButton = ({
   contracts,
   onSuccess,
+  account,
 }: {
   contracts: Contracts;
   onSuccess(): void;
+  account: string;
 }) => {
   const { showDefault, showError } = useToast();
+
+  const [mintedBlockNumber, setMintedBlockNumber] = useState<
+    number | undefined
+  >(undefined);
+
+  const { data, isLoading, isError, error } = useQuery(
+    ['mintEvents', account],
+    async (): Promise<any> => {
+      assert(mintedBlockNumber);
+      console.log({ mintedBlockNumber });
+      return getMintEvents(contracts, {
+        deployment_block: mintedBlockNumber,
+        profile_address: account,
+      });
+    },
+    {
+      enabled: !!mintedBlockNumber,
+      retry: false,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+      staleTime: Infinity,
+    }
+  );
+
+  useEffect(() => {
+    console.log({ data, isLoading, isError, error });
+  }, [data, isLoading, isError, error]);
 
   const mint = async () => {
     try {
@@ -54,6 +96,7 @@ const MintButton = ({
         }
       );
       if (receipt) {
+        setMintedBlockNumber(receipt.blockNumber);
         onSuccess();
       }
     } catch (e: any) {
@@ -108,3 +151,31 @@ const BurnButton = ({
     </Button>
   );
 };
+
+interface RawTransaction {
+  block: number;
+  date: string;
+  type: string;
+  details: string;
+  hash: string;
+}
+
+async function getMintEvents(
+  contracts: Contracts,
+  {
+    deployment_block,
+    profile_address,
+  }: {
+    deployment_block: number;
+    profile_address: string;
+  }
+): Promise<any> {
+  const mintEvents = await contracts.cosoul.queryFilter(
+    contracts.cosoul.filters.Transfer(null, profile_address),
+    deployment_block
+  );
+
+  console.log('mintEvents', mintEvents);
+
+  return [];
+}
