7c632c9a46c4fd6584dd846e5209b8360f5f8e8a
crabsinger <83605543+crabsinger@users.noreply.github.com>
2023-06-01 11:14:40 -0700

    update storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,37 +1,28 @@
 module.exports = {
-  stories: [
-    {
-      directory: '../src/ui',
-      titlePrefix: 'Design System/Components/',
-      files: '**/*.stories.*',
-    },
-    {
-      directory: '../src/pages',
-      titlePrefix: 'Design System/Pages/',
-      files: '**/*.stories.*',
-    },
-    {
-      directory: '../src/components',
-      titlePrefix: 'App UI',
-      files: '**/*.stories.*',
-    },
-    {
-      directory: '../src/features/cosoul/art',
-      titlePrefix: 'CoSoul',
-      files: '**/*.stories.*',
-    },
-  ],
+  stories: [{
+    directory: '../src/ui',
+    titlePrefix: 'Design System/Components/',
+    files: '**/*.stories.*'
+  }, {
+    directory: '../src/pages',
+    titlePrefix: 'Design System/Pages/',
+    files: '**/*.stories.*'
+  }, {
+    directory: '../src/components',
+    titlePrefix: 'App UI',
+    files: '**/*.stories.*'
+  }, {
+    directory: '../src/features/cosoul/art',
+    titlePrefix: 'CoSoul',
+    files: '**/*.stories.*'
+  }],
   core: {
-    builder: 'webpack5',
+    builder: 'webpack5'
   },
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    'storybook-addon-designs',
-    '@storybook/preset-create-react-app',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-addon-designs', '@storybook/preset-create-react-app'],
   staticDirs: ['../public'],
   features: {
-    storyStoreV7: true,
+    storyStoreV7: true
   },
+  framework: '@storybook/react'
 };
