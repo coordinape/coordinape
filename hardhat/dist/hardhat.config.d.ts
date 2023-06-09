74c2dd4eba766e064c95355df0f5106c422354e2
crabsinger <83605543+crabsinger@users.noreply.github.com>
2023-06-09 15:36:03 -0700

    wip

--- hardhat/dist/hardhat.config.d.ts
+++
@@ -1,8 +0,0 @@
-import { HardhatUserConfig } from 'hardhat/config';
-import '@typechain/hardhat';
-import 'hardhat-deploy';
-import '@nomiclabs/hardhat-ethers';
-import '@nomiclabs/hardhat-waffle';
-import '@nomicfoundation/hardhat-verify';
-declare const config: HardhatUserConfig;
-export default config;
--- hardhat/scripts/cp-declarations.sh
+++ hardhat/scripts/cp-declarations.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+mkdir -p dist/typechain 
+
 # Copy all TypeScript declaration files (files ending with .d.ts) from typechain/ to dist/typechain/ directory.
 find typechain -name "*.d.ts" | xargs -I _ cp _ dist/typechain/
+
+# Copy deploymentInfo
+cp deploymentInfo.json dist/
