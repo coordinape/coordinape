38bdcb7d71b7bee666eb3e7f3af440a9dbd3db64
crabsinger <83605543+crabsinger@users.noreply.github.com>
2023-06-13 17:05:37 -0700

    wip

---
+++ hardhat/scripts/manage/cosoul_verify_data.ts
@@ -0,0 +1,22 @@
+import { default as hre, deployments, ethers } from 'hardhat';
+
+import { CoSoul__factory } from '../../typechain';
+
+async function main() {
+  const { deployer } = await hre.getNamedAccounts();
+  const deployed_cosoul = await hre.deployments.get('CoSoul');
+
+  const deployerSigner = await ethers.getSigner(deployer);
+
+  const cosoul = CoSoul__factory.connect(
+    deployed_cosoul.address,
+    deployerSigner
+  );
+
+  console.log('baseUri: ', await cosoul.getBaseURI());
+}
+
+main().catch(error => {
+  console.error(error);
+  process.exitCode = 1;
+});
