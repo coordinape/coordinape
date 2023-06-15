c23734c183524872b17e3c22dc50662f606b7124
Wing <wingfeatherwave@gmail.com>
2023-06-15 11:32:46 -0700

    adding cosoul linking for bugfixing help

--- src/features/cosoul/art/CoSoulArt2.stories.tsx
+++ src/features/cosoul/art/CoSoulArt2.stories.tsx
@@ -9,15 +9,13 @@ export default {
 
 const Template: ComponentStory<typeof CoSoulArtComponent> = args => (
   <>
-    <LinkTo kind="cosoul-cosoulart2--co-soul-art-2">
-      Link to another cosoul
-    </LinkTo>
+    <LinkTo kind="cosoul-cosoulart--co-soul-art">Link to another cosoul</LinkTo>
     <CoSoulArtComponent {...args} />
   </>
 );
 
-export const CoSoulArt = Template.bind({});
-CoSoulArt.args = {
+export const CoSoulArt2 = Template.bind({});
+CoSoulArt2.args = {
   pGive: 9999,
   address: '0x6959A0e3C5486222cB8Ba3ab94e4f9444Df4F3bC',
   showGui: true,
