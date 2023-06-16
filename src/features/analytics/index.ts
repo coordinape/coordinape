a4aa40ddec4dd650e906e309d45124d2938e5e9f
CryptoGraffe <graffe@graffe.co>
2023-06-16 11:48:47 -0700

    fixing other uncaught exceptions when we are sandboxed

--- src/features/analytics/index.ts
+++ src/features/analytics/index.ts
@@ -5,29 +5,47 @@ import shajs from 'sha.js';
 
 import { isFeatureEnabled } from '../../config/features';
 
-const enabled = !!process.env.REACT_APP_MIXPANEL_TOKEN && !!mp;
+const enabled = true; //!!process.env.REACT_APP_MIXPANEL_TOKEN && !!mp;
 
 export const initFrontend = () => {
-  if (!enabled) return;
+  try {
+    if (!enabled) return;
 
-  mp.init(process.env.REACT_APP_MIXPANEL_TOKEN as string, {
-    api_host: process.env.REACT_APP_MIXPANEL_HOST,
-    debug: true,
-    ignore_dnt: isFeatureEnabled('ignore_dnt'),
-  });
+    mp.init(process.env.REACT_APP_MIXPANEL_TOKEN as string, {
+      api_host: process.env.REACT_APP_MIXPANEL_HOST,
+      debug: true,
+      ignore_dnt: isFeatureEnabled('ignore_dnt'),
+    });
 
-  if (typeof window !== 'undefined') {
-    (window as any).testMp = () => {
-      mp.track('test', { foo: 'bar' });
-    };
+    if (typeof window !== 'undefined') {
+      (window as any).testMp = () => {
+        mp.track('test', { foo: 'bar' });
+      };
+    }
+  } catch (e: any) {
+    console.error('Error initializing Mixpanel', e);
   }
 };
 
-export const track = (name: string, props: any) =>
-  enabled && mp.track(name, props);
+export const track = (name: string, props: any) => {
+  if (enabled) {
+    try {
+      mp.track(name, props);
+    } catch {
+      // ignore
+    }
+  }
+};
 
-export const identify = (profileId: number) =>
-  enabled && mp.identify(`profile_${obfuscateId(String(profileId))}`);
+export const identify = (profileId: number) => {
+  if (enabled) {
+    try {
+      mp.identify(`profile_${obfuscateId(String(profileId))}`);
+    } catch {
+      // ignore
+    }
+  }
+};
 
 export const obfuscateId = (input: string) =>
   shajs('sha256').update(input).digest('base64');
