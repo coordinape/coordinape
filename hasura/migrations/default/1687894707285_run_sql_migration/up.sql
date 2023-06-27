39395c0dcdec4a513120aa78753446f366a64db1
sshmm <s.shawky92@gmail.com>
2023-06-27 23:26:17 +0300

    filter notes count and contributions count

---
+++ hasura/migrations/default/1687894707285_run_sql_migration/up.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE VIEW "public"."contribution_count" AS 
+ SELECT p.id AS profile_id,
+    count(*) AS contributions
+   FROM ((contributions c
+     JOIN users u ON ((u.id = c.user_id)))
+     JOIN profiles p ON (((p.address)::text = (u.address)::text))
+     JOIN circles ci ON ((ci.id = c.circle_id))
+     JOIN organizations o ON ((o.id = ci.organization_id)))
+  WHERE (o.sample = false)
+  GROUP BY p.id;
