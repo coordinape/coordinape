ff21043346c36cf0a14ffb86da15fe8ade834de5
sshmm <s.shawky92@gmail.com>
2023-07-10 19:05:35 +0300

    update down migration

--- hasura/migrations/default/1687893933614_run_sql_migration/down.sql
+++ hasura/migrations/default/1687893933614_run_sql_migration/down.sql
@@ -1,8 +1,10 @@
-CREATE OR REPLACE VIEW "public"."note_count" AS 
+DROP VIEW "public"."note_count" AS 
  SELECT p.id AS profile_id,
     count(*) AS notes
    FROM ((token_gifts t
      JOIN users u ON ((u.id = t.recipient_id)))
+     JOIN circles c ON c.id = t.circle_id
+     JOIN organizations o ON o.id = c.organization_id
      JOIN profiles p ON (((p.address)::text = (u.address)::text)))
-  WHERE (t.note <> ''::text)
+  WHERE (t.note <> ''::text AND o.sample = false)
   GROUP BY p.id;
