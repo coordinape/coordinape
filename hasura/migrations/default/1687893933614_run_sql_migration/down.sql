1c8890258f2bf6dc1f7804fe4ecb43b0bd021e0f
sshmm <s.shawky92@gmail.com>
2023-07-12 02:48:22 +0300

    update down migration

--- hasura/migrations/default/1687893933614_run_sql_migration/down.sql
+++ hasura/migrations/default/1687893933614_run_sql_migration/down.sql
@@ -1,10 +1,1 @@
-DROP VIEW "public"."note_count" AS 
- SELECT p.id AS profile_id,
-    count(*) AS notes
-   FROM ((token_gifts t
-     JOIN users u ON ((u.id = t.recipient_id)))
-     JOIN circles c ON c.id = t.circle_id
-     JOIN organizations o ON o.id = c.organization_id
-     JOIN profiles p ON (((p.address)::text = (u.address)::text)))
-  WHERE (t.note <> ''::text AND o.sample = false)
-  GROUP BY p.id;
+DROP VIEW "public"."note_count";
