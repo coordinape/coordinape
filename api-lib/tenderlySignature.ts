217f3fa5435800128f50ba2273f174c3b9885f19
crabsinger <83605543+crabsinger@users.noreply.github.com>
2023-08-03 16:25:11 -0700

    remove consoles

--- api-lib/tenderlySignature.ts
+++ api-lib/tenderlySignature.ts
@@ -1,13 +1,12 @@
-import { TENDERLY_WEBHOOK_SECRET } from './config';
 import { createHmac, timingSafeEqual } from 'crypto';
+
+import { TENDERLY_WEBHOOK_SECRET } from './config';
 
 export function isValidSignature(
   signature: string,
   body: string,
   timestamp: string
 ) {
-  // eslint-disable-next-line no-console
-  console.log('realz $$$ invoked', { signature, body, timestamp });
   const signingKey = TENDERLY_WEBHOOK_SECRET;
 
   const hmac = createHmac('sha256', signingKey); // Create a HMAC SHA256 hash using the signing key
