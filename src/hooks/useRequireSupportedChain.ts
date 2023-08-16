c39208d9cb66bcf0cf5e5b3bed3a2cda0f4a90f5
crabsinger <83605543+crabsinger@users.noreply.github.com>
2023-08-16 15:55:47 -0700

    Fix switchnetwork reload bug (#2295)

--- src/hooks/useRequireSupportedChain.ts
+++ src/hooks/useRequireSupportedChain.ts
@@ -1,14 +1,16 @@
 import { useEffect } from 'react';
 
+import { findConnectorName } from 'features/auth/connectors';
 import { supportedChainIds } from 'lib/vaults';
 
+import { EConnectorNames } from 'config/constants';
 import { useToast } from 'hooks';
 import { switchNetwork } from 'utils/provider';
 
 import { useWeb3React } from './useWeb3React';
 
 export default function useRequireSupportedChain() {
-  const { chainId } = useWeb3React();
+  const { chainId, connector, providerType } = useWeb3React();
   const { showError } = useToast();
 
   useEffect(() => {
@@ -18,7 +20,15 @@ export default function useRequireSupportedChain() {
       showError(
         `Contract interactions do not support chain ${chainId}. Please switch to Ethereum Mainnet.`
       );
-      switchNetwork('1');
+
+      // Only prompt to switch networks if using Injected connector
+      const connectorName = connector
+        ? findConnectorName(connector)
+        : providerType;
+
+      if (connectorName === EConnectorNames.Injected) {
+        switchNetwork('1');
+      }
     }
   }, [chainId]);
 }
