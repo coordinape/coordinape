61d717a29c4930f5ae65ec868191c95ce658b3d3
Wing <100873710+wingfeatherwave@users.noreply.github.com>
2023-08-18 11:04:43 -0700

    Feat/cosoul puppet (#2260)
    * webgl messaging
    
    * refactor webgl check.  add aggression check.  nice messaging.
    
    * comment about hiding message
    
    * aggressive message removal placement
    
    * add else statement
    
    * another try to remove aggresive message
    
    * another stab at removing the msssagging
    
    * just log the stupid thing
    
    * gl compile check
    
    * remove console log
    
    * be sure to place test canvas outside of conditions
    
    * check webgl for art only public view page
    
    * check webgl in cosoulart component for more flexibility
    
    * check webgl in cosoulartcontainer component instead of parent
    
    * screenshot ts?
    
    * screenshot with ts
    
    * remove images
    
    * screenshot to s3
    
    ---------
    
    Co-authored-by: CryptoGraffe <graffe@graffe.co>

---
+++ src/features/cosoul/art/screenshot.ts
@@ -0,0 +1,27 @@
+import { launch } from 'puppeteer';
+
+import { uploadImage } from '../../../../api-lib/s3';
+
+// const BASE_URL = 'http://localhost:3000/cosoul/image/';
+const BASE_URL = 'https://app.coordinape.com/cosoul/image/';
+
+export async function screenshotCoSoul(tokenId: number): Promise<Buffer> {
+  const browser = await launch({
+    headless: 'new',
+    args: ['--enable-gpu'],
+  });
+  const page: any = await browser.newPage();
+  await page.setViewport({ width: 1000, height: 1000 });
+
+  await page.goto(`${BASE_URL}${tokenId}`);
+
+  const element = await page.$('#cosoulSolo');
+  const buffer = await element.screenshot();
+  await browser.close();
+  return buffer;
+}
+
+export async function storeCoSoulImage(tokenId: number) {
+  const buffer = await screenshotCoSoul(tokenId);
+  await uploadImage(`cosoul/${tokenId}.png`, buffer);
+}
