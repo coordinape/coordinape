1f374d4900a482e5879b9499f73901c11524589f
crabsinger <83605543+crabsinger@users.noreply.github.com>
2023-08-31 15:37:49 -0700

    setup graphql subscriptions (#2306)

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,45 @@
 import React from 'react';
 
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { getAuthToken } from 'features/auth';
+import { createClient } from 'graphql-ws';
 import ReactDOM from 'react-dom';
 
 import { initSentry } from 'utils/reporting';
 
 import App from './App';
+import { REACT_APP_HASURA_URL } from './config/env';
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: REACT_APP_HASURA_URL.replace('http', 'ws'),
+    connectionParams: async () => {
+      const token = getAuthToken(false);
+      if (token) {
+        return {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        };
+      }
+      return {};
+    },
+  })
+);
+
+const client = new ApolloClient({
+  link: wsLink,
+  cache: new InMemoryCache(),
+});
 
 initSentry();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
