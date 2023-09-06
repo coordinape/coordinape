924e6ede68ab8016ecf9425bea61dfc23dad2a4d
Wing <wingfeatherwave@gmail.com>
2023-09-06 15:49:35 -0700

    query circle data to show circle avatar in activity page header

--- src/pages/CircleActivityPage/CircleActivityPage.tsx
+++ src/pages/CircleActivityPage/CircleActivityPage.tsx
@@ -2,13 +2,18 @@ import assert from 'assert';
 import { useState } from 'react';
 
 import { EpochEndingNotification } from 'features/nav/EpochEndingNotification';
+import {
+  getCircleData,
+  QUERY_KEY_CIRCLE_DATA,
+} from 'features/orgs/getCircleData';
+import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 
 import { ActivityList } from '../../features/activities/ActivityList';
 import { SingleColumnLayout } from '../../ui/layouts';
 import isFeatureEnabled from 'config/features';
 import { ContributionForm } from 'pages/ContributionsPage/ContributionForm';
-import { ContentHeader, Flex, Text } from 'ui';
+import { Avatar, ContentHeader, Flex, Text } from 'ui';
 
 export const CircleActivityPage = () => {
   const [showLoading, setShowLoading] = useState(false);
@@ -17,6 +22,12 @@ export const CircleActivityPage = () => {
   assert(circleIdS);
 
   const circleId = parseInt(circleIdS);
+  const query = useQuery(
+    [QUERY_KEY_CIRCLE_DATA, circleId],
+    () => getCircleData(circleId as number),
+    { enabled: !!circleId, staleTime: Infinity }
+  );
+  const circle = query.data?.circles_by_pk;
 
   return (
     <SingleColumnLayout>
@@ -30,7 +41,14 @@ export const CircleActivityPage = () => {
           }}
         >
           <Flex alignItems="center" css={{ gap: '$sm' }}>
-            <Text h1>Activity</Text>
+            <Text h1 css={{ gap: '$sm' }}>
+              <Avatar
+                path={circle?.logo}
+                size="small"
+                name={circle?.name || ''}
+              />
+              {circle?.name || ''} Activity
+            </Text>
             <EpochEndingNotification
               circleId={circleId}
               css={{ gap: '$sm' }}
