26927c571548a5cda73eed65c27121bfad8a45c2
sshmm <s.shawky92@gmail.com>
2023-09-15 01:34:38 +0300

    add copying sql to the migration

--- hasura/migrations/default/1694549051704_alter_table_public_users_add_column_profile_id/up.sql
+++ hasura/migrations/default/1694549051704_alter_table_public_users_add_column_profile_id/up.sql
@@ -1,2 +1,7 @@
 alter table "public"."users"
-add column "profile_id" bigint null;
+add column "profile_id" bigint;
+UPDATE "public"."users"
+SET profile_id = profiles.id
+FROM "public"."profiles"
+WHERE "public"."users".address = profiles.address;
+
