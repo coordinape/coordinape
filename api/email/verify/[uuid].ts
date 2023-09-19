0fb66db8b900344aeee4794525fecfc86e7907bb
crabsinger <83605543+crabsinger@users.noreply.github.com>
2023-09-19 12:20:53 -0700

    Ensure never verifying mulitple of same emails

--- api/email/verify/[uuid].ts
+++ api/email/verify/[uuid].ts
@@ -3,7 +3,7 @@ import assert from 'assert';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 import { adminClient } from '../../../api-lib/gql/adminClient';
-import { errorResponse } from '../../../api-lib/HttpError';
+import { errorResponse, UnprocessableError } from '../../../api-lib/HttpError';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
@@ -14,6 +14,10 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
     assert(uuid, 'no uuid provided');
 
+    // ensure there are no other verified emails for this email
+    await checkForOtherVerifiedEmails(uuid);
+
+    // then verify the email
     const data = await setVerifiedAt(uuid);
     return res
       .status(200)
@@ -21,6 +25,49 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   } catch (error: any) {
     return errorResponse(res, error);
   }
+}
+
+async function checkForOtherVerifiedEmails(uuid: string) {
+  const { emails } = await adminClient.query(
+    {
+      emails: [
+        {
+          where: {
+            verification_code: { _eq: uuid },
+          },
+        },
+        { email: true },
+      ],
+    },
+    { operationName: 'verifyEmail__getEmail' }
+  );
+
+  const email = emails[0].email;
+  assert(email, 'no email found for verification code');
+
+  // check if this email is already verified
+  const { emails: verified_emails } = await adminClient.query(
+    {
+      emails: [
+        {
+          where: {
+            email: { _eq: emails[0].email },
+            verified_at: { _is_null: false },
+          },
+        },
+        { email: true },
+      ],
+    },
+    { operationName: 'verifyEmail__checkForOtherEmails' }
+  );
+
+  if (verified_emails.length > 0) {
+    throw new UnprocessableError(
+      'email is already verified: ' + emails[0].email
+    );
+  }
+
+  return emails;
 }
 
 async function setVerifiedAt(uuid: string) {
@@ -42,7 +89,9 @@ async function setVerifiedAt(uuid: string) {
   );
 
   if (update_emails?.affected_rows != 1) {
-    throw new Error('no unverified email found for verification code');
+    throw new UnprocessableError(
+      'no unverified email found for verification code'
+    );
   }
 
   return update_emails;
