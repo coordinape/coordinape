576f57db8bd87a37bc60dad0d8876e245681f11b
CryptoGraffe <graffe@graffe.co>
2023-10-16 22:30:39 -0700

    fixing deploy

--- hardhat/scripts/deploy/04-soulkeys/00-soulkeys-deploy.ts
+++ hardhat/scripts/deploy/04-soulkeys/00-soulkeys-deploy.ts
@@ -6,12 +6,10 @@ const FIVE_PERCENT_IN_WEI = (1.0 / WEI) * 0.05;
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const contractName = 'SoulKeys';
-  console.log('deploying', 1);
   // TODO: add a different deployer for the soulkeys contract
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  console.log('deploying', deployer);
   // Deploy the cosoul implementation and proxy
   const soulkeys_deploy = await deploy(contractName, {
     contract: contractName,
@@ -19,13 +17,6 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     args: [],
     log: true,
   });
-  console.log('deploying', 3);
-  console.log('deployed soul_keys', !!soulkeys_deploy);
-
-  const soulKeys = await deploy.get('SoulKeys');
-  await soulKeys.setSubjectFeePercent(FIVE_PERCENT_IN_WEI);
-  await soulKeys.setFeeDestination(deployer);
-  await soulKeys.setProtocolFeePercent(FIVE_PERCENT_IN_WEI);
 };
 
 export default func;
