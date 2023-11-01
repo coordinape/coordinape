b4fd9c8f91fd820fe0a3871fbedd058af9cb9d18
crabsinger <83605543+crabsinger@users.noreply.github.com>
2023-11-01 14:59:45 -0700

    fix local dev check (#2396)

--- api/hasura/cron/fetchPoapData.ts
+++ api/hasura/cron/fetchPoapData.ts
@@ -1,11 +1,11 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
+import { IS_LOCAL_ENV } from '../../../api-lib/config';
 import { fetchPoapDataForTopCosouls } from '../../../api-lib/poap/poap-api';
 import { verifyHasuraRequestMiddleware } from '../../../api-lib/validate';
-import { IN_DEVELOPMENT } from '../../../src/config/env';
 
 async function handler(req: VercelRequest, res: VercelResponse) {
-  if (!IN_DEVELOPMENT) {
+  if (!IS_LOCAL_ENV) {
     await fetchPoapDataForTopCosouls();
   }
 
