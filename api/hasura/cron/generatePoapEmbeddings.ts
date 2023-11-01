b813dbfc1f5e4b304e08d95d233760f96afe48e1
crabsinger <83605543+crabsinger@users.noreply.github.com>
2023-11-01 14:58:37 -0700

    fix local dev check

--- api/hasura/cron/generatePoapEmbeddings.ts
+++ api/hasura/cron/generatePoapEmbeddings.ts
@@ -1,15 +1,11 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
+import { IS_LOCAL_ENV } from '../../../api-lib/config';
 import { generateEmbeddings } from '../../../api-lib/poap/generate-embeddings';
 import { verifyHasuraRequestMiddleware } from '../../../api-lib/validate';
-import { IN_DEVELOPMENT } from '../../../src/config/env';
 
 async function handler(req: VercelRequest, res: VercelResponse) {
-  // eslint-disable-next-line no-console
-  console.log('cron generatePoapEmbeddings started');
-  if (!IN_DEVELOPMENT) {
-    // eslint-disable-next-line no-console
-    console.log('running generatePoapEmbeddings');
+  if (!IS_LOCAL_ENV) {
     await generateEmbeddings();
   }
 
