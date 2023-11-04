d500c7c37d63b46d00c509f5478619a0168bd313
CryptoGraffe <graffe@graffe.co>
2023-11-04 16:55:37 -0700

    remove console log

--- src/pages/InviteCodePage.tsx
+++ src/pages/InviteCodePage.tsx
@@ -7,9 +7,7 @@ import { paths } from '../routes/paths';
 export const InviteCodePage = () => {
   const { code } = useParams();
 
-  console.log('INVITEPAGE!!!');
   if (code) {
-    console.log('INVITEPAGE.SETCOOKIE!!!', code);
     Cookies.set(INVITE_CODE_COOKIE, code, { expires: 1 });
   }
   return <Navigate to={paths.soulKeys} />;
