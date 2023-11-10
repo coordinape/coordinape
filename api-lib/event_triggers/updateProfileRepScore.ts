67213d127b28e584b71f549ffd1ec36f8294d20c
crabsinger <83605543+crabsinger@users.noreply.github.com>
2023-11-10 09:08:24 -0800

    Cleanup and simple refactors

--- api-lib/event_triggers/updateProfileRepScore.ts
+++ api-lib/event_triggers/updateProfileRepScore.ts
@@ -3,12 +3,16 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { updateRepScore } from '../../src/features/rep/api/updateRepScore';
 import { EventTriggerPayload } from '../types';
 
+type Tables =
+  | 'twitter_accounts'
+  | 'github_accounts'
+  | 'linkedin_accounts'
+  | 'emails';
+
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
-    const payload: EventTriggerPayload<
-      'twitter_accounts' | 'github_accounts' | 'linkedin_accounts' | 'emails',
-      'INSERT' | 'DELETE' | 'UPDATE'
-    > = req.body;
+    const payload: EventTriggerPayload<Tables, 'INSERT' | 'DELETE' | 'UPDATE'> =
+      req.body;
     const profileId: number =
       payload.event.data.new?.profile_id ?? payload.event.data.old?.profile_id;
     await updateRepScore(profileId);
