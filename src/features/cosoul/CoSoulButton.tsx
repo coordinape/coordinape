fa8e1349096e9fe3c9324de8a46219b8329c316a
crabsinger <83605543+crabsinger@users.noreply.github.com>
2023-11-14 18:30:59 -0800

    Add balacne check (#2430)

--- src/features/cosoul/CoSoulButton.tsx
+++ src/features/cosoul/CoSoulButton.tsx
@@ -1,18 +1,36 @@
 import assert from 'assert';
 
+import { ethers } from 'ethers';
+import { useQuery } from 'react-query';
+
 import { useToast } from '../../hooks';
 import { useWeb3React } from '../../hooks/useWeb3React';
-import { Button, Text } from '../../ui';
+import { Button, Flex, Link, Panel, Text } from '../../ui';
 import { switchToCorrectChain } from '../web3/chainswitch';
 
 import { chain } from './chains';
 import { MintOrBurnButton } from './MintOrBurnButton';
 import { useCoSoulContracts } from './useCoSoulContracts';
 
+const MIN_BALANCE = ethers.utils.parseEther('0.005');
+
 export const CoSoulButton = ({ onReveal }: { onReveal(): void }) => {
   const { library, chainId, account } = useWeb3React();
   const contracts = useCoSoulContracts();
   const { showError } = useToast();
+
+  const { data: balance } = useQuery(
+    ['balanceOf', account],
+    async () => {
+      if (account) {
+        return await library?.getBalance(account);
+      }
+    },
+    {
+      refetchInterval: 2000,
+      enabled: !!account,
+    }
+  );
 
   const onCorrectChain = chainId === Number(chain.chainId);
 
@@ -30,6 +48,27 @@ export const CoSoulButton = ({ onReveal }: { onReveal(): void }) => {
       <Button color="cta" size="large" onClick={safeSwitchToCorrectChain}>
         Switch to {chain.chainName} to Mint
       </Button>
+    );
+  }
+
+  if (balance?.lt(MIN_BALANCE)) {
+    return (
+      <>
+        <Panel css={{ gap: '$sm' }}>
+          <Flex>
+            <Text semibold color={'alert'}>
+              Your balance is {ethers.utils.formatEther(balance).slice(0, 10)}{' '}
+              ETH.
+            </Text>
+          </Flex>
+          <Flex>
+            <Text>Please deposit more ETH to your wallet.</Text>
+          </Flex>
+          <Button as={Link} href="https://app.optimism.io/bridge/deposit">
+            Bridge ETH using the Optimism Bridge
+          </Button>
+        </Panel>
+      </>
     );
   }
 
