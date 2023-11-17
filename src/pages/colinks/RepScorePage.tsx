cf635278f9fbbcaea74d9fbe56299e264a3ab016
Wing <wingfeatherwave@gmail.com>
2023-11-17 11:25:18 -0800

    init cosoul on score page

--- src/pages/colinks/RepScorePage.tsx
+++ src/pages/colinks/RepScorePage.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-
+import { artWidth, QUERY_KEY_COSOUL_VIEW } from 'features/cosoul';
+import { CoSoulArt } from 'features/cosoul/art/CoSoulArt';
+import { CoSoulArtContainer } from 'features/cosoul/CoSoulArtContainer';
+import { CoSoulCompositionRep } from 'features/cosoul/CoSoulCompositionRep';
+import { CoSoulPromo } from 'features/cosoul/CoSoulPromo';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 
+import { CosoulData } from '../../../api/cosoul/[address]';
 import { LoadingIndicator } from '../../components/LoadingIndicator';
 import { useAuthStore } from '../../features/auth';
 import { InviteCodeLink } from '../../features/invites/InviteCodeLink';
-import { client } from '../../lib/gql/client';
 import { Avatar, ContentHeader, Flex, Panel, Text } from '../../ui';
 import { SingleColumnLayout } from '../../ui/layouts';
 
@@ -14,130 +17,114 @@ export const RepScorePage = () => {
   const profileId = useAuthStore(state => state.profileId);
 
   const { address } = useParams();
-  const { data, isLoading } = useQuery(
-    ['repscore', address, 'details'],
-    async () => {
-      const { profiles_public } = await client.query(
-        {
-          profiles_public: [
-            {
-              where: {
-                address: {
-                  _eq: address,
-                },
-              },
-            },
-            {
-              name: true,
-              avatar: true,
-              id: true,
-              reputation_score: {
-                total_score: true,
-                github_score: true,
-                twitter_score: true,
-                linkedin_score: true,
-                poap_score: true,
-                email_score: true,
-                links_score: true,
-                pgive_score: true,
-                invite_score: true,
-              },
-            },
-          ],
-        },
-        {
-          operationName: 'repscore_details',
-        }
-      );
-      return profiles_public.pop();
+  let coSoulMinted;
+  const {
+    data: cosoul_data,
+    isLoading: coSoulLoading,
+    isError,
+    error,
+  } = useQuery(
+    [QUERY_KEY_COSOUL_VIEW, address],
+    async (): Promise<CosoulData> => {
+      const res = await fetch('/api/cosoul/' + address);
+      if (res.status === 404) {
+        coSoulMinted = false;
+      } else if (!res.ok) {
+        throw new Error('Failed to fetch cosoul data');
+      }
+      return res.json();
+    },
+    {
+      enabled: !!address,
+      retry: false,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+      staleTime: Infinity,
     }
   );
+  coSoulMinted = !!cosoul_data?.mintInfo;
 
-  if (isLoading && data === undefined) {
+  // Error
+  if (isError) {
+    return (
+      <SingleColumnLayout
+        css={{
+          m: 'auto',
+          alignItems: 'center',
+          gap: 0,
+          maxWidth: '1200px',
+          minHeight: '100vh',
+        }}
+      >
+        <Text h1 color="alert">
+          {(error as string).toString()}
+        </Text>
+      </SingleColumnLayout>
+    );
+  }
+  if (coSoulLoading && cosoul_data === undefined) {
     return <LoadingIndicator />;
   }
-  if (data === undefined) {
+  if (cosoul_data === undefined) {
     return <Panel>User not found</Panel>;
   }
   return (
     <SingleColumnLayout>
-      <ContentHeader>
+      <ContentHeader transparent>
         <Flex alignItems="center" css={{ gap: '$sm' }}>
           <Avatar
             size="large"
-            name={data.name}
-            path={data.avatar}
+            name={cosoul_data.profileInfo.name}
+            path={cosoul_data.profileInfo.avatar}
             margin="none"
             css={{ mr: '$sm' }}
           />
           <Flex column>
             <Text h2 display css={{ color: '$secondaryButtonText' }}>
-              {data.name}
+              {cosoul_data.profileInfo.name}
             </Text>
             <Text>Reputation Score</Text>
           </Flex>
         </Flex>
       </ContentHeader>
-      {!data.reputation_score ? (
-        <Panel>No score yet.</Panel>
-      ) : (
-        <Flex column css={{ maxWidth: '$readable', gap: '$sm' }}>
-          <ScoreItem
-            big={true}
-            title="Total"
-            score={data.reputation_score.total_score}
-          />
-          <ScoreItem
-            title="GitHub"
-            score={data.reputation_score.github_score}
-          />
-          <ScoreItem
-            title="Twitter"
-            score={data.reputation_score.twitter_score}
-          />
-          <ScoreItem
-            title="LinkedIn"
-            score={data.reputation_score.linkedin_score}
-          />
-          <ScoreItem title="POAP" score={data.reputation_score.poap_score} />
-          <ScoreItem title="Email" score={data.reputation_score.email_score} />
-          <ScoreItem title="Links" score={data.reputation_score.links_score} />
-          <ScoreItem title="PGIVE" score={data.reputation_score.pgive_score} />
-          <ScoreItem
-            title="Invites"
-            score={data.reputation_score.invite_score}
-          />
-        </Flex>
-      )}
-
-      {!!profileId && data.id == profileId && (
+      {!!profileId && cosoul_data.profileInfo.id == profileId && (
         <InviteCodeLink profileId={profileId} />
       )}
+      {cosoul_data && coSoulMinted ? (
+        <>
+          <CoSoulCompositionRep cosoul_data={cosoul_data}>
+            <CoSoulArtContainer cosoul_data={cosoul_data}>
+              <CoSoulArt
+                pGive={cosoul_data.totalPgive}
+                repScore={cosoul_data.repScore}
+                address={address}
+              />
+            </CoSoulArtContainer>
+          </CoSoulCompositionRep>
+        </>
+      ) : (
+        <Flex
+          column
+          css={{
+            justifyContent: 'center',
+            height: `${artWidth}`,
+            alignItems: 'center',
+            position: 'relative',
+            gap: '$sm',
+          }}
+        >
+          <Text tag color="secondary">
+            No CoSoul minted for this address
+          </Text>
+          {cosoul_data && (
+            <CoSoulPromo
+              css={{ mt: 0 }}
+              cosoul_data={cosoul_data}
+              address={address}
+            />
+          )}
+        </Flex>
+      )}
     </SingleColumnLayout>
-  );
-};
-
-const ScoreItem = ({
-  title,
-  score,
-  big,
-}: {
-  title: string;
-  score: number;
-  big?: boolean;
-}) => {
-  return (
-    <Flex css={{ gap: '$md' }}>
-      <Text
-        size={big ? 'large' : 'medium'}
-        semibold={big ? true : false}
-        css={{ width: '100px', justifyContent: 'flex-end' }}
-      >
-        {score}
-      </Text>
-      <Text size={big ? 'large' : 'medium'} semibold>
-        {title}
-      </Text>
-    </Flex>
   );
 };
