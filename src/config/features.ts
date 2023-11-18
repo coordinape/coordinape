023cb6f191de9bd056864bc5f7536c78d13f6bf8
graffe <graffe@graffe.co>
2023-11-17 17:31:15 -0800

    use env var to feature flag cosouls

--- src/config/features.ts
+++ src/config/features.ts
@@ -12,6 +12,7 @@ export type FeatureName =
 // hardcode or set with environment variables
 const staticFeatureFlags: Partial<Record<FeatureName, boolean>> = {
   rep_cosouls: process.env.REP_COSOULS == 'true',
+  soulkeys: process.env.COLINKS_ENABLED == 'true',
 };
 
 // this code is safe to use in a non-browser environment because of the typeof
