2d5ef830a383f6c97a8ce430cb89febe3f461c4e
Wing <wingfeatherwave@gmail.com>
2023-11-29 15:16:19 -0800

    merge main

--- src/pages/colinks/ViewProfilePage/ViewProfilePageContents.tsx
+++ src/pages/colinks/ViewProfilePage/ViewProfilePageContents.tsx
@@ -2,6 +2,7 @@ import assert from 'assert';
 import React, { useContext, useEffect, useState } from 'react';
 
 import { CoLinks } from '@coordinape/hardhat/dist/typechain/CoLinks';
+import { artWidthMobile } from 'features/cosoul/constants';
 import { useQuery } from 'react-query';
 
 import { LoadingModal } from '../../../components';
@@ -24,11 +25,12 @@ import { Briefcase, Clock, Users } from '../../../icons/__generated';
 import { client } from '../../../lib/gql/client';
 import { coLinksPaths } from '../../../routes/paths';
 import { AppLink, Flex, Link, Panel, Text } from '../../../ui';
-import { TwoColumnSmallRightLayout } from '../../../ui/layouts';
 import { CoLinksTaskCards } from '../CoLinksTaskCards';
 import { CoSoulItem } from 'pages/CoSoulExplorePage/CoSoulItem';
+import { SingleColumnLayout } from 'ui/layouts';
 
 import { CoLinksProfileHeader } from './CoLinksProfileHeader';
+
 const LINK_HOLDERS_LIMIT = 5;
 const LINKS_HOLDING_LIMIT = 5;
 
@@ -211,241 +213,239 @@ const PageContents = ({
   }
 
   return (
-    <TwoColumnSmallRightLayout>
-      <Flex column css={{ gap: '$1xl', flex: 2 }}>
-        <CoLinksProfileHeader
-          showLoading={showLoading}
-          setShowLoading={setShowLoading}
-          target={targetProfile}
-          currentUserAddress={currentUserAddress}
-          targetAddress={targetAddress}
-          contract={contract}
-        />
-        {targetIsCurrentUser && (
-          <CoLinksTaskCards currentUserAddress={currentUserAddress} />
-        )}
-        {needsToBuyLink === true && (
-          <Flex
-            css={{
-              alignItems: 'center',
-              mb: '$xl',
-              borderRadius: '$3',
-              background: '$surface',
-              overflow: 'clip',
-              '@sm': { flexDirection: 'column' },
-            }}
-          >
+    <SingleColumnLayout>
+      <Flex css={{ gap: '$xl' }}>
+        <Flex column css={{ gap: '$xl', flexGrow: 1 }}>
+          <CoLinksProfileHeader
+            showLoading={showLoading}
+            setShowLoading={setShowLoading}
+            target={targetProfile}
+            currentUserAddress={currentUserAddress}
+            targetAddress={targetAddress}
+            contract={contract}
+          />
+          {needsToBuyLink === true && (
             <Flex
               css={{
-                flexGrow: 1,
-                height: '100%',
-                minHeight: '200px',
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'bottom',
-                backgroundSize: 'cover',
-                backgroundImage: "url('/imgs/background/colink-other.jpg')",
-              }}
-            ></Flex>
-
-            <Panel
-              css={{
-                flex: 2,
-                p: 0,
-                border: 'none',
-                borderRadius: 0,
+                alignItems: 'center',
+                mb: '$xl',
+                borderRadius: '$3',
+                background: '$surface',
+                overflow: 'clip',
+                '@sm': { flexDirection: 'column' },
               }}
             >
               <Flex
                 css={{
-                  p: '$lg $lg $sm',
-                  gap: '$sm',
-                  alignItems: 'flex-start',
+                  flexGrow: 1,
+                  height: '100%',
+                  minHeight: '200px',
+                  backgroundRepeat: 'no-repeat',
+                  backgroundPosition: 'center',
+                  backgroundSize: 'cover',
+                  backgroundImage: "url('/imgs/background/colink-other.jpg')",
                 }}
-                column
-              >
-                <Text semibold>
-                  {targetBalance === undefined ? (
-                    <LoadingIndicator />
-                  ) : targetBalance > 0 ? (
-                    <Flex column css={{ gap: '$sm', alignItems: 'flex-start' }}>
-                      <Text tag color="complete">
-                        {targetProfile.profile.name} owns your link
-                      </Text>{' '}
-                      <Text size="large" semibold>
-                        Become Mutual Friends
-                      </Text>
-                      <Text inline>Buy theirs to become Mutual Friends!</Text>
-                    </Flex>
-                  ) : (
-                    <Flex column css={{ gap: '$sm' }}>
-                      <Text size="large" semibold>
-                        Link Up
-                      </Text>
-                      <Text>{`Connect to see each other's posts`}</Text>
-                    </Flex>
-                  )}
-                </Text>
-              </Flex>
-              <Flex css={{ p: '$md $lg $lg' }}>
-                <BuyOrSellCoLinks
-                  css={{
-                    alignItems: 'flex-start',
-                    '>div': { width: '100%', alignItems: 'center' },
-                    '.bridgeContainer': {
-                      width: '100%',
-                    },
-                  }}
-                  subject={targetAddress}
-                  address={currentUserAddress}
-                  hideTitle={true}
-                  constrainWidth={false}
-                />
-              </Flex>
-            </Panel>
-          </Flex>
-        )}
-        {weAreLinked && (
-          <Flex column>
-            <ActivityList
-              queryKey={[
-                QUERY_KEY_COLINKS,
-                'activity',
-                targetProfile.profile.id,
-              ]}
-              onSettled={() => setShowLoading(false)}
-              where={{
-                private_stream: { _eq: true },
-                actor_profile_id: { _eq: targetProfile.profile.id },
-              }}
-              noPosts={
-                <Panel noBorder>
-                  {targetProfile.mutedThem
-                    ? `You have muted ${targetProfile.profile.name}. Unmute to see their posts.`
-                    : (targetIsCurrentUser
-                        ? "You haven't"
-                        : `${targetProfile.profile.name} hasn't`) +
-                      ' posted yet.'}
-                </Panel>
-              }
-            />
-          </Flex>
-        )}
-      </Flex>
-      <Flex column css={{ flex: 1, gap: '$lg', mr: '$xl' }}>
-        <CoSoulItem cosoul={cosoul} exploreView={false} />
-        {targetIsCurrentUser && (
-          <InviteCodeLink profileId={currentUserProfileId} />
-        )}
-        {needsToBuyLink === false && (
-          <RightColumnSection>
-            <Flex column css={{ width: '100%' }}>
-              <BuyOrSellCoLinks
-                subject={targetAddress}
-                address={currentUserAddress}
-              />
-              {needsBootstrapping && (
-                <Panel info css={{ mt: '$lg', gap: '$md' }}>
-                  <Text inline>
-                    <strong>Buy your first Link</strong> to allow other CoLink
-                    holders to buy your Link.
-                  </Text>
-                  <Text>
-                    Your link holders will gain access to X. You will receive Y%
-                    of the price when they buy or sell.
-                  </Text>
-                  <Text>
-                    <Link> Learn More about Links</Link>
-                  </Text>
-                </Panel>
-              )}
-            </Flex>
-          </RightColumnSection>
-        )}
+              ></Flex>
 
-        <LinkHolders target={targetAddress} limit={LINK_HOLDERS_LIMIT}>
-          {(
-            list: React.ReactNode,
-            counts?: { link_holders: number; total_links: number }
-          ) => (
-            <RightColumnSection
-              title={
-                <Flex css={{ justifyContent: 'space-between', width: '100%' }}>
-                  <Text
-                    as={AppLink}
-                    to={coLinksPaths.holders(targetAddress)}
-                    color={'default'}
-                    semibold
-                  >
-                    <Users /> {counts?.link_holders} Link Holders
-                  </Text>
-                  <Text
-                    as={AppLink}
-                    to={coLinksPaths.holders(targetAddress)}
-                    color={'default'}
-                    semibold
-                  >
-                    {counts?.total_links} Total Links
+              <Panel
+                css={{
+                  flex: 2,
+                  p: 0,
+                  border: 'none',
+                  borderRadius: 0,
+                }}
+              >
+                <Flex
+                  css={{
+                    p: '$lg $md $sm',
+                    gap: '$sm',
+                    alignItems: 'center',
+                  }}
+                  column
+                >
+                  <Text semibold>
+                    {targetBalance === undefined ? (
+                      <LoadingIndicator />
+                    ) : targetBalance > 0 ? (
+                      <Flex column css={{ alignItems: 'center', gap: '$xs' }}>
+                        <Text size="large" semibold>
+                          Owns Your Link
+                        </Text>
+                        <Text>Buy theirs to become Mutual Friends</Text>
+                      </Flex>
+                    ) : (
+                      <Flex column css={{ alignItems: 'center', gap: '$xs' }}>
+                        <Text size="large" semibold>
+                          Link Up
+                        </Text>
+                        <Text>{`Connect to see each other's posts`}</Text>
+                      </Flex>
+                    )}
                   </Text>
                 </Flex>
-              }
-            >
-              <Flex column css={{ width: '100%' }}>
-                {list}
-                {counts?.link_holders &&
-                  counts.link_holders > LINK_HOLDERS_LIMIT && (
-                    <Flex css={{ justifyContent: 'flex-end' }}>
-                      <AppLink to={coLinksPaths.holders(targetAddress)}>
-                        <Text size="xs">
-                          View all {counts.link_holders} Holders
-                        </Text>
-                      </AppLink>
-                    </Flex>
-                  )}
-              </Flex>
-            </RightColumnSection>
+                <Flex css={{ p: '$md $md' }}>
+                  <BuyOrSellCoLinks
+                    css={{ alignItems: 'center' }}
+                    subject={targetAddress}
+                    address={currentUserAddress}
+                    hideTitle={true}
+                    constrainWidth={true}
+                  />
+                </Flex>
+              </Panel>
+            </Flex>
           )}
-        </LinkHolders>
-        <LinkHoldings holder={targetAddress}>
-          {(list: React.ReactNode, heldCount?: number) => (
-            <RightColumnSection
-              title={
-                <Text
-                  as={AppLink}
-                  to={coLinksPaths.holdings(targetAddress)}
-                  color={'default'}
-                  semibold
-                >
-                  <Briefcase /> Holding {heldCount} Link
-                  {heldCount == 1 ? '' : 's'}
-                </Text>
-              }
-            >
+          {targetIsCurrentUser && (
+            <CoLinksTaskCards currentUserAddress={currentUserAddress} />
+          )}
+          {weAreLinked && (
+            <Flex column>
+              <ActivityList
+                queryKey={[
+                  QUERY_KEY_COLINKS,
+                  'activity',
+                  targetProfile.profile.id,
+                ]}
+                onSettled={() => setShowLoading(false)}
+                where={{
+                  private_stream: { _eq: true },
+                  actor_profile_id: { _eq: targetProfile.profile.id },
+                }}
+                noPosts={
+                  <Panel noBorder>
+                    {targetProfile.mutedThem
+                      ? `You have muted ${targetProfile.profile.name}. Unmute to see their posts.`
+                      : (targetIsCurrentUser
+                          ? "You haven't"
+                          : `${targetProfile.profile.name} hasn't`) +
+                        ' posted yet.'}
+                  </Panel>
+                }
+              />
+            </Flex>
+          )}
+        </Flex>
+        <Flex
+          column
+          css={{ gap: '$lg', mr: '$xl', width: `${artWidthMobile}` }}
+        >
+          <CoSoulItem cosoul={cosoul} exploreView={false} />
+          {targetIsCurrentUser && (
+            <InviteCodeLink profileId={currentUserProfileId} />
+          )}
+          {needsToBuyLink === false && (
+            <RightColumnSection>
               <Flex column css={{ width: '100%' }}>
-                {list}
-                {heldCount && heldCount > LINKS_HOLDING_LIMIT && (
-                  <Flex css={{ justifyContent: 'flex-end' }}>
-                    <AppLink to={coLinksPaths.holdings(targetAddress)}>
-                      <Text size="xs">View all {heldCount} Holdings</Text>
-                    </AppLink>
-                  </Flex>
+                <BuyOrSellCoLinks
+                  subject={targetAddress}
+                  address={currentUserAddress}
+                />
+                {needsBootstrapping && (
+                  <Panel info css={{ mt: '$lg', gap: '$md' }}>
+                    <Text inline>
+                      <strong>Buy your first Link</strong> to allow other CoLink
+                      holders to buy your Link.
+                    </Text>
+                    <Text>
+                      Your link holders will gain access to X. You will receive
+                      Y% of the price when they buy or sell.
+                    </Text>
+                    <Text>
+                      <Link> Learn More about Links</Link>
+                    </Text>
+                  </Panel>
                 )}
               </Flex>
             </RightColumnSection>
           )}
-        </LinkHoldings>
-        <RightColumnSection
-          title={
-            <Flex as={AppLink} to={coLinksPaths.history(targetAddress)}>
-              <Text color={'default'} semibold>
-                <Clock /> Recent Link Transactions
-              </Text>
-            </Flex>
-          }
-        >
-          <CoLinksHistory target={targetAddress} limit={5} />
-        </RightColumnSection>
-        <Poaps address={targetAddress} />
+
+          <LinkHolders target={targetAddress} limit={LINK_HOLDERS_LIMIT}>
+            {(
+              list: React.ReactNode,
+              counts?: { link_holders: number; total_links: number }
+            ) => (
+              <RightColumnSection
+                title={
+                  <Flex
+                    css={{ justifyContent: 'space-between', width: '100%' }}
+                  >
+                    <Text
+                      as={AppLink}
+                      to={coLinksPaths.holders(targetAddress)}
+                      color={'default'}
+                      semibold
+                    >
+                      <Users /> {counts?.link_holders} Link Holders
+                    </Text>
+                    <Text
+                      as={AppLink}
+                      to={coLinksPaths.holders(targetAddress)}
+                      color={'default'}
+                      semibold
+                    >
+                      {counts?.total_links} Total Links
+                    </Text>
+                  </Flex>
+                }
+              >
+                <Flex column css={{ width: '100%' }}>
+                  {list}
+                  {counts?.link_holders &&
+                    counts.link_holders > LINK_HOLDERS_LIMIT && (
+                      <Flex css={{ justifyContent: 'flex-end' }}>
+                        <AppLink to={coLinksPaths.holders(targetAddress)}>
+                          <Text size="xs">
+                            View all {counts.link_holders} Holders
+                          </Text>
+                        </AppLink>
+                      </Flex>
+                    )}
+                </Flex>
+              </RightColumnSection>
+            )}
+          </LinkHolders>
+          <LinkHoldings holder={targetAddress}>
+            {(list: React.ReactNode, heldCount?: number) => (
+              <RightColumnSection
+                title={
+                  <Text
+                    as={AppLink}
+                    to={coLinksPaths.holdings(targetAddress)}
+                    color={'default'}
+                    semibold
+                  >
+                    <Briefcase /> Holding {heldCount} Link
+                    {heldCount == 1 ? '' : 's'}
+                  </Text>
+                }
+              >
+                <Flex column css={{ width: '100%' }}>
+                  {list}
+                  {heldCount && heldCount > LINKS_HOLDING_LIMIT && (
+                    <Flex css={{ justifyContent: 'flex-end' }}>
+                      <AppLink to={coLinksPaths.holdings(targetAddress)}>
+                        <Text size="xs">View all {heldCount} Holdings</Text>
+                      </AppLink>
+                    </Flex>
+                  )}
+                </Flex>
+              </RightColumnSection>
+            )}
+          </LinkHoldings>
+          <RightColumnSection
+            title={
+              <Flex as={AppLink} to={coLinksPaths.history(targetAddress)}>
+                <Text color={'default'} semibold>
+                  <Clock /> Recent Link Transactions
+                </Text>
+              </Flex>
+            }
+          >
+            <CoLinksHistory target={targetAddress} limit={5} />
+          </RightColumnSection>
+          <Poaps address={targetAddress} />
+        </Flex>
       </Flex>
-    </TwoColumnSmallRightLayout>
+    </SingleColumnLayout>
   );
 };
