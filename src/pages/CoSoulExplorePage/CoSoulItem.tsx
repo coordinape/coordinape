9e61b2782b886107de40fd1e7cf40c9434c9d97d
Wing <100873710+wingfeatherwave@users.noreply.github.com>
2023-11-29 16:30:13 -0800

    merge main (#2480)

--- src/pages/CoSoulExplorePage/CoSoulItem.tsx
+++ src/pages/CoSoulExplorePage/CoSoulItem.tsx
@@ -1,3 +1,6 @@
+import { CoSoulArt } from 'features/cosoul/art/CoSoulArt';
+import { artWidthMobile } from 'features/cosoul/constants';
+
 import { CoSoul } from '../../features/colinks/fetchCoSouls';
 import { useIsCoLinksSite } from '../../features/colinks/useIsCoLinksSite';
 import { Users } from '../../icons/__generated';
@@ -64,15 +67,24 @@ export const CoSoulItem = ({
               mixBlendMode: 'overlay',
             }}
           />
-          <Image
-            css={{
-              width: '100%',
-              aspectRatio: '1 / 1',
-            }}
-            /*This is hardcoded to the prod url ... not sure about this, predates the domain work -g*/
-            src={`https://app.coordinape.com/_vercel/image?url=/cdn/cosoul/screenshots/${cosoul.token_id}.png&w=512&q=100`}
-            alt="CoSoul Screenshot"
-          />
+          {exploreView ? (
+            <Image
+              css={{
+                width: '100%',
+                aspectRatio: '1 / 1',
+              }}
+              /*This is hardcoded to the prod url ... not sure about this, predates the domain work -g*/
+              src={`https://app.coordinape.com/_vercel/image?url=/cdn/cosoul/screenshots/${cosoul.token_id}.png&w=512&q=100`}
+              alt="CoSoul Screenshot"
+            />
+          ) : (
+            <CoSoulArt
+              pGive={cosoul.pgive}
+              address={cosoul.address}
+              repScore={repScore}
+              width={artWidthMobile}
+            />
+          )}
         </Box>
         <Flex
           column
@@ -85,80 +97,94 @@ export const CoSoulItem = ({
           }}
         >
           <Flex
+            column
             css={{
-              p: '$sm',
-              width: '100%',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              color: '$text',
+              p: '$sm $md',
+              gap: '$xs',
             }}
           >
-            {exploreView && (
-              <Flex
-                css={{
-                  gap: '$sm',
-                  alignItems: 'center',
-                  flexGrow: 0,
-                  flexShrink: 1,
-                  color: '$text',
-                }}
-              >
-                <Avatar
-                  path={cosoul.profile_public?.avatar}
-                  size="small"
-                  name={cosoul.profile_public?.name ?? 'New'}
-                />
-                {/*TODO: if the name is long here this doesn't properly shrink */}
-                <Text
-                  ellipsis
-                  size="small"
-                  semibold
-                  css={{ flexGrow: 0, flexShrink: 1 }}
-                >
-                  {abbreviateString(cosoul.profile_public?.name ?? 'Anon', 14)}
-                </Text>
-              </Flex>
-            )}
             <Flex
               css={{
-                alignItems: 'center',
-                flexShrink: 0,
-                width: exploreView ? undefined : '100%',
-                gap: '$md',
                 justifyContent: 'space-between',
+                alignItems: 'center',
+                color: '$text',
               }}
             >
-              <Flex css={{ gap: '$xs', alignItems: 'center' }}>
-                <Text size={'xs'}>Rep</Text>
-                <Text semibold>{repScore ?? 0}</Text>
-              </Flex>
-              {!exploreView && (
-                <>
-                  <Flex css={{ gap: '$xs', alignItems: 'center' }}>
-                    <Text size={'xs'}>Holders</Text>
-                    <Text semibold>{cosoul.holders ?? 0}</Text>
-                  </Flex>
-                  <Flex css={{ gap: '$xs', alignItems: 'center' }}>
-                    <Text size={'xs'}>Posts</Text>
-                    <Text semibold>
-                      {cosoul.profile_public?.post_count ?? 0}
-                    </Text>
-                  </Flex>
-                  <Flex css={{ gap: '$xs', alignItems: 'center' }}>
-                    <Text size={'xs'}>Posts/30d</Text>
-                    <Text semibold>
-                      {cosoul.profile_public?.post_count_last_30_days ?? 0}
-                    </Text>
-                  </Flex>
-                  <AppLink
-                    to={coLinksPaths.score(cosoul.address)}
-                    css={{ fontSize: '$xs' }}
+              {exploreView && (
+                <Flex
+                  css={{
+                    gap: '$sm',
+                    alignItems: 'center',
+                    flexGrow: 0,
+                    flexShrink: 1,
+                    color: '$text',
+                  }}
+                >
+                  <Avatar
+                    path={cosoul.profile_public?.avatar}
+                    size="small"
+                    name={cosoul.profile_public?.name ?? 'New'}
+                  />
+                  {/*TODO: if the name is long here this doesn't properly shrink */}
+                  <Text
+                    ellipsis
+                    size="small"
+                    semibold
+                    css={{ flexGrow: 0, flexShrink: 1 }}
                   >
-                    View Details
-                  </AppLink>
-                </>
+                    {abbreviateString(
+                      cosoul.profile_public?.name ?? 'Anon',
+                      14
+                    )}
+                  </Text>
+                </Flex>
               )}
+              <Flex
+                css={{
+                  alignItems: 'center',
+                  flexShrink: 0,
+                  width: exploreView ? undefined : '100%',
+                  gap: '$md',
+                  justifyContent: 'space-between',
+                }}
+              >
+                <Flex css={{ gap: '$xs', alignItems: 'center' }}>
+                  <Text size={'xs'}>Rep</Text>
+                  <Text semibold>{repScore ?? 0}</Text>
+                </Flex>
+                {!exploreView && (
+                  <>
+                    <Flex css={{ gap: '$xs', alignItems: 'center' }}>
+                      <Text size={'xs'}>Holders</Text>
+                      <Text semibold>{cosoul.holders ?? 0}</Text>
+                    </Flex>
+                    <Flex css={{ gap: '$xs', alignItems: 'center' }}>
+                      <Text size={'xs'}>Posts</Text>
+                      <Text semibold>
+                        {cosoul.profile_public?.post_count ?? 0}
+                      </Text>
+                    </Flex>
+                    <Flex css={{ gap: '$xs', alignItems: 'center' }}>
+                      <Text size={'xs'}>Posts/30d</Text>
+                      <Text semibold>
+                        {cosoul.profile_public?.post_count_last_30_days ?? 0}
+                      </Text>
+                    </Flex>
+                  </>
+                )}
+              </Flex>
             </Flex>
+            {!exploreView && (
+              <AppLink
+                to={coLinksPaths.score(cosoul.address)}
+                css={{
+                  fontSize: '$xs',
+                  textAlign: 'center',
+                }}
+              >
+                View Details
+              </AppLink>
+            )}
           </Flex>
           {exploreView && (
             <Flex
