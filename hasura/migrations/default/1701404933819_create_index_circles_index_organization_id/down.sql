263590d22c2d6c83f1f15c7b2a88e8fb5ca7a9c8
crabsinger <83605543+crabsinger@users.noreply.github.com>
2023-11-30 20:35:38 -0800

    Add more indices for common query patterns

---
+++ hasura/migrations/default/1701404933819_create_index_circles_index_organization_id/down.sql
@@ -0,0 +1,1 @@
+DROP INDEX IF EXISTS "public"."circles_index_organization_id";
---
+++ hasura/migrations/default/1701404933819_create_index_circles_index_organization_id/up.sql
@@ -0,0 +1,2 @@
+CREATE  INDEX "circles_index_organization_id" on
+  "public"."circles" using btree ("organization_id");
