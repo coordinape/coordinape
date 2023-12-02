740fc36c6daa90275c101f2eb2a4b6b007b15ff7
Graffe <95149165+CryptoGraffe@users.noreply.github.com>
2023-12-02 01:10:45 -0800

    fixing migration (#2490)

--- hasura/migrations/default/1701412112144_bootstrap_profiles_links_and_links_held/up.sql
+++ hasura/migrations/default/1701412112144_bootstrap_profiles_links_and_links_held/up.sql
@@ -1,2 +1,2 @@
-update profiles p set links=(select sum(amount) from link_holders lh where lh.target ilike p.address);
-update profiles p set links_held=(select sum(amount) from link_holders lh where lh.holder ilike p.address);
+update profiles p set links=(select COALESCE(sum(amount),0) from link_holders lh where lh.target ilike p.address);
+update profiles p set links_held=(select COALESCE(sum(amount),0) from link_holders lh where lh.holder ilike p.address);
