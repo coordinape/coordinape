f814016d401eb463754a1eddfa86b56c1a7506bf
graffe <graffe@graffe.co>
2023-12-06 10:08:48 -0800

    only fire on first buy

--- api-lib/event_triggers/linkTxNotification.ts
+++ api-lib/event_triggers/linkTxNotification.ts
@@ -86,7 +86,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     const {
       event: {
         data: {
-          new: { created_at, target, holder, tx_hash },
+          new: { created_at, target, holder, tx_hash, supply },
         },
       },
     }: EventTriggerPayload<'link_tx', 'INSERT'> = req.body;
@@ -101,7 +101,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
         message: `the user with address ${target} doesn't have a profile`,
       });
     }
-    if (profileId === actorProfileId) {
+    if (profileId === actorProfileId && supply === 1) {
       // This is a first timer! See if it was an invite!
       const invitedBy = await getInvitedBy(profileId);
       if (invitedBy) {
