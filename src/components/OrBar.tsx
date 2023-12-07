df7e81100fd553a29fbfa8a1712f3dfea4006eff
Wing <100873710+wingfeatherwave@users.noreply.github.com>
2023-12-07 11:24:59 -0800

    invite form style (#2519)
    * active invite form
    
    * verification page iamgery
    
    * remove image

--- src/components/OrBar.tsx
+++ src/components/OrBar.tsx
@@ -1,15 +1,23 @@
+import { CSS } from '@stitches/react';
+
 import { Flex, HR, Text } from '../ui';
 
-export const OrBar = ({ children }: { children: React.ReactNode }) => {
+export const OrBar = ({
+  children,
+  css,
+}: {
+  children: React.ReactNode;
+  css?: CSS;
+}) => {
   return (
     <Flex
       css={{
         alignItems: 'center',
         justifyContent: 'space-between',
         gap: '$md',
-        mt: '$sm',
         flexWrap: 'nowrap',
         width: '100%',
+        ...css,
       }}
     >
       <HR css={{ flexShrink: 2 }} />
