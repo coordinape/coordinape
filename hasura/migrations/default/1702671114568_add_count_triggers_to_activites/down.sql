b85bdd217873cd3c054748e8a3a9e7536906abde
graffe <graffe@graffe.co>
2023-12-15 12:31:42 -0800

    fixing down down down down down

--- hasura/migrations/default/1702671114568_add_count_triggers_to_activites/down.sql
+++ hasura/migrations/default/1702671114568_add_count_triggers_to_activites/down.sql
@@ -3,5 +3,8 @@ alter table "public"."activities" drop column "reply_count";
 
 DROP FUNCTION IF EXISTS update_reply_count();
 DROP FUNCTION IF EXISTS update_reaction_count();
+
+DROP TRIGGER trigger_update_reply_count ON replies;
+DROP TRIGGER trigger_update_reaction_count ON replies;
 DROP INDEX IF EXISTS "public"."activites_index_contribution_id";
 DROP INDEX IF EXISTS "public"."contributions_index_profile_id_private_stream";
