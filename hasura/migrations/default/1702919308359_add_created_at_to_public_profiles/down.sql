0d2d1e32a9bef62f3e4d27ef123ab6bdb57dce1b
graffe <graffe@graffe.co>
2023-12-18 10:43:33 -0800

    down migration

--- hasura/migrations/default/1702919308359_add_created_at_to_public_profiles/down.sql
+++ hasura/migrations/default/1702919308359_add_created_at_to_public_profiles/down.sql
@@ -1,1 +1,20 @@
-DROP VIEW "public"."profiles_public";
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+CREATE OR REPLACE VIEW "public"."profiles_public" AS
+ SELECT p.id,
+    p.address,
+    p.name,
+    p.avatar,
+    p.description,
+    count(c.*) AS post_count,
+    count(
+        CASE
+            WHEN (c.created_at >= (CURRENT_DATE - '30 days'::interval)) THEN 1
+            ELSE NULL::integer
+        END) AS post_count_last_30_days,
+    p.website,
+    p.links,
+    p.links_held
+   FROM (profiles p
+     LEFT JOIN contributions c ON (((c.profile_id = p.id) AND (c.private_stream = true))))
+  GROUP BY p.id, p.address, p.name, p.avatar;
