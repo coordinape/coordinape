55b657fda4bf712e99295fb8ac2965a6bd68a0f1
graffe <graffe@graffe.co>
2024-01-17 12:10:03 -0800

    mpa

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   // This changes the out put dir from dist to build
   // comment this out if that isn't relevant for your project
+  appType: 'mpa',
   build: {
     outDir: 'dist',
   },
