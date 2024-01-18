138a34707fede3e158f7e25173e9972d41d4a4ea
graffe <graffe@graffe.co>
2024-01-17 21:21:35 -0800

    stupid .js extensions

--- api/login.ts
+++ api/login.ts
@@ -11,18 +11,18 @@ import {
   generateTokenString,
   hashTokenString,
 } from '../api-lib/authHelpers.js';
-import { adminClient } from '../api-lib/gql/adminClient';
-import { insertInteractionEvents } from '../api-lib/gql/mutations';
-import { errorResponse } from '../api-lib/HttpError';
-import { getProvider } from '../api-lib/provider';
-import { parseInput } from '../api-lib/signature';
-import { loginSupportedChainIds } from '../src/common-lib/constants';
-import { COLINKS_LOCAL_URL, GIVE_LOCAL_URL } from '../src/config/webAppURL';
-import { getInviteCodeCookieValue } from '../src/features/invites/invitecodes';
-import { updateRepScore } from '../src/features/rep/api/updateRepScore';
-import { supportedChainIds } from '../src/lib/vaults/contracts';
+import { adminClient } from '../api-lib/gql/adminClient.js';
+import { insertInteractionEvents } from '../api-lib/gql/mutations.js';
+import { errorResponse } from '../api-lib/HttpError.js';
+import { getProvider } from '../api-lib/provider.js';
+import { parseInput } from '../api-lib/signature.js';
+import { loginSupportedChainIds } from '../src/common-lib/constants.js';
+import { COLINKS_LOCAL_URL, GIVE_LOCAL_URL } from '../src/config/webAppURL.js';
+import { getInviteCodeCookieValue } from '../src/features/invites/invitecodes.js';
+import { updateRepScore } from '../src/features/rep/api/updateRepScore.js';
+import { supportedChainIds } from '../src/lib/vaults/contracts.js';
 
-import { createSampleCircleForProfile } from './hasura/actions/_handlers/createSampleCircle';
+import { createSampleCircleForProfile } from './hasura/actions/_handlers/createSampleCircle.js';
 
 Settings.defaultZone = 'utc';
 
