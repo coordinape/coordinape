fbab2b7a322a58ac5f6d15b6c2a080a716d98da7
Wing <wingfeatherwave@gmail.com>
2024-02-15 15:30:41 -0800

    remove lightbox eyeball

--- src/ui/MarkdownPreview/LightboxImage.tsx
+++ src/ui/MarkdownPreview/LightboxImage.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 
-import { Eye } from 'icons/__generated';
 import { Text, Modal, Image } from 'ui';
 
 export const LightboxImage = ({ src, alt }: { src: string; alt: string }) => {
@@ -16,40 +15,9 @@ export const LightboxImage = ({ src, alt }: { src: string; alt: string }) => {
           position: 'relative',
           overflow: 'hidden',
           backgroundImage: `url('${src})`,
-          '&:hover, &:focus': {
-            '.lightboxButton': {
-              left: -50,
-              top: -50,
-            },
-          },
         }}
       >
         <Image src={src} alt={alt} />
-        <Text
-          className="lightboxButton"
-          css={{
-            display: 'flex',
-            background: '$surface',
-            position: 'absolute',
-            alignItems: 'center',
-            justifyContent: 'center',
-            width: 100,
-            height: 100,
-            left: -100,
-            top: -100,
-            transform: 'rotate(-45deg)',
-            transition: 'all .2s ease-in-out',
-          }}
-        >
-          <Eye
-            size="lg"
-            css={{
-              transform: 'rotate(45deg)',
-              position: 'absolute',
-              bottom: 10,
-            }}
-          />
-        </Text>
       </Text>
       <Modal
         lightbox
