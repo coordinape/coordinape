b5aa547504902b5b517b8017a4d7f349e7fd87dc
Wing <wingfeatherwave@gmail.com>
2024-02-20 14:15:51 -0800

    styling wizard step

--- src/features/colinks/wizard/WizardCompetition.tsx
+++ src/features/colinks/wizard/WizardCompetition.tsx
@@ -20,40 +20,37 @@ export const WizardCompetition = ({ skipStep }: { skipStep: () => void }) => {
         css={{
           ...fullScreenStyles,
           backgroundImage: "url('/imgs/background/eth-denver.jpg')",
-          backgroundPosition: 'bottom',
+          backgroundPosition: '50% 65%',
         }}
       />
       <WizardInstructions>
-        <Flex column css={{ gap: '$sm' }}>
-          <Text h2>
-            Congratulations! You&apos;ve entered the ETH Denver 2024 Contest!
+        <Flex column css={{ gap: '$md' }}>
+          <Text h2>Congratulations!</Text>
+          <Text h2>You&apos;ve entered the ETH Denver 2024 Contest!</Text>
+          <AppLink to={coLinksPaths.invites}>
+            Now, go build your network!
+          </AppLink>
+          <Text inline>
+            Invite your friends to CoLinks, and for each one that joins
+            you&apos;ll receive an additional contest entry (max 10 additional
+            entries).
           </Text>
-          <Flex column>
-            <AppLink to={coLinksPaths.invites}>
-              Now, go build your network!
-            </AppLink>
-            <Text inline>
-              Invite your friends to CoLinks, and for each one that joins
-              you&apos;ll receive an additional contest entry (max 10 additional
-              entries).
-            </Text>
-          </Flex>
+          <Button
+            outlined
+            color={'secondary'}
+            as={Link}
+            inlineLink
+            onClick={skipStep}
+            css={{
+              '&:hover': { textDecoration: 'none' },
+              width: '100%',
+              alignItems: 'center',
+              gap: '$xs',
+            }}
+          >
+            <Text>Continue</Text> <ArrowRight />
+          </Button>
         </Flex>
-        <Button
-          outlined
-          color={'secondary'}
-          as={Link}
-          inlineLink
-          onClick={skipStep}
-          css={{
-            '&:hover': { textDecoration: 'none' },
-            width: '100%',
-            alignItems: 'center',
-            gap: '$xs',
-          }}
-        >
-          <Text>Continue</Text> <ArrowRight />
-        </Button>
       </WizardInstructions>
     </>
   );
