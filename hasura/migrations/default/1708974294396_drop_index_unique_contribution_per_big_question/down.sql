194c72cbd01ff086d2db63f3bbe50819713f3c0f
graffe <graffe@graffe.co>
2024-02-26 11:20:09 -0800

    fixing migration

--- hasura/migrations/default/1708974294396_drop_index_unique_contribution_per_big_question/down.sql
+++ hasura/migrations/default/1708974294396_drop_index_unique_contribution_per_big_question/down.sql
@@ -1,2 +1,2 @@
-CREATE  INDEX "unique_contribution_per_big_question" on
+CREATE UNIQUE INDEX "unique_contribution_per_big_question" on
   "public"."contributions" using btree ("big_question_id", "profile_id");
