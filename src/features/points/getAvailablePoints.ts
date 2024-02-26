9ec30724b259e8452fb94de14e050d869934cc92
crabsinger <83605543+crabsinger@users.noreply.github.com>
2024-02-26 09:59:43 -0800

    Change emission rate to 1 give / day, max 5

--- src/features/points/getAvailablePoints.ts
+++ src/features/points/getAvailablePoints.ts
@@ -1,8 +1,9 @@
 import { DateTime } from 'luxon';
 
-export const MAX_POINTS_CAP = 1800;
-export const EMISSION_PER_SECOND = 180;
+// EMISSION_PER_SECOND is the rate at which points are accrued
+export const EMISSION_PER_SECOND = 1;
 export const MAX_GIVE = 5;
+export const MAX_POINTS_CAP = MAX_GIVE * EMISSION_PER_SECOND * 60 * 60 * 24;
 export const POINTS_PER_GIVE = MAX_POINTS_CAP / MAX_GIVE;
 
 export const getAvailablePoints = (balance: number, checkpoint: string) => {
