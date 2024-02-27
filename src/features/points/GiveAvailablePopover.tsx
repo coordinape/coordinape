57a5ef1e47ac1a4b558a9de947e829179045dbbb
Wing <100873710+wingfeatherwave@users.noreply.github.com>
2024-02-26 17:27:05 -0800

    Give colinks progress bar info2 (#2754)
    * mobile menu give popover
    
    * mobile popover of gives available bar
    
    * add larger icon in give header
    
    * import popover

---
+++ src/features/points/GiveAvailablePopover.tsx
@@ -0,0 +1,43 @@
+import { useQuery } from 'react-query';
+
+import { IconButton, Popover, PopoverContent, PopoverTrigger } from '../../ui';
+import { GemCoFill } from 'icons/__generated';
+
+import { POINTS_PER_GIVE } from './getAvailablePoints';
+import { getMyAvailablePoints } from './getMyAvailablePoints';
+import { POINTS_QUERY_KEY, PointsBar } from './PointsBar';
+
+export const GiveAvailablePopover = () => {
+  const { data: points } = useQuery(
+    [POINTS_QUERY_KEY],
+    async () => {
+      return await getMyAvailablePoints();
+    },
+    {
+      onError: error => {
+        console.error(error);
+      },
+    }
+  );
+  return (
+    <Popover>
+      <PopoverTrigger css={{ cursor: 'pointer' }}>
+        <IconButton css={{ width: 'auto' }}>
+          {points && Math.floor(points / POINTS_PER_GIVE)}
+          <GemCoFill fa size="lg" css={{ ml: '$xs' }} />
+        </IconButton>
+      </PopoverTrigger>
+      <PopoverContent
+        align="end"
+        css={{
+          background: '$surface',
+          p: 0,
+          width: 270,
+          mt: 5,
+        }}
+      >
+        <PointsBar open />
+      </PopoverContent>
+    </Popover>
+  );
+};
