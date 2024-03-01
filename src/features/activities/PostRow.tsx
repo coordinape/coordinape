9072e3acaa3439983984f3b9b84d3140adf1a757
Wing <wingfeatherwave@gmail.com>
2024-02-29 16:19:39 -0800

    remove min heights

--- src/features/activities/PostRow.tsx
+++ src/features/activities/PostRow.tsx
@@ -211,11 +211,6 @@ export const PostRow = ({
                     cursor: 'auto',
                     mb: '-$xs',
                     mt: '$xs',
-                    // apply min height to make room for absolute positioned GIVE button
-                    minHeight:
-                      editableContribution || !isFeatureEnabled('colinks_give')
-                        ? 0
-                        : '$3xl',
                   }}
                 />
                 <Flex
@@ -276,8 +271,8 @@ export const PostRow = ({
                             css={{
                               p: '$xs',
                               width: 'auto',
+                              minHeight: 0,
                               '*': {
-                                // fill: '$link',
                                 fill: '$secondaryText',
                               },
                             }}
