89be27285b964be26729ee90b3e9674ef352fd7c
graffe <graffe@graffe.co>
2024-03-13 14:47:43 -0700

    try  this vercel

--- _api/og/bqimage/[id].tsx
+++ _api/og/bqimage/[id].tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+// import React from 'react';
 
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { ImageResponse } from '@vercel/og';
 
+// import { ImageResponse } from '@vercel/og';
 import { getBigQuestionInfo } from '../getBigQuestionInfo.ts';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
@@ -20,72 +20,73 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       });
     }
 
-    const ir = new ImageResponse(
-      (
-        <div
-          style={{
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: `${bq.css_background_position ?? 'center'}`,
-            backgroundSize: 'cover',
-            backgroundImage: `url('${bq.cover_image_url}')`,
-            height: '100%',
-            width: '100%',
-            display: 'flex',
-            alignItems: 'flex-start',
-            justifyContent: 'flex-end',
-            flexDirection: 'column',
-            flexWrap: 'nowrap',
-            padding: '72px 32px',
-          }}
-        >
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'flex-start',
-            }}
-          >
-            <div
-              style={{
-                fontSize: '32px',
-                alignItems: 'flex-start',
-                color: '#ffffffee',
-                fontStyle: 'italic',
-                background: 'rgba(0,0,0,0.8)',
-                paddingLeft: 8,
-                paddingRight: 8,
-                width: 'auto',
-                display: 'flex',
-              }}
-            >
-              The Big Question
-            </div>
-            <div
-              style={{
-                display: 'flex',
-                alignItems: 'flex-start',
-                textAlign: 'left',
-                fontWeight: 'bold',
-                fontSize: '72px',
-                color: 'white',
-                width: '100%',
-                textShadow: 'rgb(0 0 0 / 64%) 1px 1px 8px',
-              }}
-            >
-              {bq.prompt}
-            </div>
-          </div>
-        </div>
-      ),
-      {
-        width: 1200,
-        height: 630,
-      }
-    );
-    const ab = await ir.arrayBuffer();
-    const buf = Buffer.from(ab);
-    res.setHeader('Content-Type', 'image/png');
-    return res.send(buf);
+    // const ir = new ImageResponse(
+    //   (
+    //     <div
+    //       style={{
+    //         backgroundRepeat: 'no-repeat',
+    //         backgroundPosition: `${bq.css_background_position ?? 'center'}`,
+    //         backgroundSize: 'cover',
+    //         backgroundImage: `url('${bq.cover_image_url}')`,
+    //         height: '100%',
+    //         width: '100%',
+    //         display: 'flex',
+    //         alignItems: 'flex-start',
+    //         justifyContent: 'flex-end',
+    //         flexDirection: 'column',
+    //         flexWrap: 'nowrap',
+    //         padding: '72px 32px',
+    //       }}
+    //     >
+    //       <div
+    //         style={{
+    //           display: 'flex',
+    //           flexDirection: 'column',
+    //           alignItems: 'flex-start',
+    //         }}
+    //       >
+    //         <div
+    //           style={{
+    //             fontSize: '32px',
+    //             alignItems: 'flex-start',
+    //             color: '#ffffffee',
+    //             fontStyle: 'italic',
+    //             background: 'rgba(0,0,0,0.8)',
+    //             paddingLeft: 8,
+    //             paddingRight: 8,
+    //             width: 'auto',
+    //             display: 'flex',
+    //           }}
+    //         >
+    //           The Big Question
+    //         </div>
+    //         <div
+    //           style={{
+    //             display: 'flex',
+    //             alignItems: 'flex-start',
+    //             textAlign: 'left',
+    //             fontWeight: 'bold',
+    //             fontSize: '72px',
+    //             color: 'white',
+    //             width: '100%',
+    //             textShadow: 'rgb(0 0 0 / 64%) 1px 1px 8px',
+    //           }}
+    //         >
+    //           {bq.prompt}
+    //         </div>
+    //       </div>
+    //     </div>
+    //   ),
+    //   {
+    //     width: 1200,
+    //     height: 630,
+    //   }
+    // );
+    // const ab = await ir.arrayBuffer();
+    // const buf = Buffer.from(ab);
+    // res.setHeader('Content-Type', 'image/png');
+    // return res.send(buf);
+    res.send('WHATUP');
   } catch (e: any) {
     console.error(`${e.message}`);
     return new Response(`Failed to generate the image`, {
