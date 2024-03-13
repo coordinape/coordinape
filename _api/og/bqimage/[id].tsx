a4e7227df11cfcde6f37500f5cacae435e913405
graffe <graffe@graffe.co>
2024-03-13 16:13:42 -0700

    sup

--- _api/og/bqimage/[id].tsx
+++ _api/og/bqimage/[id].tsx
@@ -35,62 +35,61 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     }
 
     const ir = new ImageResponse(
-      (
-        <div
-          style={{
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: `${bq.css_background_position ?? 'center'}`,
-            backgroundSize: 'cover',
-            backgroundImage: `url('${bq.cover_image_url}')`,
-            height: '100%',
-            width: '100%',
-            display: 'flex',
-            alignItems: 'flex-start',
-            justifyContent: 'flex-end',
-            flexDirection: 'column',
-            flexWrap: 'nowrap',
-            padding: '72px 32px',
-          }}
-        >
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'flex-start',
-            }}
-          >
-            <div
-              style={{
-                fontSize: '32px',
-                alignItems: 'flex-start',
-                color: '#ffffffee',
-                fontStyle: 'italic',
-                background: 'rgba(0,0,0,0.8)',
-                paddingLeft: 8,
-                paddingRight: 8,
-                width: 'auto',
-                display: 'flex',
-              }}
-            >
-              The Big Question
-            </div>
-            <div
-              style={{
-                display: 'flex',
-                alignItems: 'flex-start',
-                textAlign: 'left',
-                fontWeight: 'bold',
-                fontSize: '72px',
-                color: 'white',
-                width: '100%',
-                textShadow: 'rgb(0 0 0 / 64%) 1px 1px 8px',
-              }}
-            >
-              {bq.prompt}
-            </div>
-          </div>
-        </div>
-      ),
+      <div>sup</div>,
+      // <div
+      //   style={{
+      //     backgroundRepeat: 'no-repeat',
+      //     backgroundPosition: `${bq.css_background_position ?? 'center'}`,
+      //     backgroundSize: 'cover',
+      //     backgroundImage: `url('${bq.cover_image_url}')`,
+      //     height: '100%',
+      //     width: '100%',
+      //     display: 'flex',
+      //     alignItems: 'flex-start',
+      //     justifyContent: 'flex-end',
+      //     flexDirection: 'column',
+      //     flexWrap: 'nowrap',
+      //     padding: '72px 32px',
+      //   }}
+      // >
+      //   <div
+      //     style={{
+      //       display: 'flex',
+      //       flexDirection: 'column',
+      //       alignItems: 'flex-start',
+      //     }}
+      //   >
+      //     <div
+      //       style={{
+      //         fontSize: '32px',
+      //         alignItems: 'flex-start',
+      //         color: '#ffffffee',
+      //         fontStyle: 'italic',
+      //         background: 'rgba(0,0,0,0.8)',
+      //         paddingLeft: 8,
+      //         paddingRight: 8,
+      //         width: 'auto',
+      //         display: 'flex',
+      //       }}
+      //     >
+      //       The Big Question
+      //     </div>
+      //     <div
+      //       style={{
+      //         display: 'flex',
+      //         alignItems: 'flex-start',
+      //         textAlign: 'left',
+      //         fontWeight: 'bold',
+      //         fontSize: '72px',
+      //         color: 'white',
+      //         width: '100%',
+      //         textShadow: 'rgb(0 0 0 / 64%) 1px 1px 8px',
+      //       }}
+      //     >
+      //       {bq.prompt}
+      //     </div>
+      //   </div>
+      // </div>
       {
         width: 1200,
         height: 630,
