298f3c733ccc7bf9c4cf3d0a000803e843d36cac
graffe <graffe@graffe.co>
2024-03-25 19:46:36 -0400

    f

--- _api/jokes.ts
+++
@@ -1,7 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-import { RenderFrameImage } from './frames/router/FrameImage.tsx';
-
-export default async function (_req: VercelRequest, res: VercelResponse) {
-  RenderFrameImage({ res });
-}
