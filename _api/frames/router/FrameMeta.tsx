0c19766ab6f9a76525f4ebec63e2a371893f28ae
crabsinger <83605543+crabsinger@users.noreply.github.com>
2024-03-25 15:13:47 -0700

    test: no crash without renderToString

--- _api/frames/router/FrameMeta.tsx
+++ _api/frames/router/FrameMeta.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { VercelResponse } from '@vercel/node';
-import { renderToString } from 'react-dom/server';
+// import { renderToString } from 'react-dom/server';
 
 import { FrameButton } from './FrameButton';
 import { Frame, FRAME_ROUTER_URL_BASE } from './router';
@@ -51,6 +51,6 @@ export const RenderFrameMeta = ({
       </body>
     </html>
   );
-  const sString = renderToString(content);
-  return res.status(200).send(sString);
+  // const sString = renderToString(content);
+  return res.status(200).send('sString');
 };
