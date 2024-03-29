ee1e65dd13fb181cb0e84efb78f733b89a25350d
crabsinger <83605543+crabsinger@users.noreply.github.com>
2024-03-29 12:42:53 -0700

    fix routing

--- _api/frames/personas/personaResourceIdentifier.ts
+++ _api/frames/personas/personaResourceIdentifier.ts
@@ -1,8 +1,8 @@
 import { ResourceIdentifier } from '../router';
 
 export const personaResourceIdentifier: ResourceIdentifier = {
-  resourcePathExpression: '/:personaId',
-  getResourceId: (params: Record<string, string>) => {
-    return `/${params.personaId}`;
+  resourcePathExpression: '',
+  getResourceId: () => {
+    return '';
   },
 };
