72203104ff159776d12174d9ae4f673d7045c689
Wing <wingfeatherwave@gmail.com>
2024-04-03 15:55:08 -0700

    generic (not enough give) frame art

--- _api/frames/FrontDoorFrame.tsx
+++ _api/frames/FrontDoorFrame.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 
-import { OGAvatar } from '../og/OGAvatar';
-
-import { getViewerFromParams } from './_getViewerFromParams';
 import { staticResourceIdentifier } from './_staticResourceIdentifier';
 import { FrameBgImage, IMAGE_URL_BASE } from './layoutFragments/FrameBgImage';
 import { FrameBody } from './layoutFragments/FrameBody';
@@ -12,32 +9,34 @@ import { FrameHeadline } from './layoutFragments/FrameHeadline';
 import { FrameWrapper } from './layoutFragments/FrameWrapper';
 import { Frame } from './router';
 
-const imageNode = async (params: Record<string, string>) => {
-  const { viewerProfile } = await getViewerFromParams(params);
-
+const imageNode = async () => {
+  const randomArtNumber = Math.floor(Math.random() * 5) + 1;
   return (
     <FrameWrapper>
-      <FrameBgImage src="persona-1.jpg" />
+      <FrameBgImage src={`frontdoor-generic-${randomArtNumber}.jpg`} />
       <FrameBody>
         <FrameBodyGradient
           gradientStyles={{
             background:
-              'radial-gradient(circle at 25% 0%, #04AFF9 0%, #FFB800 80%)',
+              'radial-gradient(circle at 25% 0%, #0A8F57 0%, #303030 80%)',
             opacity: 0.7,
           }}
         />
         <FrameHeadline>
-          <OGAvatar avatar={viewerProfile?.avatar} />
           <div tw="flex items-center grow justify-center">
-            Generic Front Door Frame
+            <img
+              alt="gem"
+              src={IMAGE_URL_BASE + 'GemWhite.png'}
+              style={{ width: 70, height: 70, marginRight: 20 }}
+            />
+            GIVE
           </div>
-          <img
-            alt="gem"
-            src={IMAGE_URL_BASE + 'GemWhite.png'}
-            style={{ width: 70, height: 70 }}
-          />
         </FrameHeadline>
-        <FrameFooter>Heres how you use GiveBot</FrameFooter>
+        <FrameFooter>
+          Giving is free.
+          <br />
+          Let others know you appreciate them.
+        </FrameFooter>
       </FrameBody>
     </FrameWrapper>
   );
@@ -50,10 +49,9 @@ export const FrontDoor: Frame = {
   resourceIdentifier: staticResourceIdentifier,
   buttons: [
     {
-      title: 'Use on Farcaster',
+      title: 'Get GIVE',
       action: 'link',
-      target:
-        'https://warpcast.com/~/compose?text=@givebot @singer did some nice work today&embeds[]=https://farcaster.xyz',
+      target: 'https://colinks.coordinape.com',
     },
   ],
 };
