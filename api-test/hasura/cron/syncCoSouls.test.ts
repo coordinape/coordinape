c7a8084302341bfd0e8464245404aa049d783272
sshmm <s.shawky92@gmail.com>
2024-04-05 00:17:13 +0200

    update test to cover all cases

--- api-test/hasura/cron/syncCoSouls.test.ts
+++ api-test/hasura/cron/syncCoSouls.test.ts
@@ -14,10 +14,7 @@ import {
   cosouls_update_column,
 } from '../../../api-lib/gql/__generated__/zeus';
 import { adminClient } from '../../../api-lib/gql/adminClient';
-import {
-  getOnChainPGIVE,
-  getTokenId,
-} from '../../../src/features/cosoul/api/cosoul';
+import * as cosoulApi from '../../../src/features/cosoul/api/cosoul';
 import { Contracts } from '../../../src/features/cosoul/contracts';
 import {
   provider,
@@ -29,73 +26,102 @@ import { createProfile, createUser } from '../../helpers';
 let contract: CoSoul;
 let snapshotId: string;
 let mainAccount: string;
-let tokenId: Awaited<ReturnType<typeof getTokenId>>;
-let memberPGiveId: number;
+let secondAccount: string;
+let mainTokenId: Awaited<ReturnType<typeof cosoulApi.getTokenId>>;
+let secondTokenId: Awaited<ReturnType<typeof cosoulApi.getTokenId>>;
+let accounts: string[];
+let user: any;
+let user2: any;
+let mainTx: Awaited<ReturnType<typeof contract.mint>>;
+let secondTx: Awaited<ReturnType<typeof contract.mint>>;
 
-vi.spyOn(syncCosouls, 'isHistoricalPGiveFinished').mockResolvedValue(true);
 const req = {
   headers: { verification_key: process.env.HASURA_EVENT_SECRET },
 } as unknown as VercelRequest;
 const res: any = { status: vi.fn(() => res), json: vi.fn() };
-let user: any;
-let profile: any;
+describe('syncCoSouls cron', () => {
+  beforeEach(async () => {
+    vi.spyOn(syncCosouls, 'isHistoricalPGiveFinished').mockResolvedValue(true);
+    vi.spyOn(cosoulApi, 'setOnChainPGIVE');
+    vi.spyOn(cosoulApi, 'setBatchOnChainPGIVE');
+    snapshotId = await takeSnapshot();
+    accounts = await provider().listAccounts();
+    mainAccount = accounts[0];
+    const mainProfile = await createProfile(adminClient, {
+      address: mainAccount,
+    });
+    user = await createUser(adminClient, { profile_id: mainProfile.id });
+    contract = (await Contracts.fromProvider(provider())).cosoul;
+    mainTx = await contract.mint();
+    mainTokenId = await cosoulApi.getTokenId(mainAccount);
+    secondAccount = accounts[1];
+    const secondProfile = await createProfile(adminClient, {
+      address: secondAccount,
+    });
+    user2 = await createUser(adminClient, { profile_id: secondProfile.id });
+    secondTx = await cosoulApi.mintCoSoulForAddress(secondAccount);
+    secondTokenId = await cosoulApi.getTokenId(secondAccount);
+    console.log('mainTokenId', mainTokenId);
+    console.log('secondTokenId', secondTokenId);
 
-beforeEach(async () => {
-  snapshotId = await takeSnapshot();
-
-  mainAccount = (await provider().listAccounts())[0];
-  profile = await createProfile(adminClient, { address: mainAccount });
-  console.log(profile.id, profile.address);
-  user = await createUser(adminClient, { profile_id: profile.id });
-  contract = (await Contracts.fromProvider(provider())).cosoul;
-});
-
-afterEach(async () => {
-  await adminClient.mutate(
-    {
-      delete_member_epoch_pgives_by_pk: [
-        { id: memberPGiveId },
-        { __typename: true },
-      ],
-    },
-    { operationName: 'syncCoSoul__deleteMemberEpochPGive' }
-  );
-  await restoreSnapshot(snapshotId);
-});
-test('updated give is synched on chain', async () => {
-  const tx = await contract.mint();
-  tokenId = await getTokenId(mainAccount);
-  assert(tokenId);
-  await adminClient.mutate(
-    {
-      insert_cosouls_one: [
-        {
-          object: {
-            created_tx_hash: tx.hash,
-            token_id: tokenId,
-            address: mainAccount,
-            checked_at: null,
+    await adminClient.mutate(
+      {
+        delete_member_epoch_pgives: [
+          {
+            where: {},
           },
-          on_conflict: {
-            constraint: cosouls_constraint.cosouls_token_id_key,
-            update_columns: [
-              cosouls_update_column.token_id,
-              cosouls_update_column.created_tx_hash,
-              cosouls_update_column.address,
-              cosouls_update_column.checked_at,
+          { affected_rows: true },
+        ],
+      },
+      { operationName: 'syncCoSoul__deleteMembersEpochPGive' }
+    );
+
+    await adminClient.mutate(
+      {
+        insert_cosouls: [
+          {
+            objects: [
+              {
+                created_tx_hash: mainTx.hash,
+                token_id: mainTokenId,
+                address: mainAccount,
+                checked_at: null,
+              },
+              {
+                created_tx_hash: secondTx.hash,
+                token_id: secondTokenId,
+                address: secondAccount,
+                checked_at: null,
+              },
             ],
+            on_conflict: {
+              constraint: cosouls_constraint.cosouls_token_id_key,
+              update_columns: [
+                cosouls_update_column.token_id,
+                cosouls_update_column.created_tx_hash,
+                cosouls_update_column.address,
+                cosouls_update_column.checked_at,
+              ],
+            },
           },
-        },
-        {
-          id: true,
-        },
-      ],
-    },
-    {
-      operationName: 'syncCoSoulTest__insertCoSoul',
-    }
-  );
-  const { insert_member_epoch_pgives_one: memberPGive } =
+          {
+            affected_rows: true,
+          },
+        ],
+      },
+      {
+        operationName: 'syncCoSoulTest__insertCoSoul',
+      }
+    );
+  });
+
+  afterEach(async () => {
+    vi.clearAllMocks();
+    await restoreSnapshot(snapshotId);
+  });
+
+  test('updates pgive for one user on chain using setOnChainPGIVE', async () => {
+    assert(mainTokenId);
     await adminClient.mutate(
       {
         insert_member_epoch_pgives_one: [
@@ -112,10 +138,49 @@ test('updated give is synched on chain', async () => {
       },
       { operationName: 'syncCoSoul__insertMemberEpochPGive' }
     );
-  assert(memberPGive);
-  memberPGiveId = memberPGive.id;
-  await handler(req, res);
+    await handler(req, res);
+    expect(res.json).toHaveBeenCalled();
+    expect(cosoulApi.setOnChainPGIVE).toHaveBeenCalledWith(mainTokenId, 500);
+    expect(await cosoulApi.getOnChainPGIVE(mainTokenId)).toEqual(500);
+  });
 
-  expect(res.json).toHaveBeenCalled();
-  expect(await getOnChainPGIVE(tokenId)).toEqual(500);
+  test('updates pgive for multiple users on chain using setBatchOnChainPGIVE', async () => {
+    assert(mainTokenId);
+    assert(secondTokenId);
+    await adminClient.mutate(
+      {
+        insert_member_epoch_pgives: [
+          {
+            objects: [
+              {
+                user_id: user.id,
+                epoch_id: 1,
+                pgive: 320,
+                normalized_pgive: 320,
+              },
+              {
+                user_id: user2.id,
+                epoch_id: 1,
+                pgive: 330,
+                normalized_pgive: 330,
+              },
+            ],
+          },
+          { affected_rows: true },
+        ],
+      },
+      { operationName: 'syncCoSoul__insertMembersEpochPGive' }
+    );
+
+    await handler(req, res);
+    expect(res.json).toHaveBeenCalled();
+    let payload = '0x00';
+    payload +=
+      cosoulApi.getPayload(320, mainTokenId) +
+      cosoulApi.getPayload(330, secondTokenId);
+    expect(cosoulApi.setBatchOnChainPGIVE).toHaveBeenCalledWith(payload);
+    expect(cosoulApi.setOnChainPGIVE).not.toBeCalled();
+    expect(await cosoulApi.getOnChainPGIVE(mainTokenId)).toEqual(320);
+    expect(await cosoulApi.getOnChainPGIVE(secondTokenId)).toEqual(330);
+  });
 });
