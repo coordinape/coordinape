712ca45b6ee9d248138383301a840d8e41591650
crabsinger <83605543+crabsinger@users.noreply.github.com>
2024-04-17 15:04:54 -0700

    Scaffold GiveParty homepage

---
+++ src/pages/GiveParty.tsx
@@ -0,0 +1,13 @@
+import { Flex, Text } from 'ui';
+
+export const GiveParty = () => {
+  return (
+    <>
+      <Flex column>
+        <Text h1>Give.Party</Text>
+        <Text h1>GIVE</Text>
+        <Text h1>PARTY</Text>
+      </Flex>
+    </>
+  );
+};
