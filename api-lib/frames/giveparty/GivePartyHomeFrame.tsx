4eb159a26c9fafee0316f7da42fad17de6932c23
Wing <wingfeatherwave@gmail.com>
2024-04-17 11:06:55 -0700

    3d skill style

--- api-lib/frames/giveparty/GivePartyHomeFrame.tsx
+++ api-lib/frames/giveparty/GivePartyHomeFrame.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import { Frame } from '../../../_api/frames/router.tsx';
-import { getFrameUrl } from '../../../_api/webhooks/neynar_mention.ts';
+// import { getFrameUrl } from '../../../_api/webhooks/neynar_mention.ts';
 import { insertInteractionEvents } from '../../gql/mutations.ts';
 import { insertCoLinksGive } from '../../insertCoLinksGive.ts';
 import { findOrCreateProfileByUsername } from '../../neynar/findOrCreateProfileByFid.ts';
-import { publishCast } from '../../neynar.ts';
+// import { publishCast } from '../../neynar.ts';
 import { FramePostInfo } from '../_getFramePostInfo.tsx';
 import { ErrorFrameImage } from '../ErrorFrame.tsx';
 import { IMAGE_URL_BASE } from '../layoutFragments/FrameBgImage.tsx';
@@ -39,22 +39,85 @@ const givePartyImageNode = async (params: Record<string, string>) => {
       <FrameBodyGradient
         gradientStyles={{
           background:
-            'radial-gradient(circle at 25% 0%, #E31A1A 0%, #790066 80%)',
+            'radial-gradient(circle at 25% 0%, #7516BF 0%, #00AEF9 80%)',
         }}
       />
       <div
-        tw="flex h-full items-center text-center"
-        style={{ fontSize: 60, fontWeight: 600 }}
+        tw="flex flex-col h-full justify-between"
+        style={{ fontSize: 80, fontWeight: 600, padding: '50px 40px' }}
       >
-        <div tw="flex flex-col" style={{ gap: 60 }}>
-          its a give party yall big up the people who are good at stuff
-          <div tw="flex items-center justify-center">
+        <div
+          tw="flex flex-col overflow-x-auto"
+          style={{ gap: 20, lineHeight: 1 }}
+        >
+          <div tw="flex">Who is great at</div>
+          <div
+            tw="flex relative whitespace-norwap "
+            style={{ marginBottom: 10, fontSize: 90 }}
+          >
+            <span
+              tw="absolute"
+              style={{
+                color: '#7647FF',
+                opacity: 0.75,
+                left: -16,
+                top: 0,
+              }}
+            >
+              #{skill}
+            </span>
+            <span
+              tw="absolute"
+              style={{
+                color: '#FF1FFF',
+                opacity: 0.75,
+                left: -10,
+                top: 8,
+              }}
+            >
+              #{skill}
+            </span>
+            <span
+              tw="absolute"
+              style={{
+                color: '#FF5FFF',
+                opacity: 0.75,
+                left: -6,
+                top: 4,
+              }}
+            >
+              #{skill}
+            </span>
+            <span
+              tw="absolute"
+              style={{
+                color: '#FFF',
+              }}
+            >
+              #{skill}
+            </span>
+            <span style={{ opacity: 0 }}>.</span>
+          </div>
+          <div tw="flex">on Farcaster?</div>
+        </div>
+
+        <div tw="flex justify-between">
+          <div
+            tw="flex"
+            style={{ fontWeight: 400, fontSize: 40, maxWidth: '50%' }}
+          >
+            Enter a name below, we&apos;ll send them a GIVE on your behalf
+          </div>
+          <div
+            tw="flex items-center"
+            style={{ fontSize: 70, fontWeight: 400, gap: 20 }}
+          >
+            <span>give.party</span>
             <img
               alt="gem"
               src={IMAGE_URL_BASE + 'GemWhite.png'}
-              style={{ width: 70, height: 70, margin: '0 20px' }}
+              style={{ width: 90, height: 90 }}
             />
-            <span>#{skill}</span>
           </div>
         </div>
         {error_message && (
