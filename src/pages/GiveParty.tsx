e98bb8a2ad40758da845e3ac5bc432d7aa00c7f2
graffe <graffe@graffe.co>
2024-04-18 20:38:18 -0700

    extend username max, special message for based

--- src/pages/GiveParty.tsx
+++ src/pages/GiveParty.tsx
@@ -122,7 +122,7 @@ export const GiveParty = () => {
                   },
                 }}
               >
-                Who is great at
+                Who is{skill === 'based' ? '' : ' great at'}
               </Text>
               <Text
                 semibold
