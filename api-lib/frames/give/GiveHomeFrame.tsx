5ea97c11c66547ef0fc8185098bb6fab799a3e6a
Wing <wingfeatherwave@gmail.com>
2024-04-20 10:25:59 -0700

    absolute position pill skill

--- api-lib/frames/give/GiveHomeFrame.tsx
+++ api-lib/frames/give/GiveHomeFrame.tsx
@@ -60,28 +60,27 @@ const homeFrameImageNode = async (params: Record<string, string>) => {
           <OGAvatar avatar={give.giver_profile_public.avatar} />
 
           <div
-            tw="flex flex-col items-center grow justify-center"
+            tw="flex flex-col items-center grow justify-center relative"
             style={{ lineHeight: 1, gap: 8 }}
           >
-            <div
-              tw="flex items-center grow justify-center"
-              style={{ fontSize: 55, lineHeight: 1, gap: 20 }}
-            >
+            <div tw="flex items-center grow justify-center" style={{ gap: 20 }}>
               +1
               <img
                 alt="gem"
                 src={IMAGE_URL_BASE + 'GemWhite.png'}
-                style={{ width: 60, height: 60 }}
+                style={{ width: 70, height: 70 }}
               />
               <span>GIVE</span>
             </div>
             {give.skill && (
               <span
                 style={{
+                  position: 'absolute',
+                  bottom: -47,
                   fontSize: 34,
-                  background: 'rgba(0,0,0,0.4)',
-                  padding: '6px 14px 6px 15px',
-                  borderRadius: 8,
+                  background: 'rgba(20,20,20,0.8)',
+                  padding: '8px 24px 8px 25px',
+                  borderRadius: 99,
                 }}
               >
                 #{give.skill}
