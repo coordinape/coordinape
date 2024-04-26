d137318cc1a17ca001cd3e8b37ec340f578bb049
Graffe <95149165+CryptoGraffe@users.noreply.github.com>
2024-04-25 19:37:23 -0700

    Revert "user farcast user name for mentions and lower username case (#2889)" (#2921)
    This reverts commit 6d12e9005124bf17699bfed722dff4731c88d015.

--- api-lib/neynar/findOrCreateProfileByFid.ts
+++ api-lib/neynar/findOrCreateProfileByFid.ts
@@ -59,7 +59,7 @@ const findOrCreateUser = async (fc_profile: {
   const profile = await findProfileByAddresses(potential_addresses);
 
   if (profile) {
-    return { ...profile, fc_username: fc_profile.username };
+    return profile;
   } else {
     console.log('No profile found for addresses', potential_addresses);
     const address = potential_addresses.pop();
@@ -132,6 +132,6 @@ const createProfile = async (
     }
   );
   assert(insert_profiles_one, "panic: adding profile didn't succeed");
-  return { ...insert_profiles_one, fc_username: preferred_name };
+  return insert_profiles_one;
 };
 const FC_BOT_CONNECTOR = 'farcaster-bot-created';
