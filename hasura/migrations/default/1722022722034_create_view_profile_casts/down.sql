8ef6a7ddf8d4f257633adb1f58a35a09ecdfbf44
graffe <graffe@graffe.co>
2024-07-31 10:49:00 -0700

    cleanup migrations

--- hasura/migrations/default/1722022722034_create_view_profile_casts/down.sql
+++
@@ -1,5 +0,0 @@
--- Could not auto-generate a down migration.
--- Please write an appropriate down migration for the SQL below:
--- CREATE
--- OR REPLACE VIEW "public"."profile_casts" AS
--- SELECT c.id as cast_id,fa.profile_id,fa.fid FROM "farcaster"."casts" c JOIN "public"."farcaster_accounts" fa ON fa.fid = c.fid WHERE c.deleted_at IS NULL;
