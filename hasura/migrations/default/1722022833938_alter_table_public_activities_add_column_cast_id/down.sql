0111b2ad1848fdd3bf780551765355afc159d151
graffe <graffe@graffe.co>
2024-07-31 10:46:27 -0700

    fix down migration

--- hasura/migrations/default/1722022833938_alter_table_public_activities_add_column_cast_id/down.sql
+++ hasura/migrations/default/1722022833938_alter_table_public_activities_add_column_cast_id/down.sql
@@ -1,4 +1,1 @@
--- Could not auto-generate a down migration.
--- Please write an appropriate down migration for the SQL below:
--- alter table "public"."activities" add column "cast_id" int8
---  null;
+alter table "public"."activities" drop column "cast_id"
