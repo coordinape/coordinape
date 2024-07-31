9f1cae503557d60f9da10aaaaa3922160365c172
graffe <graffe@graffe.co>
2024-07-30 18:07:14 -0700

    cleanup

--- src/features/rainbowkit/Rainbowify.tsx
+++ src/features/rainbowkit/Rainbowify.tsx
@@ -9,38 +9,6 @@ import '@rainbow-me/rainbowkit/styles.css';
 import { RainbowKitSiweProvider } from './SiweProvider';
 
 const Rainbow = ({ children }: { children: ReactNode }) => {
-  // const refreshKey = useRefresh();
-  //
-  // useEffect(() => {
-  //   // refresh auth token
-  //   const t = getAuthToken(false);
-  //   if (t != token) {
-  //     setToken(t);
-  //   }
-  // }, [refreshKey]);
-
-  // const account = useAccount();
-
-  // // if we have a connected wallet and auth token in memory is null, reload from cookie
-  // useEffect(() => {
-  //   // if (token) {
-  //   //   // if (account) {
-  //   //   //   setAuthState('authenticated');
-  //   //   //   return;
-  //   //   // } else {
-  //   //   //   setAuthState('unauthenticated');
-  //   //   // }
-  //   // } else {
-  //   //   // if (reloadAuthFromCookie()) {
-  //   //   //   // TODO: this needs to work
-  //   //   //   setAuthState('authenticated');
-  //   //   //   setToken(getAuthToken(false));
-  //   //   // } else {
-  //   //   //   setAuthState('unauthenticated');
-  //   //   // }
-  //   // }
-  // }, [account, token]);
-
   return (
     <RainbowKitSiweProvider>
       <RainbowKitProvider theme={darkTheme()}>
@@ -49,7 +17,6 @@ const Rainbow = ({ children }: { children: ReactNode }) => {
         {/*<p>Refresh count: {refreshKey}</p>*/}
         {/*<p>auth token: {token}</p>*/}
         {/* = = = = = = = = = = END DEBUG ==========  */}
-
         {children}
       </RainbowKitProvider>
     </RainbowKitSiweProvider>
