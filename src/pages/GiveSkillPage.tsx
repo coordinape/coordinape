46792cf3192456cd8bde2ed169a40ee05139624a
Wing <wingfeatherwave@gmail.com>
2024-09-26 09:56:13 -0700

    merge

--- src/pages/GiveSkillPage.tsx
+++ src/pages/GiveSkillPage.tsx
@@ -1,43 +1,65 @@
 import { Helmet } from 'react-helmet';
 import { Navigate, useParams } from 'react-router-dom';
+import { CSS } from 'stitches.config';
 
 import { webAppURL } from '../config/webAppURL';
 import { Wand } from '../icons/__generated';
 import { coLinksPaths } from '../routes/paths';
-import { disabledStyle } from '../stitches.config';
 import { Button, Flex, Link } from '../ui';
 
+import { GiveBotCard } from './colinks/give/GiveBotCard';
 import { ResponsiveColumnLayout } from './colinks/give/GivePage';
+import { GivePartyCard } from './colinks/give/GivePartyCard';
+import { LearnAboutGiveCard } from './colinks/give/LearnAboutGiveCard';
 import { GiveSkillLeaderboard } from './GiveSkillLeaderboard';
 
 export const GiveSkillPage = () => {
   const { skill } = useParams();
-
-  const castLeaderboardUrl = `https://warpcast.com/~/compose?text=${encodeURIComponent(skill ? '#' + skill + ' GIVE Leaders' : '')}&embeds[]=${webAppURL('colinks')}/api/frames/router/meta/skill.leaderboard/${encodeURIComponent(skill ?? '')}`;
 
   return (
     <Flex column>
       <Helmet>
         <title>{skill} / GIVE / CoLinks</title>
       </Helmet>
-      <ResponsiveColumnLayout>
+      <ResponsiveColumnLayout smallColumnReverse>
         <Flex column>
+          <CastButton
+            skill={skill}
+            css={{
+              display: 'none',
+              '@sm': {
+                display: 'block',
+              },
+            }}
+          />
           <GiveSkillLeaderboard />
         </Flex>
-        <Flex column>
-          <Button
-            as={Link}
-            href={castLeaderboardUrl}
-            target="_blank"
-            rel="noreferrer"
+        <Flex
+          column
+          css={{
+            gap: '$xl',
+            flexGrow: 1,
+            '@sm': {
+              flexDirection: 'row',
+              gap: '$sm',
+              pb: '$sm',
+              overflow: 'scroll',
+              mx: '-$md',
+              px: '$md',
+            },
+          }}
+        >
+          <CastButton
+            skill={skill}
             css={{
-              ...(!skill && {
-                ...disabledStyle,
-              }),
+              '@sm': {
+                display: 'none',
+              },
             }}
-          >
-            <Wand fa size={'md'} /> Cast in Farcaster
-          </Button>
+          />
+          <LearnAboutGiveCard />
+          <GivePartyCard />
+          <GiveBotCard />
         </Flex>
       </ResponsiveColumnLayout>
     </Flex>
@@ -47,4 +69,22 @@ export const GiveSkillPage = () => {
 export const GiveSkillRedirect = () => {
   const { skill } = useParams();
   return <Navigate to={coLinksPaths.giveSkill(skill ?? '')} replace />;
+};
+
+const CastButton = ({ css, skill }: { css?: CSS; skill?: string }) => {
+  const castLeaderboardUrl = `https://warpcast.com/~/compose?text=${encodeURIComponent(skill ? '#' + skill + ' GIVE Leaders' : '')}&embeds[]=${webAppURL('colinks')}/api/frames/router/meta/skill.leaderboard/${encodeURIComponent(skill ?? '')}`;
+
+  return (
+    <Button
+      as={Link}
+      href={castLeaderboardUrl}
+      target="_blank"
+      rel="noreferrer"
+      css={{
+        ...css,
+      }}
+    >
+      <Wand fa size={'md'} /> Cast in Farcaster
+    </Button>
+  );
 };
