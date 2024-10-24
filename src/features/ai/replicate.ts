ca3f572eb905fbf66dae09a7693555c0e48cb53b
crabsinger <83605543+crabsinger@users.noreply.github.com>
2024-10-24 11:26:08 -0700

    udpate bones ai img prompt

--- src/features/ai/replicate.ts
+++ src/features/ai/replicate.ts
@@ -15,7 +15,7 @@ export const generateBonesGiveImg = async ({
   giverName: string;
   receiverName: string;
 }) => {
-  const prompt = `8bit, pixelated style, colorful, halloween. A scene of two dead animal skeleton spirits, one with an floating title '${giverName}', the other with a floating title '${receiverName}'. They are joyfully dancing and celebrating.`;
+  const prompt = `Top left corner has the title "${giverName}". Top right corner has the title: "${receiverName}". The scene is a dark fantasy, 8bit, pixel art.  The two skeletal spirits are interacting and exchanging a magical, glowing undead object. Graveyard scene with sharp spires and broken windows. The background features retro-style lightning strikes that illuminate the scene in stark flashes. Include video-glitch elements.`;
 
   const options = {
     model: 'ideogram-ai/ideogram-v2-turbo',
@@ -23,6 +23,8 @@ export const generateBonesGiveImg = async ({
     input: {
       prompt: prompt,
       resolution: '1088x768',
+      negative_prompt:
+        'cute, happy, videogame, game, bricks, hats, symmetrical',
       style_type: 'None',
       magic_prompt_option: 'On',
     },
