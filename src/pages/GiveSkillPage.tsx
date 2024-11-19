d42171d79484a0a690d15171a6cd29515a84b4ec
Wing <wingfeatherwave@gmail.com>
2024-11-19 14:51:20 -0800

    micro map

--- src/pages/GiveSkillPage.tsx
+++ src/pages/GiveSkillPage.tsx
@@ -1,16 +1,20 @@
 import { RecentGives } from 'features/colinks/RecentGives';
 import { Helmet } from 'react-helmet';
-import { Navigate, useParams } from 'react-router-dom';
+import { Navigate, NavLink, useParams } from 'react-router-dom';
 
 import { coLinksPaths } from '../routes/paths';
-import { Flex } from '../ui';
+import { Button, Flex } from '../ui';
+import useMobileDetect from 'hooks/useMobileDetect';
+import { Maximize } from 'icons/__generated';
 
 import { ResponsiveColumnLayout } from './colinks/give/GivePage';
 import { LearnAboutGiveCard } from './colinks/give/LearnAboutGiveCard';
 import { GiveSkillNav } from './GiveSkillNav';
+import { AutosizedGiveGraph } from './NetworkViz/AutosizedGiveGraph';
 
 export const GiveSkillPage = () => {
   const { skill } = useParams();
+  const { isMobile } = useMobileDetect();
 
   return (
     <Flex column>
@@ -31,24 +35,61 @@ export const GiveSkillPage = () => {
 
           {skill && <RecentGives skill={skill} />}
         </Flex>
-        <Flex
-          column
-          css={{
-            gap: '$md',
-            flexGrow: 1,
-            '@sm': {
-              flexDirection: 'row',
-              gap: '$sm',
-              pb: '$sm',
-              overflow: 'scroll',
-              mx: '-$md',
-              px: '$md',
-            },
-          }}
-        >
-          Micro Leaderboard
-          <LearnAboutGiveCard />
-        </Flex>
+        {!isMobile && (
+          <Flex
+            column
+            css={{
+              gap: '$md',
+              flexGrow: 1,
+              '@sm': {
+                flexDirection: 'row',
+                gap: '$sm',
+                pb: '$sm',
+                overflow: 'scroll',
+                mx: '-$md',
+                px: '$md',
+              },
+            }}
+          >
+            <Flex
+              css={{
+                position: 'relative',
+                height: 200,
+                width: '100%',
+                overflow: 'hidden',
+                background: 'rgba(0,0,0,0.1)',
+                borderRadius: '$2',
+                mb: '$sm',
+              }}
+            >
+              <AutosizedGiveGraph
+                mapHeight={200}
+                expand={false}
+                skill={skill}
+              />
+              <Flex
+                css={{
+                  position: 'absolute',
+                  bottom: '$sm',
+                  width: '100%',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                <Button
+                  as={NavLink}
+                  to={coLinksPaths.skillGiveMap(`${skill}`)}
+                  color={'cta'}
+                  size="xs"
+                >
+                  <Maximize />
+                  Expand GIVE Map
+                </Button>
+              </Flex>
+            </Flex>
+            <LearnAboutGiveCard />
+          </Flex>
+        )}
       </ResponsiveColumnLayout>
     </Flex>
   );
