483b82ddf4262a0b6b7c7d32dd12ed4c778ccc7e
crabsinger <83605543+crabsinger@users.noreply.github.com>
2024-11-19 14:58:28 -0800

    Create api to get top/trending give skills.

---
+++ _api/give/trending.ts
@@ -0,0 +1,53 @@
+import assert from 'assert';
+
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+import { order_by } from '../../api-lib/gql/__generated__/zeus/index.ts';
+import { adminClient } from '../../api-lib/gql/adminClient.ts';
+import { errorResponse } from '../../api-lib/HttpError.ts';
+
+const LIMIT = 10;
+
+// TODO: refactor to one global cache config?
+// 1 hour
+const maxAge = 60 * 60;
+export const CACHE_CONTENT = `public, s-maxage=${maxAge}, max-age=${maxAge}, stale-while-revalidate=${maxAge * 2}`;
+
+export default async function handler(
+  _req: VercelRequest,
+  res: VercelResponse
+) {
+  try {
+    const data = await fetchTopGiveSkills();
+
+    res.setHeader('Cache-Control', CACHE_CONTENT);
+    return res.status(200).json(data);
+  } catch (e) {
+    return errorResponse(res, e);
+  }
+}
+
+export async function fetchTopGiveSkills() {
+  // fetch all give and cache response
+  const { colinks_gives_skill_count } = await adminClient.query(
+    {
+      colinks_gives_skill_count: [
+        {
+          limit: LIMIT,
+          order_by: [{ gives_last_7_days: order_by.desc }],
+        },
+        {
+          gives_last_7_days: true,
+          skill: true,
+        },
+      ],
+    },
+    {
+      operationName: `api_give_trending_fetchTopGiveSkills @cached(ttl: 300)`,
+    }
+  );
+
+  assert(colinks_gives_skill_count, 'colinks_gives_skill_count not found');
+
+  return colinks_gives_skill_count;
+}
